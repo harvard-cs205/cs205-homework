d42b7867c1c36eaf4c37c71b575d00b75e466106
Peilin <duan@g.harvard.edu>
2015-09-05 08:40:07 -0400

    P4 part 4

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -30,44 +30,78 @@ if __name__ == '__main__':
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
     #####################
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    s_true=np.loadtxt('P4_trajectory.txt',delimiter=',')
+    #Get transpose
+    s_true=zip(*s_true)
+    ax.plot(s_true[0], s_true[1], s_true[2],'--b', label='True trajectory')
 
     #####################
     # Part 2:
     #
     # Read the observation array and plot it (Part 2)
     #####################
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    m_k=np.loadtxt('P4_measurements.txt',delimiter=',')
+    #Set up Matrix C
+    C=np.identity(3)
+    C[0][0]=1/rx
+    C[1][1]=1/ry
+    C[2][2]=1/rz
+    Col_length=len(m_k[:,0])
+    x_k=np.zeros([Col_length,3])
+    i=0
+    while i<Col_length:
+     x_k[i]=np.dot(m_k[i],C)
+     i=i+1
+    x_k=np.transpose(x_k)
+    ax.plot(x_k[0], x_k[1], x_k[2], '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
     # Use the initial conditions and propagation matrix for prediction
     #####################
 
-    # A = ?
-    # a = ?
-    # s = ?
-
+    A = np.zeros([6,6])
+    A[0][0]=A[1][1]=A[2][2]=1
+    A[0][3]=A[1][4]=A[2][5]=dt
+    A[3][3]=A[4][4]=A[5][5]=1-c*dt
+    
+    a = [0,0,0,0,0,g*dt]
+ 
+    s = np.array([0,0,2,15,3.5,4.0])
     # Initial conditions for s0
+    S=np.zeros([6,K+1])
+    S[:,0]=s
     # Compute the rest of sk using Eq (1)
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
+    k=0
+    while k<K:
+      S[:,k+1]=np.dot(A,S[:,k])+a
+      k=k+1
+    
+    ax.plot(S[0], S[1], S[2],'-k', label='Blind trajectory')
 
     #####################
     # Part 4:
     # Use the Kalman filter for prediction
     #####################
 
-    # B = ?
-    # C = ?
+    B = np.zeros([6,6])
+    B[0][0]=bx;B[1][1]=by;B[2][2]=bz
+    B[3][3]=bvx;B[4][4]=bvy;B[5][5]=bvz
+    
+    C=np.identity(3)
+    C[0][0]=rx
+    C[1][1]=ry
+    C[2][2]=rz
 
     # Initial conditions for s0 and Sigma0
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
+    # predictS
+    def predictS(x,A,a):
+      return np.dot(A,x)+a
+    #predictSig
+    def predictSig(A,Sig,B):
+      return np.    
+
 
     # ax.plot(x_coords, y_coords, z_coords,
     #         '-r', label='Filtered trajectory')
