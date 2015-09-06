f0e051b385314cc048827444a94cd64428045cb9
Kendrick Lo <klo@g.harvard.edu>
2015-09-06 19:54:36 -0400

    complete Part 1 of Question P4

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -1,3 +1,18 @@
+###############################
+
+# CS 205 Fall 2015 Homework 0 Parts 4-6
+# Submitted by Kendrick Lo (Harvard ID: 70984997)
+# Github username: ppgmg
+
+# Note to reader: 
+# I am in the process of acquainting myself
+# with Python; therefore, I may have included extraneous
+# comments to document my process and observations for my
+# own reference. These would be unnecessary if writing 
+# for an audience that already understands the language.
+
+###############################
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -31,8 +46,25 @@ if __name__ == '__main__':
     # Normally, this data wouldn't be available in the real world
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    # We load the data from the file P4_trajectory.txt
+    # which we assume is in the current working directory
+
+    # We previewed the data and noted it appeared to consist
+    # of 120 lines of six comma-separated values, with no
+    # obvious anomalies or data entry errors/missing values
+
+    filename_true = "P4_trajectory.txt"
+    s_true = np.loadtxt(filename_true,delimiter=",")
+    # print s_true.shape, s_true.dtype 
+
+    # extract first three columns to obtain series of positions
+    x_coords = s_true[:,0]
+    y_coords = s_true[:,1]
+    z_coords = s_true[:,2]
+    # print x_coords.shape, y_coords.shape, z_coords.shape
+
+    ax.plot(x_coords, y_coords, z_coords,
+            '--b', label='True trajectory')
 
     #####################
     # Part 2:
