2834d8670fc343208ce2f5004f77de13a1278817
Abhishek Malali <abhishekmalali@g.harvard.edu>
2015-09-05 23:54:39 -0400

    Progress till part 3

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -30,33 +30,45 @@ if __name__ == '__main__':
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
     #####################
+    s_true = np.loadtxt('P4_trajectory.txt', delimiter = ',', dtype = [('x_coords',float),('y_coords',float),('z_coords',float),('vx',float),('vy',float),('vz',float)])
+    x_coords =  s_true['x_coords']
+    y_coords =  s_true['y_coords']
+    z_coords =  s_true['z_coords']
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    ax.plot(x_coords, y_coords, z_coords,
+            '--b', label='True trajectory')
 
     #####################
     # Part 2:
     #
     # Read the observation array and plot it (Part 2)
-    #####################
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    ##################### 
+    s_obs = np.loadtxt('P4_measurements.txt',delimiter =',')
+    C = np.matrix([[1/rx,0,0],[0,1/ry,0],[0,0,1/rz]])
+    meas = s_obs * C;
+    ax.plot(s_obs[:,0],s_obs[:,1],s_obs[:,2],
+             '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
     # Use the initial conditions and propagation matrix for prediction
     #####################
-
-    # A = ?
-    # a = ?
-    # s = ?
+    A = np.concatenate((np.concatenate((np.eye(3),dt*np.eye(3))\
+        ,axis=1),np.concatenate((np.zeros((3,3)),\
+	(1-c*dt)*np.eye(3)),axis=1)),axis = 0)  
+    a = np.matrix([0,0,0,0,0,g*dt])
+    a = a.T 
+    s = np.matrix(np.zeros((6,K)))
+    
 
     # Initial conditions for s0
-    # Compute the rest of sk using Eq (1)
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
+    s[:,0] = np.matrix([0,0,2,15,3.5,4.0]).T
+    # Compute the rest of sk, using Eq (1)
+    for i in range(1,K):
+        s[:,i] = A*np.matrix(s[:,i-1]) + a
+    s = np.array(s)
+    ax.plot(s[0,:], s[1,:],s[2,:],\
+         '-k', label='Blind trajectory')
 
     #####################
     # Part 4:
