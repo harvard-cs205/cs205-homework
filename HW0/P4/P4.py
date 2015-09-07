04f3410bf24944f20d7061fd2450ec4230d126e3
shenjeffrey <shen01@g.harvard.edu>
2015-09-06 22:33:23 -0400

    Finished P4 Part 3

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -30,9 +30,18 @@ if __name__ == '__main__':
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
     #####################
+    s_true = np.loadtxt('P4_trajectory.txt', delimiter=',')
+    # print(s_true)
+    # print("2nd column")
+    # print(s_true[:,1])
+    
+    x_coords_truth = s_true[:,0]
+    y_coords_truth = s_true[:,1]
+    z_coords_truth = s_true[:,2]
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    # Plot True Trajectory Coordinates
+    ax.plot(x_coords_truth, y_coords_truth, z_coords_truth,
+            '--b', label='True trajectory')
 
     #####################
     # Part 2:
@@ -40,8 +49,10 @@ if __name__ == '__main__':
     # Read the observation array and plot it (Part 2)
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    s_obs = np.loadtxt('P4_measurements.txt', delimiter=',')
+
+    ax.plot(s_obs[:,0]/rx, s_obs[:,1]/ry, s_obs[:,2]/rz,
+            '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
@@ -49,14 +60,42 @@ if __name__ == '__main__':
     #####################
 
     # A = ?
-    # a = ?
-    # s = ?
+    left_top = np.zeros((3, 3), float)
+    np.fill_diagonal(left_top, 1)
+    right_top = np.zeros((3, 3), float)
+    np.fill_diagonal(right_top, dt)
+
+    left_bot = np.zeros((3, 3), float)
+    right_bot = np.zeros((3, 3), float)
+    np.fill_diagonal(right_bot, 1-c*dt)
+
+    top = np.hstack([left_top, right_top])
+    bottom = np.hstack([left_bot, right_bot])
+    top
+    bottom
+
+    A = np.vstack([top, bottom])
+    A
+
+    # a
+    a = np.matrix([0,0,0,0,0,g*dt])
+    a = a.transpose()
+
+    # s 6x121
+    s = np.matrix(np.zeros((6, K-1), float))
+
 
     # Initial conditions for s0
-    # Compute the rest of sk using Eq (1)
+    s0 = np.matrix([0, 0, 2, 15, 3.5, 4.0])
+    s0 = s0.transpose()
+    s[:,0] = s0
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
+    # Compute the rest of sk using Eq (1)
+    for i in range(1, K-1):
+        s[:,i] = A * s[:,i-1] + a
+
+    s = np.asarray(s)
+    ax.plot(s[0,:], s[1,:], s[2,:], '-k', label='Blind trajectory')
 
     #####################
     # Part 4:
