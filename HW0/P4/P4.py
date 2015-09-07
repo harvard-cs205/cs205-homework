9ccdf04b29031f602f63a2b3686d7c63be9b6f1e
Zhi Qian Seah <zhiqianseah@g.harvard.edu>
2015-09-07 18:33:56 -0400

    finished P4, P5, P6

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+from numpy.linalg import inv
 
 if __name__ == '__main__':
     # Model parameters
@@ -29,10 +29,19 @@ if __name__ == '__main__':
     #
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
+
+    trajectory = np.loadtxt("P4_trajectory.txt", delimiter=',')
+
+
+    #extract coordinates
+    x_coords = trajectory[:,0]
+    y_coords = trajectory[:,1]
+    z_coords = trajectory[:,2]
+
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    ax.plot(x_coords, y_coords, z_coords,
+             '--b', label='True trajectory')
 
     #####################
     # Part 2:
@@ -40,37 +49,124 @@ if __name__ == '__main__':
     # Read the observation array and plot it (Part 2)
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    #Note: there is an easier way of doing this part, but I am using the longer way to practice
+    #my python =)
+
+    observation = np.loadtxt("P4_measurements.txt", delimiter=',')
+
+    #define the C matrix. only first 3 columns are needed
+    C = np.diag([1/rx, 1/ry, 1/rz])
+
+    #convert observation array into a matrix
+    ob_matrix = np.matrix(observation)
+
+    #create a matrix for the un-stretched values. Populated with zeros first
+    fixed_matrix = np.zeros(ob_matrix.shape)
+
+    #calculate the un-stretched values based on matrix multiplication of
+    #each row with the C matrix. Transpose the row to a column for the multiplication
+    #then transpose it back to a row, and save it to fixed_matrix
+    for x in range (0, ob_matrix.shape[0]):
+        fixed_matrix[x,:] = (C * ob_matrix[x,:].transpose()).transpose()
+
+
+    #sanity check
+    #print fixed_matrix
+
+    #extract coordinates
+    x_coords = fixed_matrix[:,0]
+    y_coords = fixed_matrix[:,1]
+    z_coords = fixed_matrix[:,2]
+
+    ax.plot(x_coords, y_coords, z_coords,
+             '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
     # Use the initial conditions and propagation matrix for prediction
     #####################
 
-    # A = ?
-    # a = ?
-    # s = ?
+    A = np.matrix([[1, 0, 0, dt, 0, 0],
+                   [0, 1, 0, 0, dt, 0],
+                   [0, 0, 1, 0, 0, dt],
+                   [0, 0, 0, 1 - c*dt, 0, 0],
+                   [0, 0, 0, 0, 1 - c*dt, 0],
+                   [0, 0, 0, 0, 0, 1-c * dt]
+                    ])
+    a = np.matrix([0, 0, 0, 0, 0, g*dt]).transpose()
+    s = np.matrix([0, 0, 2, 15, 3.5, 4.0]).transpose()
+
 
     # Initial conditions for s0
-    # Compute the rest of sk using Eq (1)
+    prediction_matrix = np.zeros((6,121))
+    prediction_matrix[:,0] = s.reshape(6)
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
+    # Compute the rest of sk using Eq (1)
+    for x in range (1, K):
+        prediction_matrix[:,x] = (A * prediction_matrix[:,x-1].reshape(6,1) + a).reshape(6)
+
+    #extract coordinates
+    x_coords = prediction_matrix[0,:]
+    y_coords = prediction_matrix[1,:]
+    z_coords = prediction_matrix[2,:]        
+
+    ax.plot(x_coords, y_coords, z_coords,
+             '-k', label='Blind trajectory')
 
     #####################
     # Part 4:
     # Use the Kalman filter for prediction
     #####################
 
-    # B = ?
-    # C = ?
+    B = np.diag([bx,by, bz, bvx, bvy, bvz])
+    C = np.matrix([[rx, 0, 0, 0, 0, 0],
+                  [0, ry, 0, 0, 0, 0],
+                  [0, 0, rz, 0, 0, 0]])
+
+
+    observation = np.loadtxt("P4_measurements.txt", delimiter=',')
+
+    #convert observation array into a matrix
+    ob_matrix = np.matrix(observation)
 
     # Initial conditions for s0 and Sigma0
+
+
+    s0 = np.matrix([0, 0, 2, 15, 3.5, 4.0]).transpose()
+    sigma = 0.01 * np.identity(6)
+
+
+    filtered_matrix = np.zeros((6,121))
+    filtered_matrix[:,0] = s0.reshape(6)
+
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-r', label='Filtered trajectory')
+    for x in range(1,K):
+        #equation 2: predictS
+        s_tilde = (A * filtered_matrix[:,x-1].reshape(6,1) + a).reshape(6).transpose()
+
+        #equation 3: predictSig
+        sigma_tilde = inv(A * sigma * A.transpose() + B*B.transpose())
+
+        #equation 4: update sig
+        sig_updated = inv(sigma_tilde + C.transpose() * C)
+
+        #equation 5: updateS
+        s_updated = sig_updated * (sigma_tilde * s_tilde + C.transpose() *  ob_matrix[x,:].transpose())
+
+        filtered_matrix[:,x] = s_updated.reshape(6)     
+
+        #save sigma for next iteration
+        sigma = sig_updated   
+
+
+    #extract coordinates
+    x_coords = filtered_matrix[0,:]
+    y_coords = filtered_matrix[1,:]
+    z_coords = filtered_matrix[2,:]     
+
+    ax.plot(x_coords, y_coords, z_coords,
+             '-r', label='Filtered trajectory')
 
     # Show the plot
     ax.legend()
--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Sleep for t seconds
 def burnTime(t):
@@ -10,22 +11,37 @@ def burnTime(t):
 if __name__ == '__main__':
     N = 16  # The number of jobs
     P = 4   # The number of processes
+    Tests = 100
 
     # A thread pool of P processes
     pool = mp.Pool(P)
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
+    wait_time = np.logspace(-6, 0, Tests)
 
     for t in wait_time:
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
+
+        start = time.time()
+        for x in range(0,16):
+            burnTime(wait_time[t])
+        end = time.time()
+        serialTime = end - start
+
+
+        start = time.time()
+        pool.map(burnTime(wait_time[t]), range(16))
+        end = time.time()
+        parallelTime = end - start      
+
+        print serialTime
+        print parallelTime
+        print
 
         # Compute the ratio of these times
-        # ratio.append(serialTime/parallelTime)
+        ratio.append(serialTime/parallelTime)
 
     # Plot the results
     plt.plot(wait_time, ratio, '-ob')
