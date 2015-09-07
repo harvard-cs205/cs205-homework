f582eedf6a6709874efa496693fcecb07f10b8b8
Kevin <kevinkeyjkw@gmail.com>
2015-09-06 20:48:17 -0400

    Can calculate elapsed time for serial and parallel..But still cant plot

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -1,7 +1,7 @@
 import multiprocessing as mp
 import time
 import matplotlib.pyplot as plt
-
+import random as rand
 # Sleep for t seconds
 def burnTime(t):
     time.sleep(t)
@@ -17,20 +17,29 @@ if __name__ == '__main__':
     # Use a variety of wait times
     ratio = []
     wait_time = []
+    for i in range(16):
+        wait_time.append(rand.uniform(0.000001,1))
 
+    parallelStartTime = time.time()
+    parallelResult = pool.map(burnTime,wait_time)
+    parallelElapsedTime = time.time() - parallelStartTime
+
+    serialStartTime = time.time()
     for t in wait_time:
-        # Compute jobs serially and in parallel
-        # Use time.time() to compute the elapsed time for each
+        burnTime(t)
         serialTime = 1
         parallelTime = 1
-
+        # Compute jobs serially and in parallel
+        # Use time.time() to compute the elapsed time for each
         # Compute the ratio of these times
         # ratio.append(serialTime/parallelTime)
-
+    serialElapsedTime = time.time() - serialStartTime
+    print serialElapsedTime
+    print parallelElapsedTime
     # Plot the results
-    plt.plot(wait_time, ratio, '-ob')
-    plt.xscale('log')
-    plt.xlabel('Wait Time (sec)')
-    plt.ylabel('Serial Time (sec) / Parallel Time (sec)')
-    plt.title('Speedup versus function time')
-    plt.show()
+    # plt.plot(wait_time, ratio, '-ob')
+    # plt.xscale('log')
+    # plt.xlabel('Wait Time (sec)')
+    # plt.ylabel('Serial Time (sec) / Parallel Time (sec)')
+    # plt.title('Speedup versus function time')
+    # plt.show()
