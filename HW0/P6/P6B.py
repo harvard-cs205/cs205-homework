3a5af46a3bbaf920e21166af014fb07734a638c0
Andrew Petschek <petschek@g.harvard.edu>
2015-09-08 23:22:42 -0400

    updated code for P6b, seems to be working now

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -7,30 +7,25 @@ import matplotlib.pyplot as plt
 def burnTime(t):
     time.sleep(t)
 
-
-
-
 # Main
 if __name__ == '__main__':
     N = 16  # The number of jobs
     P = 4   # The number of processes
 
-    pool = mp.Pool(P)
+    pool = mp.Pool(P) #create a pool of P processes
 
     # Use a variety of wait times
-    ratio = []
-    wait_time = np.linspace(10**-6,10,1000)
+    ratio = [] #empty ratio array to be filled in below
+    wait_time = np.array([10**-6,10**-5,10**-4,10**-3,10**-2,.5*10**-1,10**-1,1])
 
     for t in wait_time:
         # Compute jobs serially and in parallel
-        pool.map(burnTime,range(N))        
+        pool.map(burnTime(t),range(N))        
         parallelTime = time.time()        
         
         i = 1
         while i<=N:
             burnTime(t)
-            time.time()
-        # Use time.time() to compute the elapsed time for each
             serialTime = time.time()
             i+=1
 
