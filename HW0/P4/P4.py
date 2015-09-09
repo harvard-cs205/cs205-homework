cc3d2951f6fcc4ecd41f28ea18137625b935701b
zxjyjr <jxia01@fas.harvard.edu>
2015-09-09 17:50:07 -0400

    Work on P4 P5 P6 for HW0b

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -29,49 +29,118 @@ if __name__ == '__main__':
     #
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
+
+read_temp=np.loadtxt('P4_trajectory.txt',delimiter=',')
+xx=(np.transpose(read_temp))[0]
+yy=(np.transpose(read_temp))[1]
+zz=(np.transpose(read_temp))[2]
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+ax.plot(xx, yy, zz,'--b', label='True trajectory')
 
     #####################
     # Part 2:
     #
     # Read the observation array and plot it (Part 2)
-    #####################
+read_temp_meas=np.loadtxt('P4_measurements.txt',delimiter=',')
+C=np.matrix([[1/rx,0,0],[0,1/ry,0],[0,0,1/rz]])
+sm=np.transpose(read_temp_meas)
+xk=C*sm
+x_apx=np.squeeze(np.asarray(xk[0]))
+y_apx=np.squeeze(np.asarray(xk[1]))
+z_apx=np.squeeze(np.asarray(xk[2])) 
+	#####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+ax.plot(x_apx, y_apx, z_apx,'.g', label='Observed trajectory')
 
     #####################
     # Part 3:
     # Use the initial conditions and propagation matrix for prediction
     #####################
-
-    # A = ?
-    # a = ?
+s0=np.matrix([[0],[0],[2],[15],[3.5],[4.0]])
+A=np.matrix([[1,0,0,dt,0,0],[0,1,0,0,dt,0],[0,0,1,0,0,dt],[0,0,0,1-c*dt,0,0],[0,0,0,0,1-c*dt,0],[0,0,0,0,0,1-c*dt]])
+a=np.matrix([[0],[0],[0],[0],[0],[g*dt]])
+s=np.zeros((120,6))
+    
     # s = ?
+
+
+    # calculate each position
+s_temp=s0
+s[0]=np.transpose(s0)
+i=1
+while i < 120:
+	s_temp1=A*s_temp+a
+	s[i]=np.transpose(s_temp1)
+	s_temp=s_temp1
+	i+=1
+
 
     # Initial conditions for s0
     # Compute the rest of sk using Eq (1)
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
-
+s_transpose=np.transpose(s)
+x_cal=s_transpose[0]
+y_cal=s_transpose[1]
+z_cal=s_transpose[2]
+ax.plot(x_cal, y_cal, z_cal,'-k', label='Blind trajectory')
+	
     #####################
     # Part 4:
     # Use the Kalman filter for prediction
-    #####################
+def predictS(s):
+	A=np.matrix([[1,0,0,dt,0,0],[0,1,0,0,dt,0],[0,0,1,0,0,dt],[0,0,0,1-c*dt,0,0],[0,0,0,0,1-c*dt,0],[0,0,0,0,0,1-c*dt]])
+	a=np.matrix([[0],[0],[0],[0],[0],[g*dt]])
+	s_bar=A*s+a
+	return s_bar
 
-    # B = ?
-    # C = ?
+def predictSig(sigma):
+	A=np.matrix([[1,0,0,dt,0,0],[0,1,0,0,dt,0],[0,0,1,0,0,dt],[0,0,0,1-c*dt,0,0],[0,0,0,0,1-c*dt,0],[0,0,0,0,0,1-c*dt]])
+	B=np.matrix([[bx,0,0,0,0,0],[0,by,0,0,0,0],[0,0,bz,0,0,0],[0,0,0,bvx,0,0],[0,0,0,0,bvy,0],[0,0,0,0,0,bvz]])
+	sigma_bar=(A*sigma*(np.transpose(A))+B*(np.transpose(B))).I
+	return sigma_bar
+
+def updateSig(sigma_bar):
+	C=np.matrix([[rx,0,0,0,0,0],[0,ry,0,0,0,0],[0,0,rz,0,0,0]])
+	sigma_kplus=(sigma_bar+(np.transpose(C))*C).I
+	return sigma_kplus
+
+def updateS(s_bar,sigma_bar,sigma_kplus,m):
+	C=np.matrix([[rx,0,0,0,0,0],[0,ry,0,0,0,0],[0,0,rz,0,0,0]])
+	mm=np.asmatrix(m)
+	s_kplus=sigma_kplus*((sigma_bar*s_bar)+(np.transpose(C))*(np.transpose(mm)))
+	return s_kplus
+
 
     # Initial conditions for s0 and Sigma0
-    # Compute the rest of sk using Eqs (2), (3), (4), and (5)
+s0=np.matrix([[0],[0],[2],[15],[3.5],[4.0]])
+sigma_temp=0.01*(np.eye(6))
+ss=np.zeros([121,6])
+ss[0]=np.transpose(s0)
+s_temp=s0
+i=1
+# should use read_temp_meas[i]) or read_temp_meas[i-1])
+while i < 121:
+	s_bar=predictS(s_temp)
+	sigma_bar=predictSig(sigma_temp)
+	sigma_kplus=updateSig(sigma_bar)
+	s_kplus=updateS(s_bar,sigma_bar,sigma_kplus,read_temp_meas[i])
+	ss[i]=np.transpose(s_kplus)
+#update the temporary variable s and sigma
+	s_temp=s_kplus
+	sigma_temp=sigma_kplus
+	i+=1
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-r', label='Filtered trajectory')
 
-    # Show the plot
-    ax.legend()
-    plt.show()
+    #####################
+
+# Compute the rest of sk using Eqs (2), (3), (4), and (5)
+ss_transpose=np.transpose(ss)
+x_KF=ss_transpose[0]
+y_KF=ss_transpose[1]
+z_KF=ss_transpose[2]
+ax.plot(x_KF, y_KF, z_KF,'-r', label='Filtered trajectory')
+
+# Show the plot
+ax.legend()
+plt.show()
+
--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Sleep for t seconds
 def burnTime(t):
@@ -15,14 +16,30 @@ if __name__ == '__main__':
     pool = mp.Pool(P)
 
     # Use a variety of wait times
-    ratio = []
-    wait_time = []
-
+    ratio =np.zeros(7)
+    parallelTime=np.zeros(7)
+    serialTime=np.zeros(7)
+#    wait_time = 10**(-6)*np.array(range(1,10**2+1))
+    wait_time=[10**(-6),10**(-5),10**(-4),10**(-3),10**(-2),10**(-1),10**(0)]
+    i=0
     for t in wait_time:
         # Compute jobs serially and in parallel
-        # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
+		time_p1=time.time()
+		results_parallel=pool.map(burnTime,t*(np.ones(16)))
+		time_p2=time.time()
+		parallelTime[i] = time_p2-time_p1
+		time_s1=time.time()
+		for j in range(0,16):
+			burnTime(t)
+
+		time_s2=time.time()
+		serialTime[i] = time_s2-time_s1
+		i+=1
+
+    time_p=np.array(parallelTime)
+    time_s=np.array(serialTime)
+    ratio=time_s/time_p
+
 
         # Compute the ratio of these times
         # ratio.append(serialTime/parallelTime)
