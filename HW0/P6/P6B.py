956499c428cd1feebe9328bfc25a3fafa75b6caa
Andrew Petschek <petschek@g.harvard.edu>
2015-09-08 22:57:09 -0400

    working on 6b code

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -1,3 +1,4 @@
+import numpy as np
 import multiprocessing as mp
 import time
 import matplotlib.pyplot as plt
@@ -6,26 +7,35 @@ import matplotlib.pyplot as plt
 def burnTime(t):
     time.sleep(t)
 
+
+
+
 # Main
 if __name__ == '__main__':
     N = 16  # The number of jobs
     P = 4   # The number of processes
 
-    # A thread pool of P processes
     pool = mp.Pool(P)
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
+    wait_time = np.linspace(10**-6,10,1000)
 
     for t in wait_time:
         # Compute jobs serially and in parallel
+        pool.map(burnTime,range(N))        
+        parallelTime = time.time()        
+        
+        i = 1
+        while i<=N:
+            burnTime(t)
+            time.time()
         # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
+            serialTime = time.time()
+            i+=1
 
         # Compute the ratio of these times
-        # ratio.append(serialTime/parallelTime)
+        ratio.append(serialTime/parallelTime)
 
     # Plot the results
     plt.plot(wait_time, ratio, '-ob')
