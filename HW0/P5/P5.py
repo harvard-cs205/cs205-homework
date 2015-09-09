bee74d1b419f91b252fc5382d4680001e22785c8
Yuhan Tang <tang01@g.harvard.edu>
2015-09-08 22:21:30 -0400

    changed P5.py, fixed bug

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -1,23 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-if __name__ == 'main':
-	ax = Axes3D(plt.figure())
-	N = [2**i for i in range(8,100)]
-	dt = 1
-	time_take = []
-	for i in N:
-		j = 0
-		while True:
-			if i/2 != 0:
-				i = i/2
-				j+=1
-				continue
-			time_take.append(j)
-			break
-	print time_take
-	ax.plot(N,time_take,'.g', label='aha')
-	ax.legend()
-	plt.show()
+fig = plt.figure()
+N = [2**i for i in range(1,25)]
+dt = 1
+time_take = []
+for i in N:
+    j = 0
+    while True:
+		if i/2 != 0:
+			i = i/2
+			j+=1
+			continue
+		time_take.append(j)
+		break
+plt.plot(N,time_take,'', label='aha')
+plt.xlabel('Time steps')
+plt.ylabel('Number of bags')
+fig.suptitle('P5 image')
+fig.savefig('P5.png')
 
+#plt.show()
 
