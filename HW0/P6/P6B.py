423d51df2318eaff466f2803886d25a1662df7a6
Yuhan Tang <tang01@g.harvard.edu>
2015-09-09 21:14:03 -0400

    modified the code of P6B

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -11,36 +11,33 @@ def burnTime(t):
 if __name__ == '__main__':
     N = 16  # The number of jobs
     P = 4   # The number of processes
-    f = open('P6.txt','r+')
     # A thread pool of P processes
     pool = mp.Pool(P)
 
     # Use a variety of wait times
     ratio = []
-    wait_time = [10**i for i in range(-6,1)]
-
+    wait_time = []
+    wait_time_ = [10**i for i in range(-6,1)]
+    for n in wait_time_[:-2]:
+        for i in xrange(1,8):
+            wait_time.append(i*n)
     for t in wait_time:
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
         x = time.time()
-        pool.map(burnTime,[t for v in range(16)])
-
+        pool.map(burnTime,[t]*N)
         parallelTime = time.time()-x
-        f.write('parallel time for wait_time %fs is %fs\n'%(t,parallelTime))
         x = time.time()
         for k in range(N):
             burnTime(t)
         serialTime = time.time()-x
-        f.write('serial time for wait_time %fs is %fs\n'%(t,serialTime))
         #y = time.time()
         #serialTime = burnTime(t)
 
         # Compute the ratio of these times
-        f.write('ratio for wait_time %fs is %fs\n'%(t,serialTime/parallelTime))
         ratio.append(serialTime/parallelTime)
     
-    # Plot the results
-    f.close()
+    # Plot the result
     plt.plot(wait_time, ratio, '-ob')
     plt.xscale('log')
     plt.xlabel('Wait Time (sec)')
