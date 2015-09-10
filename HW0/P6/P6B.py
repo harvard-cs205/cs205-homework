a88498d6d28db71831f61822a8aeef79b03f9c26
Neil Chainani <tenusplayor@gmail.com>
2015-09-10 13:59:53 -0400

    Finished number 6

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 # Sleep for t seconds
 def burnTime(t):
     time.sleep(t)
+    return t
 
 # Main
 if __name__ == '__main__':
@@ -16,16 +17,26 @@ if __name__ == '__main__':
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
+    wait_time = [1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1]
 
     for t in wait_time:
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
+        a=time.time()
+        for x in xrange(0,N):
+            burnTime(t)
+        b=time.time()
+        serialTime = b-a
 
+        time_vec = [t for x in xrange(0,N)]
+        print time_vec
+
+        c = time.time()
+        pool.map(burnTime, time_vec)
+        d = time.time()
+        parallelTime = d-c
         # Compute the ratio of these times
-        # ratio.append(serialTime/parallelTime)
+        ratio.append(serialTime/parallelTime)
 
     # Plot the results
     plt.plot(wait_time, ratio, '-ob')
