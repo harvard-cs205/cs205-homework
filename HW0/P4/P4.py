aca657357ae6fb081322df0276ea658a1a60c20f
Nithin Tumma <nithin@Nithins-MacBook-Pro.local>
2015-09-09 20:27:15 -0400

    Question 5

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -30,16 +30,17 @@ if __name__ == '__main__':
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
     #####################
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    s_true = np.loadtxt('P4_trajectory.txt', delimiter=',')
+    x_coords, y_coords, z_coords = s_true[:, 0], s_true[:, 1], s_true[:, 2]
+    ax.plot(x_coords, y_coords, z_coords,
+             '--b', label='True trajectory')
 
     #####################
     # Part 2:
     #
     # Read the observation array and plot it (Part 2)
     #####################
-
+    
     # ax.plot(x_coords, y_coords, z_coords,
     #         '.g', label='Observed trajectory')
 
