5896570d9dd909368b55460dbf24d067a62f3f00
TmyJYGL8rAFhe9XFZUkD <e6eddc26@opayq.com>
2015-09-09 22:02:55 -0400

    Almost done with P4 HW0b

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -1,15 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from numpy.linalg import inv
 
+def predictS(A,s,a):
+   	return (A*s+a)	
+def predictSig(A,Sigma,B):
+	return inv(A*Sigma*np.transpose(A)+B*np.transpose(B))
+def updateSig(SigmaTilde,C):
+	return inv(SigmaTilde+np.transpose(C)*C)
+def updateS(Sigma,SigmaTilde,stilde,C,m):
+	return Sigma*(SigmaTilde*stilde+np.transpose(C)*m)
 
 if __name__ == '__main__':
+    
+    
     # Model parameters
     K = 121    # Number of times steps
     dt = 0.01  # Delta t
     c = 0.1    # Coefficient of drag
     g = -9.81  # Gravity
-    # For constructing matrix B
+     # For constructing matrix B
     bx = 0
     by = 0
     bz = 0
@@ -29,48 +40,82 @@ if __name__ == '__main__':
     #
     # Load true trajectory and plot it
     # Normally, this data wouldn't be available in the real world
+    s_true=np.loadtxt('P4_trajectory.txt',delimiter=',')
+    
+    
     #####################
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    ax.plot(s_true[:,0], s_true[:,1], s_true[:,2],'--b', label='True trajectory')
 
     #####################
     # Part 2:
     #
     # Read the observation array and plot it (Part 2)
-    #####################
+    s_meas=np.loadtxt('P4_measurements.txt',delimiter=',')
+    cvec=np.array([rx,ry,rz])
+    c1=np.asmatrix(np.diag(cvec))
+    C=np.bmat([[c1, np.zeros([3,3])]])
+    m=np.asmatrix(np.zeros([3,K]))
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    s_t=np.bmat([[np.transpose(np.asmatrix(s_meas))],[np.asmatrix(np.ones([3,K]))]])
+    print(C)
+    for i in range(1,K):
+    	m[:,i]=np.dot(C,s_t[:,i])
+ #   m=np.bmat([np.bmat([s_meas[:,0]/rx,s_meas[:,1]/ry,s_meas[:,2]/rz]),
+  #  		np.asmatrix(np.zeros([K,3]))]).reshape((6,K))
+
+    #####################
+    ax.plot(s_meas[:,0]/rx,s_meas[:,1]/ry,s_meas[:,2]/rz,
+             '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
     # Use the initial conditions and propagation matrix for prediction
     #####################
-
-    # A = ?
-    # a = ?
-    # s = ?
-
+    II=np.asmatrix(np.identity(3))
+    A = np.bmat([[II, dt*II], [np.asmatrix(np.zeros([3,3])), (1-c*dt)*II]])
+    #print(A)
+    a = np.asmatrix(np.zeros([6,1]))
+    a[5,0]=g*dt
+    #print(a)
+    s = np.asmatrix(np.zeros([6,K]))
+	
     # Initial conditions for s0
+    s[:,0]=np.transpose(np.matrix([[0,0,2,15,3.5,4.0]]))
+    #print(s[:,0])
+    for i in range(1,K):
+    	s[:,i]=A*s[:,i-1]+a    	
+
     # Compute the rest of sk using Eq (1)
+    xk=np.asarray(s)
+    #print(xk[0,0:2])
+    ax.plot(xk[0,:],xk[1,:],xk[2,:],
+             '-k', label='Blind trajectory')
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
-
-    #####################
+	#####################
     # Part 4:
     # Use the Kalman filter for prediction
     #####################
-
-    # B = ?
-    # C = ?
-
+    #s should be 1d column vector
+    
+    bvec=np.array([bx,by,bz,bvx,bvy,bvz])	
+    B = np.asmatrix(np.diag(bvec))
+    
     # Initial conditions for s0 and Sigma0
+    
+    s[:,0]=np.transpose(np.matrix([[0,0,2,15,3.5,4.0]]))
+    Sigma=0.01*np.asmatrix(np.identity(6))
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-r', label='Filtered trajectory')
+    for i in range (0,K-1):
+		stilde=predictS(A,s[:,i],a)
+		#print(stilde)
+		#print(np.transpose(A))
+		SigmaTilde=predictSig(A,Sigma,B)
+		Sigma=updateSig(SigmaTilde,C)
+		print(m[:,i])
+		s[:,i+1]=updateS(Sigma,SigmaTilde,stilde,C,m[:,i])	
+		
+    ax.plot(s[1,:],s[2,:],s[3,:],
+     	'-r', label='Filtered trajectory')
 
     # Show the plot
     ax.legend()
