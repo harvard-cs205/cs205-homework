c4e00f03960e1461b1ab9467439e73cfbd204a19
vinayps <vinayps89@gmail.com>
2015-09-09 20:14:54 -0400

    part 4 of problem 4

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     position_estimate_array = np.array( position_estimate_matrix )
     
     ax.plot( position_estimate_array[0], position_estimate_array[1], position_estimate_array[2], 
-             '.g', label='Observed trajectory')
+             '.g', label='Observed trajectory' )
 
     #####################
     # Part 3:
@@ -66,11 +66,12 @@ if __name__ == '__main__':
     
     a = np.mat( [ [ 0, 0, 0, 0, 0, g*dt ] ] ).transpose()
     
+    # Compute the rest of sk using Eq (1)
+    
     s_complete = np.mat( np.zeros( shape = ( 6, K ) ) )
     s_complete[:,0] = s_initial
     counter = 1
     
-    # Compute the rest of sk using Eq (1)
     # total number of measurements is 121 but number of points in trajectory is only 120 (including s0). 
     # I have thus taken 120 steps for a total of 121 position values
     while counter < K:
@@ -79,21 +80,67 @@ if __name__ == '__main__':
    
     s_complete = np.array( s_complete )
     
-    ax.plot(s_complete[0], s_complete[1], s_complete[2], '-k', label='Blind trajectory')     
+    ax.plot( s_complete[0], s_complete[1], s_complete[2], '-k', label='Blind trajectory' )     
 
     #####################
     # Part 4:
     # Use the Kalman filter for prediction
     #####################
 
-    # B = ?
-    # C = ?
+    # Prediction and Updation equations
+    
+    def PredictS( s_current ) :
+        s_intermediate = A * s_current + a
+        return s_intermediate
+    
+    def PredictSig( sigma_current ) :
+        sigma_intermediate = np.linalg.inv( A * sigma_current * A.transpose() + B * B.transpose() )
+        return sigma_intermediate
+    
+    def updateSig( sigma_intermediate ) :
+        sigma_updated = np.linalg.inv( sigma_intermediate + C.transpose() * C )
+        return sigma_updated
+    
+    def updateS( sigma_updated, sigma_intermediate, s_intermediate, next_measurement ) :
+        s_updated = sigma_updated * ( sigma_intermediate * s_intermediate + C.transpose() * next_measurement )
+        return s_updated
+    
+    B = np.mat( [ [ bx, 0, 0, 0, 0, 0 ],         
+                  [ 0, by, 0, 0, 0, 0 ], 
+                  [ 0, 0, bz, 0, 0, 0 ], 
+                  [ 0, 0, 0, bvx, 0, 0 ],
+                  [ 0, 0, 0, 0, bvy, 0 ],
+                  [ 0, 0, 0, 0, 0, bvz ] ] )
+
+    C = np.mat( [ [ rx, 0, 0, 0, 0, 0 ], 
+                  [ 0, ry, 0, 0, 0, 0 ], 
+                  [ 0, 0, rz, 0, 0, 0 ] ] )
 
     # Initial conditions for s0 and Sigma0
+    # s0 is unchanged from above --> s0 = s_initial
+    sigma_initial = 0.01 * np.mat( np.eye( 6 ) )
+            
+    # Measurements matrix is unchanged from above --> measurements_matrix
+    
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
-
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-r', label='Filtered trajectory')
+    
+    s_complete = np.mat( np.zeros( shape = ( 6, K ) ) )
+    s_complete[:,0] = s_initial
+    sigma_current = sigma_initial
+    counter = 1
+ 
+    # total number of measurements is 121 but number of points in trajectory is only 120 (including s0). 
+    # I have thus taken 120 steps for a total of 121 position values
+    while counter < K:
+        s_intermediate = PredictS( s_complete[:,counter-1] )
+        sigma_intermediate = PredictSig( sigma_current )
+        sigma_current = updateSig( sigma_intermediate )
+        s_complete[:,counter] = updateS( sigma_current, sigma_intermediate, s_intermediate, measurements_matrix[:,counter] )
+        counter += 1
+   
+    s_complete = np.array( s_complete )
+    
+    ax.plot( s_complete[0], s_complete[1], s_complete[2], '-r', label='Filtered trajectory' )
 
     # Show the plot
     ax.legend()
