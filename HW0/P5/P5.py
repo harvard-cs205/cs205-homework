260df3b1f63e567d98fb89d07e84f7c45906923f
Andrew Petschek <petschek@g.harvard.edu>
2015-09-09 22:09:25 -0400

    cleaning up

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -11,24 +11,34 @@ def singleWorkerTime(N): #create function that calculates time to count n bags b
     else:
         return N-1
 
-def infiniteWorkersTime(N):
-    time = 0   
-    if N < 2:
-        return 0
+def infTime(N,t): #wrote a recursive formula to calculate number of seconds it would take infinite workers to count N bags
+    if  N < 2:
+        return t
+    elif N == 2:
+        t += 1
+        return t
+    elif N % 2 == 0:
+        N = N / 2.0
+        t += 1 
+        print "elif"
+        return infTime(N,t)
     else:
-        while N > 1:
-            N = N / 2.0
-            time+=1
-        return time    
+        N = N-1
+        N = N / 2.0
+        N += 1
+        t += 1
+        print "else"
+        return infTime(N,t)
+
         
 single_worker = np.zeros(len(num_bags)) #initialize single worker vector
-infinite_workers = np.zeros(len(num_bags)) 
+infinite_workers = np.zeros(len(num_bags)) #initialize infinite worker vector
 
 for N in num_bags:
     single_worker[N] = singleWorkerTime(N) #fill in single worker vector
 
 for N in num_bags:
-    infinite_workers[N] = infiniteWorkersTime(N) #fill in single worker vector
+    infinite_workers[N] = infTime(N,0) #fill in single worker vector
 
 plt.plot(num_bags,single_worker,'r-',label='1 worker')
 plt.plot(num_bags,infinite_workers,'b-',label='infinite workers')
