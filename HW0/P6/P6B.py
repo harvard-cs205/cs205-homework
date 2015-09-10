1c113d0927c6165950243000fcdd90a34363eb80
samighoche <samighoche@college.harvard.ed>
2015-09-10 00:28:57 -0400

    Only have to answer question 6 of part 5 and written answers to part 6

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -16,16 +16,21 @@ if __name__ == '__main__':
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
+    wait_time = [0.000001*i for i in range(1,10)] + [0.00001*i for i in range(1,10)] + [0.0001*i for i in range(1,10)] + [0.001*i for i in range(1,10)] + [0.01*i for i in range(1,10)] + [0.1*i for i in range(1,10)] + [1]
 
     for t in wait_time:
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
-
+        serialStart = time.time()
+        for i in range(16):
+            burnTime(t)
+        serialTime = time.time() - serialStart
+        
+        parallelStart = time.time()
+        pool.map(burnTime, [t for i in range(16)])
+        parallelTime = time.time() - parallelStart
         # Compute the ratio of these times
-        # ratio.append(serialTime/parallelTime)
+        ratio.append(serialTime/parallelTime)
 
     # Plot the results
     plt.plot(wait_time, ratio, '-ob')
