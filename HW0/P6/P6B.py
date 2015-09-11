7ffb99620cf23739bf41d70f46e363ba11ac0199
tperol <tperol@seas.harvard.edu>
2015-09-11 18:40:42 -0400

    finished HW0

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -1,6 +1,8 @@
 import multiprocessing as mp
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+from pylab import savefig
 
 # Sleep for t seconds
 def burnTime(t):
@@ -16,21 +18,42 @@ if __name__ == '__main__':
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
+    wait_time = np.logspace(-6.0, 0, num=10)
 
     for t in wait_time:
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
-        serialTime = 1
-        parallelTime = 1
+        
+        # Compute jobs in parralel
+        time_start = time.time()
 
+        pool.map(burnTime,t*np.ones((N,1)))
+
+        time_end = time.time()
+
+        parallelTime = time_end - time_start
+        
+        # Compute jobs in series
+        
+        time_start = time.time()
+
+        for n in range(0,N):
+            burnTime(t)
+
+        time_end = time.time()
+        
+        serialTime = time_end - time_start
+        
         # Compute the ratio of these times
-        # ratio.append(serialTime/parallelTime)
+        ratio.append(serialTime/parallelTime)
 
     # Plot the results
+    plt.close()
+    plt.figure()
     plt.plot(wait_time, ratio, '-ob')
     plt.xscale('log')
     plt.xlabel('Wait Time (sec)')
     plt.ylabel('Serial Time (sec) / Parallel Time (sec)')
     plt.title('Speedup versus function time')
-    plt.show()
+    savefig('P6.png')
+
