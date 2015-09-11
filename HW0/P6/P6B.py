2d724ab3d66991e1e7dfd4fb0d81a70b9302428c
Yj Cheng <cheng.yungjen@gmail.com>
2015-09-11 17:15:08 -0400

    Done P5 and P6

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -16,18 +16,19 @@ if __name__ == '__main__':
 
     # Use a variety of wait times
     ratio = []
-    wait_time = [a**b for a,b in zip([10]*10,range(-6,1))]
+    wait_time = [a**b for a,b in zip([10]*10,range(-10,1))]
 
     for t in wait_time:
+        print "burnTime", t
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
         serialTime = time.time()
         for i in xrange(N):
-            time.sleep(t)
+            burnTime(t)
         serialTime = time.time() - serialTime
         
         parallelTime = time.time()
-        pool.map(burnTime(t), range(N))
+        pool.map(burnTime, [t]*N)
         parallelTime = time.time() - parallelTime
         
         print "serialTime", serialTime, "parallelTime", parallelTime
