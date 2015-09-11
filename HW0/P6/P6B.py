f367511369c9f83c54348331a061a33538cbc829
Kevin <kevinkeyjkw@gmail.com>
2015-09-11 14:51:44 -0400

    Modified P6B

--- HW0/P6/P6B.py
+++ HW0/P6/P6B.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import random as rand
 import numpy as np
 import sys
+import pdb
 # Sleep for t seconds
 def burnTime(t):
     time.sleep(t)
-    return time.time()
+    return
 # Main
 if __name__ == '__main__':
     N = 16  # The number of jobs
@@ -19,29 +20,29 @@ if __name__ == '__main__':
 
     # Use a variety of wait times
     ratio = []
-    wait_time = []
-    for i in range(16):
-        wait_time.append(rand.uniform(0.000001,1))
+    wait_time = [0.000001,0.00001,0.0001,0.001,0.01,0.1]
+    # for i in range(10):
+    #     wait_time.append(rand.uniform(0.000001,1))
 
-    wait_time = np.sort(wait_time)
-    parallelStartTime = time.time()
-    parallelResult = pool.map(burnTime,wait_time)
-    parallelElapsedTime = time.time() - parallelStartTime
-    parallelResult = np.array(parallelResult)
-    parallelResult -= parallelStartTime
-    serialStartTime = time.time()
-    serialResult = []
-    for t in wait_time:
-        temp1 = time.time()
-        temp2 = burnTime(t)
-        serialResult.append(temp2-temp1)
-        serialTime = 1
-        parallelTime = 1
+    #wait_time = np.sort(wait_time)
+    parallelTimes = []
+    serialTimes = []
+    #pdb.set_trace()
+    for i in range(len(wait_time)):
+        start1 = time.time()
+        for j in range(16):
+            burnTime(wait_time[i])
+        serialTimes.append(time.time() - start1)
+    for i in range(len(wait_time)):
+        start = time.time()
+        parallelResult = pool.map(burnTime,[wait_time[i]]*16)
+        parallelTimes.append(time.time()-start)
+    
         # Compute jobs serially and in parallel
         # Use time.time() to compute the elapsed time for each
         # Compute the ratio of these times
         # ratio.append(serialTime/parallelTime)
-    serialElapsedTime = time.time() - serialStartTime
+
     #print "serialElapsedTime",serialElapsedTime
     #print "parallelElapsedTime",parallelElapsedTime
     #print "parallelResult=",parallelResult
@@ -60,18 +61,23 @@ if __name__ == '__main__':
     # f.write(']\n')
     
     # Plot the results
-    for i in range(len(serialResult)):
-        ratio.append(serialResult[i]/parallelResult[i])
+    
     # f.write('Ratios = [')
     # for x in ratio:
     #     f.write("%s " % x)
     # f.write(']\n')
     #print "ratio=",ratio
+    ratios = []
+    for i in range(len(serialTimes)):
+        ratios.append(serialTimes[i]/parallelTimes[i])
+    print 'serialTimes',serialTimes
+    print 'parallelTimes',parallelTimes
+    print 'ratios',ratios
     #print "Waittime=",wait_time
-    plt.plot(wait_time, ratio, '-ob')
+    plt.plot(wait_time, ratios, '-ob')
     plt.xscale('log')
     plt.xlabel('Wait Time (sec)')
     plt.ylabel('Serial Time (sec) / Parallel Time (sec)')
     plt.title('Speedup versus function time')
-    #plt.savefig('P6B.png')
+    plt.savefig('P6B.png')
     plt.show()
