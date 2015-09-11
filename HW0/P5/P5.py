0d64beb3fe79aca0ae23d71adb956f49166b66ba
Luis Perez <luisperez@college.harvard.edu>
2015-09-11 02:33:04 -0400

    Finished question P5 exept 6

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -2,8 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-  bags = range(1,100)
+  bags = range(1,32)
   ptime = [np.ceil(np.log(b)) for b in bags]
   ntime = [b - 1 for b in bags]
-  plt.plot(bags, ntime)
-  plt.plot(bags, ntime)
+  plt.scatter(bags, ntime, c="red")
+  plt.scatter(bags, ptime, c="green")
+  plt.xlabel('Number of bags to add')
+  plt.ylabel('Time')
+  plt.title('Parallel (green) vs Sequential (red) Addition')
+
+  plt.show()
