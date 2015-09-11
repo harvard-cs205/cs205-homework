12072ca366dd74957f1b16b3f42160de7fb3b9c8
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-09-11 16:58:21 -0400

    updated graph to log scale

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # Get lots of values for N
-N = np.array(range(2, 1000, 70))
+N = np.array(range(2, 1000, 10))
 # Calculate the logs
 logs = np.ceil(np.log2(N))
 # Plot the figure
@@ -11,7 +11,8 @@ plt.scatter(N, N-1, c='blue', label='One Employee')
 plt.xlabel("Number of bags")
 plt.ylabel("Time Needed (sec)")
 plt.yscale('log')
+plt.axis([0, 1000, 0, 1000])
 plt.title("Comparison of Time Needed with Infinite Employees vs. One")
-plt.legend()
+plt.legend(loc='lower right')
 plt.show()
 
