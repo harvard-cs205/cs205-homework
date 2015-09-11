1dc2bab8a487860b86e815e01c8a6232447f3f85
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-09-11 16:53:07 -0400

    changed scaling in P5 chart

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # Get lots of values for N
-N = np.array(range(1, 1000, 70))
+N = np.array(range(2, 1000, 70))
 # Calculate the logs
 logs = np.ceil(np.log2(N))
 # Plot the figure
@@ -10,6 +10,7 @@ plt.scatter(N, logs, c='red', label='Infinite Employees')
 plt.scatter(N, N-1, c='blue', label='One Employee')
 plt.xlabel("Number of bags")
 plt.ylabel("Time Needed (sec)")
+plt.yscale('log')
 plt.title("Comparison of Time Needed with Infinite Employees vs. One")
 plt.legend()
 plt.show()
