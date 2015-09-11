7a4457c0ca4d9a96d05a41e3913fce1f9118c6e4
jack91an <fqian91@gmail.com>
2015-09-10 21:46:42 -0400

    Finished P4

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -31,8 +31,11 @@ if __name__ == '__main__':
     # Normally, this data wouldn't be available in the real world
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '--b', label='True trajectory')
+    s_true = np.loadtxt('P4_trajectory.txt', delimiter=',')
+    x_true = s_true[:, 0]
+    y_true = s_true[:, 1]
+    z_true = s_true[:, 2]
+    ax.plot(x_true, y_true, z_true, '--b', label = 'True trajectory')
 
     #####################
     # Part 2:
@@ -40,8 +43,14 @@ if __name__ == '__main__':
     # Read the observation array and plot it (Part 2)
     #####################
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '.g', label='Observed trajectory')
+    s_measure = np.loadtxt('P4_measurements.txt', delimiter=',')
+    correction = [[1.0/rx, 0, 0], [0, 1.0/ry, 0], [0, 0, 1.0/rz]]
+    s_measure_trans = s_measure.transpose()
+    s_measure_adj = np.dot(correction, s_measure_trans)
+    x_measure = s_measure_adj[0]
+    y_measure = s_measure_adj[1]
+    z_measure = s_measure_adj[2]
+    ax.plot(x_measure, y_measure, z_measure, '.g', label='Observed trajectory')
 
     #####################
     # Part 3:
@@ -51,12 +60,24 @@ if __name__ == '__main__':
     # A = ?
     # a = ?
     # s = ?
+    s_matrix = np.zeros([6,K], dtype=float)
+    propagation = np.zeros([6,6],dtype=float)
+    propagation_diag = np.array([1.0, 1.0, 1.0, 1.0-c*dt, 1.0-c*dt, 1.0-c*dt])
+    propagation_diag3 = np.array([dt, dt, dt])
+    propagation = propagation + np.diag(propagation_diag) + np.diag(propagation_diag3, k=3)
+    acceleration = np.array([0.0, 0.0, 0.0, 0.0, 0.0, g*dt]).transpose()
 
     # Initial conditions for s0
     # Compute the rest of sk using Eq (1)
+    s_matrix[:, 0] = np.array([0.0, 0.0, 2.0, 15.0, 3.5, 4.0]).transpose()
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-k', label='Blind trajectory')
+    for k in range(1, K):
+        s_matrix[:, k] = np.add(np.dot(propagation, s_matrix[:, k-1]), acceleration)
+    x_calc = s_matrix[0]
+    y_calc = s_matrix[1]
+    z_calc = s_matrix[2]
+
+    ax.plot(x_calc, y_calc, z_calc, '-k', label='Blind trajectory')
 
     #####################
     # Part 4:
@@ -65,12 +86,48 @@ if __name__ == '__main__':
 
     # B = ?
     # C = ?
+    A_matrix = propagation
+    B_diag = np.array([bx, by, bz, bvx, bvy, bvz])
+    B_matrix = np.diag(B_diag)
+    C_matrix = np.hstack([np.diag(np.array([rx, ry, rz])), np.zeros([3,3])])
 
     # Initial conditions for s0 and Sigma0
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
+    s2_matrix = np.zeros([6, K])
+    s2_matrix[:, 0] = np.array([0.0, 0.0, 2.0, 15.0, 3.5, 4.0]).transpose()
+    sig_diag = np.array([1, 1, 1, 1, 1, 1]) * 0.01
+    sig = np.diag(sig_diag)
 
-    # ax.plot(x_coords, y_coords, z_coords,
-    #         '-r', label='Filtered trajectory')
+    def predictS():
+        s2_est = np.add(np.dot(propagation, s2_prev), acceleration)
+        return s2_est
+
+    def predictSig():
+        sig_est = np.linalg.inv(((A_matrix * sig) * A_matrix.transpose()) + B_matrix * B_matrix.transpose())
+        return sig_est
+
+    def updateSig():
+        sig = np.linalg.inv(sig_est + np.dot(C_matrix.transpose(), C_matrix))
+        return sig
+
+    def updateS():
+        s2 = np.dot(sig, (np.dot(sig_est, s2_est) + np.dot(C_matrix.transpose(), s_measure_trans[:, k])))
+        return s2
+
+    for k in range(1, K):
+        s2_prev = s2_matrix[:, k-1]
+        s2_est = predictS()
+        sig_est = predictSig()
+        sig = updateSig()
+        s2 = updateS()
+        s2_matrix[:, k] = s2
+
+    x_s2 = s2_matrix[0]
+    y_s2 = s2_matrix[1]
+    z_s2 = s2_matrix[2]
+
+
+    ax.plot(x_s2, y_s2, z_s2, '-r', label='Filtered trajectory')
 
     # Show the plot
     ax.legend()
