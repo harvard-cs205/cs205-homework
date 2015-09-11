92ae6f7a54d796b7b6ae0f18298b97e6d71f3155
George Lok <georgelok@college.harvard.edu>
2015-09-10 22:45:38 -0700

    Problem 5 completed.

---
+++ HW0/P5/P5.py
@@ -0,0 +1,24 @@
+import numpy as np
+from pylab import *
+
+if __name__ == '__main__':
+    N = 65
+
+    x_coords = range(N)
+    y_coords = [x - 1 for x in x_coords]
+    y_coords[0] = 0
+
+    xlabel('number of bags')
+    ylabel('time in seconds')
+    plot(x_coords, y_coords, 
+            '--b', label='Lone Cashier')
+
+    y_coords = [np.ceil(np.log2(x)) for x in x_coords]
+    y_coords[0] = 0
+
+    plot(x_coords, y_coords, 
+            '--r', label='Infinite Cashiers')
+    # Show the plot
+    legend()
+    savefig("P5.png")
+    show()
