63da0efb43aa01e389583a45f2ee5b0e8c3f98b7
Rohit Ahuja <rohitahuja@college.harvard.edu>
2015-09-11 21:01:23 -0400

    HW0b done

--- HW0/P4/P4.py
+++ HW0/P4/P4.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     # Initial conditions for s0
     # Compute the rest of sk using Eq (1)
 
-    # Generate propogation matrix
+    # Set propogation matrix globally
     A = np.matrix([
         [1, 0, 0, dt, 0, 0],
         [0, 1, 0, 0, dt, 0],
@@ -76,7 +76,7 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 1-c*dt]
     ])
 
-    # Account for gravity
+    # Set gravity matrix globally
     a = np.matrix([0, 0, 0, 0, 0, g*dt]).transpose()
 
     # Init matrix to hold s values
@@ -84,25 +84,16 @@ if __name__ == '__main__':
     s_blind = np.asmatrix(s_blind)
 
     # Populate values for s0
-    s_blind[0,0] = 0
-    s_blind[1,0] = 0
-    s_blind[2,0] = 2
-    s_blind[3,0] = 15
-    s_blind[4,0] = 3.5
-    s_blind[5,0] = 4.0
+    s0 = np.matrix([0, 0, 2, 15, 3.5, 4.0]).transpose()
+    s_blind[:,0] = s0
 
     # Populate rest of the values
     for k in xrange(1, K):
-        sk = np.matrix([s_blind[0,k-1], s_blind[1,k-1], s_blind[2,k-1], s_blind[3,k-1], s_blind[4,k-1], s_blind[5,k-1]]).transpose()
-        skplus1 = A*sk + a
-        s_blind[0,k] = skplus1[0]
-        s_blind[1,k] = skplus1[1]
-        s_blind[2,k] = skplus1[2]
-        s_blind[3,k] = skplus1[3]
-        s_blind[4,k] = skplus1[4]
-        s_blind[5,k] = skplus1[5]
+        s_blind[:,k] = A * s_blind[:,k-1] + a
 
+    # Set as array to display coordinates
     s_blind = np.array(s_blind)
+
     # Plot
     ax.plot(s_blind[0], s_blind[1], s_blind[2],
             '-k', label='Blind trajectory')
@@ -112,26 +103,10 @@ if __name__ == '__main__':
     # Use the Kalman filter for prediction
     #####################
 
-    # B = ?
-    # C = ?
-
     # Initial conditions for s0 and Sigma0
     # Compute the rest of sk using Eqs (2), (3), (4), and (5)
 
-    def predictS(A, sk, a):
-        return A*sk + a
-
-    def predictSig(A, B, Ek):
-        return inv(A*Ek*A.transpose() + B*B.transpose())
-
-    def updateSig(E, C):
-        return inv(E + C.transpose()*C)
-
-    def updateS(Ekplus1, E, s, C, mkplus1):
-        return Ekplus1*(E*s+C.transpose()*mkplus1)
-
-    Ek = 0.01*np.identity(6)
-
+    # Set scale of errors matrix globally
     B = np.matrix([
         [bx, 0, 0, 0, 0, 0],
         [0, by, 0, 0, 0, 0],
@@ -139,53 +114,93 @@ if __name__ == '__main__':
         [0, 0, 0, bvx, 0, 0],
         [0, 0, 0, 0, bvy, 0],
         [0, 0, 0, 0, 0, bvz]
-        ])
+    ])
 
+    # Set capacity matrix globally
     C = np.matrix([
         [rx, 0, 0, 0, 0, 0],
         [0, ry, 0, 0, 0, 0],
         [0, 0, rz, 0, 0, 0]
-        ])
+    ])
+
+    ###
+    # Predict intermediary coordinates values
+    #
+    # @param sk  coordinates and velocities at kth time interval
+    #
+    # @return s matrix
+    ###
+    def predictS(sk):
+        return A * sk + a
+
+    ###
+    # Predict intermediary Sigma value
+    #
+    # @param SigmaK  covariance matrix at kth interval
+    #
+    # @return Sigma~ matrix
+    ###
+    def predictSig(SigmaK):
+        return inv(A * SigmaK * A.transpose() + B * B.transpose())
+
+    ###
+    # Return Sigma covariance matrix at k+1th time interval
+    #
+    # @param SigmaTilde  predicted intermediary covariance matrix
+    #
+    # @return SigmaKPlus1 matrix
+    ###
+    def updateSig(SigmaTilde):
+        return inv(SigmaTilde + C.transpose() * C)
+
+    ###
+    # Predict s~ value
+    #
+    # @param SigmaKPlus1  covariance matrix at k+1th time interval
+    # @param SigmaTilde   predicted intermediary covariance matrix
+    # @param sTilde       intermediary coordinates matrix
+    # @param MkPlus1      measurements at k+1th time interval 
+    #
+    # @return s~ matrix
+    ###
+    def updateS(SigmaKPlus1, SigmaTilde, sTilde, MkPlus1):
+        return SigmaKPlus1 * (SigmaTilde * sTilde + C.transpose() * MkPlus1)
+
+    # Set initial SigmaK
+    SigmaK = 0.01*np.identity(6)
 
     # Init matrix to hold s values
     s_kalman = np.zeros([6, K])
     s_kalman = np.asmatrix(s_kalman)
 
-    # Populate values for s0
-    s_kalman[0,0] = 0
-    s_kalman[1,0] = 0
-    s_kalman[2,0] = 2
-    s_kalman[3,0] = 15
-    s_kalman[4,0] = 3.5
-    s_kalman[5,0] = 4.0
+    # Populate values for s0 (using s0 assigned before)
+    s_kalman[:,0] = s0
 
+    # Set measured data points as matrix
     m_matrix = np.matrix(m_measured)
 
     # Populate rest of the values
-    for k in xrange(K-1):
-        sk = np.matrix([s_kalman[0,k], s_kalman[1,k], s_kalman[2,k], s_kalman[3,k], s_kalman[4,k], s_kalman[5,k]]).transpose()
+    for k in xrange(1, K):
+        # Get previous values
+        sk = s_kalman[:,k-1]
 
-        # A and a were assigned before
-        s = predictS(A, sk, a)
+        # Get kth measurement data
+        MkPlus1 = m_matrix[k,:].transpose()
 
-        E = predictSig(A, B, Ek)
+        # Get next coordinate set
+        sTilde = predictS(sk)
+        SigmaTilde = predictSig(SigmaK)
+        SigmaKPlus1 = updateSig(SigmaTilde)
+        skPlus1 = updateS(SigmaKPlus1, SigmaTilde, sTilde, MkPlus1)
+        s_kalman[:,k] = skPlus1
 
-        Ekplus1 = updateSig(E, C)
-
-        mkplus1 = m_matrix[k+1,:].transpose()
-        print(mkplus1)
-
-        skplus1 = updateS(Ekplus1, E, s, C, mkplus1)
-        Ek = Ekplus1
-        s_kalman[0,k+1] = skplus1[0]
-        s_kalman[1,k+1] = skplus1[1]
-        s_kalman[2,k+1] = skplus1[2]
-        s_kalman[3,k+1] = skplus1[3]
-        s_kalman[4,k+1] = skplus1[4]
-        s_kalman[5,k+1] = skplus1[5]    
-
+        # Set SigmaK for next iteration
+        SigmaK = SigmaKPlus1
+        
+    # Set kalman matrix as array
     s_kalman = np.array(s_kalman)
 
+    # Plot
     ax.plot(s_kalman[0], s_kalman[1], s_kalman[2],
             '-r', label='Filtered trajectory')
 
