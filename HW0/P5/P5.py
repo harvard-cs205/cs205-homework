b72f2341bdbe6d9cddb731d75620dfc4f64ef57a
1Reinier <reiniermaat@msn.com>
2015-09-11 20:44:53 -0400

    Gave my P5 a makeover

--- HW0/P5/P5.py
+++ HW0/P5/P5.py
@@ -1,34 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 __author__ = 'Reinier Maat'
 
 if __name__ == "__main__":
 
-    n_bags = range(1, 65536, 256)
+    n_bags = range(1, 65536, 1)
 
-    # Single case, all is added to the first pile
-    def time_taken_for_number_of_cashiers(number_of_bags, number_of_cashiers):
-        cashiers_in_use = 0
-        if number_of_cashiers > number_of_bags:
-            cashiers_in_use = number_of_bags    # other cashiers have nothing to do
-        else:
-            cashiers_in_use = number_of_cashiers
-        return (number_of_bags/cashiers_in_use) + (cashiers_in_use - 1)  # communication to main cashier
+    def single_cashier(bags):
+        return math.ceil(bags - 1)
 
-    single_cashier = 1
+    def infinite_cashiers(bags):
+        return math.ceil(np.log2(bags))
+
     single_result = []
     for n in n_bags:
-        single_result.append(time_taken_for_number_of_cashiers(n, single_cashier))
+        single_result.append(single_cashier(n))
 
-    infinite_cashiers = 1e99
     infinite_result = []
     for n in n_bags:
-        infinite_result.append(time_taken_for_number_of_cashiers(n, infinite_cashiers))
+        infinite_result.append(infinite_cashiers(n))
 
     plt.plot(n_bags, single_result, '-b')
-    plt.plot(n_bags, infinite_result, '--g')
-    plt.xlabel('bags')
-    plt.ylabel('time')
-    plt.title('Time to count bags. Cashiers in infinite case have been limited to the number of bags')
+    plt.plot(n_bags, infinite_result, '-g')
+    plt.yscale('log')
+    plt.xlabel('Bags')
+    plt.ylabel('Time (seconds) Log-scale')
+    plt.title('Time to count bags.')
     plt.show()
 
