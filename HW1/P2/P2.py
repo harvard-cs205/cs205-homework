4c4a6f24fb105873f035ead0e83887623e244965
1Reinier <reiniermaat@msn.com>
2015-09-20 18:12:36 -0400

    Test .gitignore

--- HW1/P2/P2.py
+++ HW1/P2/P2.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
 
-
 def mandelbrot(x, y):
     z = c = complex(x, y)
     iteration = 0
@@ -14,15 +13,15 @@ def mandelbrot(x, y):
 
 
 def sum_values_for_partitions(rdd):
-    'Returns (as an RDD) the sum of V for each partition of a (K, V) RDD'
+    """Returns (as an RDD) the sum of V for each partition of a (K, V) RDD"""
     # note that the function passed to mapPartitions should return a sequence,
     # not a value.
     return rdd.mapPartitions(lambda part: [sum(V for K, V in part)])
 
 
 def draw_image(rdd):
-    '''Given a (K, V) RDD with K = (I, J) and V = count,
-    display an image of count at each I, J'''
+    """Given a (K, V) RDD with K = (I, J) and V = count,
+    display an image of count at each I, J"""
 
     data = rdd.collect()
     I = np.array([d[0][0] for d in data])
@@ -32,3 +31,7 @@ def draw_image(rdd):
     im[I, J] = np.log(C + 1)  # log intensity makes it easier to see levels
     plt.imshow(im, cmap=cm.gray)
     plt.show()
+
+if __name__ == "__main__":
+    pass
+    pixels =
