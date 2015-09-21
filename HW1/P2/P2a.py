a464732cf671eb41fd9a5ba5581cd1e27dcb41c4
1Reinier <reiniermaat@msn.com>
2015-09-20 22:16:58 -0400

    P2 part a

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,16 @@
 from P2 import *
 
-# Your code here
+if __name__ == "__main__":
+    i_pixels = [i for i in xrange(2000)]
+    sc = pyspark.SparkContext()
+    pixelrow = sc.parallelize(i_pixels, 10)
+    pixels = pixelrow.cartesian(pixelrow)
+    pixels = pixels.map(lambda (i, j): ((i, j), (j / 500.0 - 2, i / 500.0 - 2)))
+    pixels = pixels.mapValues(lambda (x, y): mandelbrot(x, y))
+    # draw_image(pixels)
+    #summed_values = sum_values_for_partitions(pixels).collect()
+    print pixels.takeSample(True, 20)
+    #plt.hist(summed_values, bins=20)
+    plt.xlabel("Iterations")
+    plt.ylabel("Frequency")
+   # plt.show()
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,17 @@
 from P2 import *
 
-# Your code here
+if __name__ == "__main__":
+    i_pixels = [i for i in xrange(2000)]
+    sc = pyspark.SparkContext()
+    pixelrow = sc.parallelize(i_pixels, 10)
+    pixels = pixelrow.cartesian(pixelrow)
+    pixels = pixels.map(lambda (i, j): ((i, j), (j / 500.0 - 2, i / 500.0 - 2)), True)
+    pixels.collect()
+    pixels.partitionBy(100, lambda key: 0)
+    pixels = pixels.mapValues(lambda (x, y): mandelbrot(x, y))
+    # draw_image(pixels)
+    summed_values = sum_values_for_partitions(pixels).collect()
+    plt.hist(summed_values, bins=20)
+    plt.xlabel("Iterations")
+    plt.ylabel("Frequency")
+    plt.show()
