861d106b8dc25d11dad78fca71bfe84f10f7cf80
Nick Boffi <boffi@g.harvard.edu>
2015-09-23 07:59:54 -0400

    coding for part 1

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,35 @@
 from P2 import *
+import numpy as np
+import matplotlib.pyplot as plt
+from pyspark import SparkContext
 
-# Your code here
+# Convert pixel (i, j) to the correct (x, y) value
+def pixelize(x):
+    return x / 500. - 2
+
+# Number of x and y pixels
+NX = NY = 2000
+
+if __name__ == "__main__":
+    sc = SparkContext("local", "madelbrot")
+
+    # Get the i, j pixel arrays
+    xs = sc.parallelize(range(0, NX), 10)#.map(pixelize)
+    ys = sc.parallelize(range(0, NY), 10)#.map(pixelize)
+
+    # Take the cartesian product to get the pixel grid
+    # Also add a count variable to each
+    pixels = xs.cartesian(ys)
+
+    # Now calculate the mandelbrot counts and store with the original pixels
+    pixels_and_counts = pixels.map(lambda (x, y): ((x, y), mandelbrot(pixelize(x), pixelize(y))))
+
+    partition_work = sum_values_for_partitions(pixels_and_counts)
+    plt.hist(partition_work.take(partition_work.count()), bins=100)
+    plt.xlabel('Partition Number')
+    plt.ylabel('Compute')
+    plt.title('Total Compute vs. Partition - Naive Partitioning Scheme')
+    plt.savefig('test_hist_a.png')
+
+    draw_image(pixels_and_counts)
+    
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,32 @@
 from P2 import *
+from pyspark import SparkContext
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint
 
-# Your code here
+# Convert pixel (i, j) to the correct (x, y) value
+def pixelize(x):
+    return x / 500. - 2
+
+# Number of x and y pixels
+NX = NY = 2000
+
+if __name__ == "__main__":
+    sc = SparkContext("local", "madelbrot")
+
+    # Get the i, j pixel arrays
+    xs = sc.parallelize(range(0, NX), 10)#.map(pixelize)
+    ys = sc.parallelize(range(0, NY), 10)#.map(pixelize)
+
+    # Take the cartesian product to get the pixel grid
+    # Also add a count variable to each
+    pixels = xs.cartesian(ys).partitionBy(100, lambda p: randint(1, 101) )
+
+    # Now calculate the mandelbrot counts and store with the original pixels
+    pixels_and_counts = pixels.map(lambda (x, y): ((x, y), mandelbrot(pixelize(x), pixelize(y))))
+    partition_work = sum_values_for_partitions(pixels_and_counts)
+    plt.hist(partition_work.take(partition_work.count()))
+    plt.savefig('test_hist_b.png')
+
+    draw_image(pixels_and_counts)
+    
