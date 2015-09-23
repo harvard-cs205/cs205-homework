36df847f1c4db04a47df95c21da62015254ba56c
Peilin <duan@g.harvard.edu>
2015-09-23 10:53:25 -0400

    P2a first commit

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,30 @@
+"""P2a.py"""
+
+import findspark
+
+findspark.init()
+
+#from pyspark import SparkContext
+import pyspark
+
 from P2 import *
 
+import numpy as np
+
 # Your code here
+
+sc = pyspark.SparkContext("local", "P2a.py", pyFiles=['P2.py'] )
+
+data=np.zeros([2000,2000,2])
+
+for i in range(1,2000):
+  for j in range(1,2000):
+    data[i,j,0]=(j/500.0)-2
+    data[i,j,1]=(i/500.0)-2
+
+rdd=sc.parallelize(data,100)
+
+
+rdd.map(mandelbrot)
+
+rdd.take(10)
