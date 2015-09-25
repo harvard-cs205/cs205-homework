a30cd870d87eb881c145879761a70b063fe09534
ytang14 <tang01@g.harvard.edu>
2015-09-25 11:55:52 -0400

    finished P2

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -2,12 +2,16 @@ from P2 import *
 import findspark
 findspark.init()
 import pyspark
+import random
 from random import randint
 sc = pyspark.SparkContext(appName="Spark1")
-x = sc.parallelize(xrange(2000),10)
-y = sc.parallelize(xrange(2000),10)
-rdd = x.cartesian(x).repartition(100)
-
+x_ = range(2000)
+random.shuffle(x_)
+x = sc.parallelize(x_,10)
+rdd = x.cartesian(x)
+#y = sc.parallelize(xrange(2000),10)
+#rdd = x.cartesian(x).repartition(100)
+#rdd = x.cartesion(x).partitionBy(lambda x:randint(1,101))
 ans = rdd.map(lambda x: [x,mandelbrot((x[0]/500.0-2),(x[1]/500.0-2))])
 draw_image(ans)
 
