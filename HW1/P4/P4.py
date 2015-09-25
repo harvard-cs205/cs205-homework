d08e88c43ace92576b5d35da31f811d8e887fc5e
Dana Katzenelson <dkatzenelson@college.harvard.edu>
2015-09-25 08:18:29 -0500

    fixed text processing

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -8,13 +8,18 @@ sc = SparkContext(conf=conf)
 #Generate mapping from character to other characters they appear with
 #Take the cross-product of characters who appear in a given issue with itself, reduceByKey to form a set of all characters who appear with each character, remove key character from set
 source = sc.textFile("source.csv")
-issue_character = source.map(lambda line: (line.split(',')[1], line.split(',')[0]))
+
+#Standardize line formatting
+def cleanup(line):
+    splitted = line.split('"')
+    return (splitted[3], splitted[1])
+
+issue_character = source.map(cleanup)
 character_characters_mutable = issue_character.join(issue_character).map(lambda KV: (KV[1][0], {KV[1][1]} - {KV[1][0]})).reduceByKey(lambda x, y: x.union(y))
 #Need hashability later
 character_characters = character_characters_mutable.mapValues(lambda v: frozenset(v))
-
 #BFS
-origins = ['"CAPTAIN AMERICA"', '"MISS THING/MARY"', '"ORWELL"']
+origins = ['CAPTAIN AMERICA', 'MISS THING/MARY', 'ORWELL']
 results = {}
 for origin in origins:
     result = bfs(character_characters, origin)
