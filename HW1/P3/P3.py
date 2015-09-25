ab465d2f05ab7eff462f9e5ffb347d4984fb251f
Nicholas Boffi <nick.boffi@gmail.com>
2015-09-24 20:12:35 -0400

    finished problem 3

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -1,23 +1,4 @@
 from pyspark import SparkContext
-from itertools import permutations
-
-def clean_up_permutations(seq):
-    """ Takes a list of all possible permutations,
-    assumed to be in the format output by itertools.permutations
-    - i.e., a list of characters - and returns a list of non-duplicate
-    strings.
-    """
-
-    return list(set([''.join(p) for p in permutations(seq)]))
-
-def find_real_anagrams(list_of_poss):
-    """ Takes a list of possible anagrams (i.e., sequences of
-    jumbled letters) and returns the subset of that list which are
-    actually words.
-    """
-
-    return filter(lambda x: x in words, list_of_poss)
-
 
 if __name__ == "__main__":
 
@@ -26,32 +7,21 @@ if __name__ == "__main__":
 
     # Load in the words
     words = sc.textFile('EOWL_words.txt')
+    
+    # Pair up all the words with themselves wrapped in a list
+    pair_words = words.map(lambda x: (x, [x]))
 
-    # Alphabetize the words
-    # This returns sorted lists
-    list_words = words.map(sorted)
+    # Sort the first element as a key
+    sorted_pair_words = pair_words.map(lambda (x, y): (''.join(sorted(x)), y))
 
-    # And this joins the "list words" into usual strings
-    alph_words = listWords.map(lambda x: ''.join(x))
+    # Reduce by key and merge lists
+    seqs_and_word_list = sorted_pair_words.reduceByKey(lambda x,y: x+y)
 
-    # Now remove duplicates
-    distinct_sequences = alph_words.distinct()
+    # Now add in the number of words
+    proper_format = seqs_and_word_list.map(lambda (x, y): (x, len(y), y))
 
-    # We want to see every *distinct* way we can rearrange these sequences
-    #all_possible_anagrams = distinct_sequences.map(lambda x: (x, clean_up_permutations(x)))
-    all_possible_anagrams = distinct_sequences.map(lambda x: clean_up_permutations(x))
+    # And lets find the max
+    max_finder = proper_format.map(lambda (x, y, z): (y, x, z)).max()
 
-    # get the product of these with every word
-    all_anas_and_words = all_possible_anagrams.cartesian(words).map(lambda (x, y): (x, [y]))
-
-    # Reduce to get all words with every possible list
-    all_anas_and_words = all_anas_and_words.reduceByKey(lambda x, y: x + y))
-
-
-    # Filter by those which are actually words
-    real_anagrams = all_possible_anagrams.map(lambda (seq, perms): (seq, find_real_anagrams(perms)))
-
-    # Now get this in the proper form
-    real_anagrams = real_anagrams.map(lambda seq, anas: (seq, len(anas), anas))
-
-
+    with open("P3.txt", 'w') as out_file:
+        print >> out_file, max_finder[1], max_finder[0], max_finder[2]
