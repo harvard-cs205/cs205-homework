d9017010e239aabcbb865f4db4d87562e18ae5a9
ytang14 <tang01@g.harvard.edu>
2015-09-25 04:02:25 -0400

    finished P2

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,17 @@
 from P2 import *
+import findspark
+findspark.init()
+import pyspark
+from random import randint
+sc = pyspark.SparkContext(appName="Spark1")
+x = sc.parallelize(xrange(2000),10)
+y = sc.parallelize(xrange(2000),10)
+rdd = x.cartesian(x).repartition(100)
 
-# Your code here
+ans = rdd.map(lambda x: [x,mandelbrot((x[0]/500.0-2),(x[1]/500.0-2))])
+draw_image(ans)
+
+ans1 = sum_values_for_partitions(ans)
+
+plt.hist(ans1.collect())
+plt.savefig('P2b_hist.png')
