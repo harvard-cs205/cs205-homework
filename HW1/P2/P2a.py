8fa88c523312151de0ba1117e7e7f00143400070
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-09-25 14:29:28 -0400

    did the basics of question 2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,22 @@
 from P2 import *
 
 # Your code here
+import pyspark
+sc = pyspark.SparkContext(appName="Spark1")
+import numpy as np 
+
+
+xs = sc.parallelize(range(2000))
+grid = xs.cartesian(xs)
+#grid.take(10)
+xys = grid.map(lambda (i, j): ((i, j), (j/500.0 - 2, i/500.0 - 2))).repartition(100)
+reses = xys.mapValues(lambda (x, y): mandelbrot(x, y))
+#print xys.take(10)
+#print reses.take(10)
+#draw_image(reses)
+distribution = sum_values_for_partitions(reses).collect()
+plt.figure()
+plt.hist(distribution)
+plt.show()
+
+
