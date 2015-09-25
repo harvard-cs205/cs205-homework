9dcc50e4ded7765617a4580bb9646bbf3ca13aa5
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2015-09-25 14:47:56 -0400

    finished problem 2 part a

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -18,22 +18,34 @@ config = config.setAppName('P2a')
 
 sc = ps.SparkContext(conf=config)
 
+#creating function for creating the pixels and passing on a tuple and the results
 def createPixels(row,column):
     xx = (column/500.0)-2.0
     yy = (row/500.0)-2.0
     result = mandelbrot(xx,yy)
     return (row,column),result
 
+# this creates an RDD that constains values in the range of 0 to 2000 stepping 
+# in increments of 1
 row = sc.range(0,2000)
 column = sc.range(0,2000)
 
+# this creates all possible combinations of rows and columns
 joined = row.cartesian(column)
+
+# this maps the joined RDD into the function createPixels
 mandlebrot = joined.map(lambda rr: createPixels(rr[0],rr[1]))
+
+# here I pass the RDD in the format ((I,J),V) to the pre-defined draw function
 draw_image(mandlebrot)
 plt.show()
 
+# here I pass the same RDD in the same format to find how many times it was 
+# iterated upon
 sum_values=sum_values_for_partitions(mandlebrot).collect()
 
+# this section gives me the histogram in a pretty fashion. This was done with
+# the help from Bryan Weinstein
 plt.hist(sum_values, bins=np.logspace(3, 8))
 plt.gca().set_xscale('log')
 plt.savefig('P2a_hist.png', bbox_inches='tight')
