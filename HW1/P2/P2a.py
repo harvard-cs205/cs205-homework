c381bed3c758d2d7e08298149f0d84c4759a30dc
Luong Hoang <lhoang@g.harvard.edu>
2015-09-25 19:52:32 -0400

    finished P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -2,17 +2,26 @@ from P2 import *
 
 
 def compute_image(s, p):
-
+    """Compute Mandelbrot image using default partitioning strategy.
+    :param s: The size of the square image to compute.
+    :param p: The square root of the number of partitions.
+    """
     c = s / 4.0
-    ii = sc.parallelize([(i, i/c-2) for i in xrange(s)], p)
-    jj = sc.parallelize([(j, j/c-2) for j in xrange(s)], p)
+    ii = sc.parallelize([(i, i / c - 2) for i in xrange(s)], p)
+    jj = sc.parallelize([(j, j / c - 2) for j in xrange(s)], p)
 
-    image = ii.cartesian(jj).map(lambda (k, v):((v[0], k[0]),mandelbrot(k[1], v[1])))
-    # Your code here
+    image = ii.cartesian(jj).map(
+        lambda (k, v): ((v[0], k[0]), mandelbrot(k[1], v[1])))
+
     draw_image(image)
 
+    # create nice evenly spaced bins
+    b = [i - 0.5 for i in range(p * p + 1)]
+    b[0] = 0
+    b[p * p] = p * p
+
     partition_work = sum_values_for_partitions(image).collect()
-    plt.hist(range(p * p), weights=partition_work, bins=[i-0.5 for i in range(p*p+1)])
+    plt.hist(range(p * p), weights=partition_work, bins=b)
 
     plt.show()
 
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,29 @@
 from P2 import *
 
-# Your code here
+
+def compute_image_balanced(s, p):
+    """Compute Mandelbrot image using balanced partitioning strategy.
+    :param s: The size of the square image to compute.
+    :param p: The square root of the number of partitions.
+    """
+    c = s / 4.0
+
+    ij = sc.parallelize([(ind / s, ind % s) for ind in sorted(
+        xrange(s * s), key=lambda k: random.random())], p * p)
+
+    image = ij.map(
+        lambda (i, j): ((i, j), mandelbrot(j / c - 2, i / c - 2)))
+
+    draw_image(image)
+
+    # create nice evenly spaced bins
+    b = [i - 0.5 for i in range(p * p + 1)]
+    b[0] = 0
+    b[p * p] = p * p
+
+    partition_work = sum_values_for_partitions(image).collect()
+    plt.hist(range(p * p), weights=partition_work, bins=b)
+
+    plt.show()
+
+    return image
