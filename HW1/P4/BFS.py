25c994164007d52ba03a9cfda65db8f3f0e8a616
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-09-26 12:21:56 -0400

    Think I have a working class. Kind of a hack, but it works ok.

--- HW1/P4/BFS.py
+++ HW1/P4/BFS.py
@@ -19,13 +19,25 @@ class BFS(object):
         self.network_rdd = network_rdd
         self.cur_iteration = 0
         self.collected_distance_rdd = None
+        self.initialize_distances()
 
-        # Finish initialization
-        self.collected_distance_rdd = BFS.initialize_distances(self.start_node, self.network_rdd, 0)
-        self.cur_iteration = 1
 
+    def initialize_distances(self):
+        self.collected_distance_rdd = BFS.initialize_distances_static(self.start_node,
+                                                                      self.network_rdd,
+                                                                      self.cur_iteration)
+        self.cur_iteration += 1
+
+    def do_iteration(self):
+        self.collected_distance_rdd = BFS.do_iteration_static(self.sc,
+                                                              self.network_rdd,
+                                                              self.collected_distance_rdd,
+                                                              self.cur_iteration)
+        self.cur_iteration += 1
+
+    #### STATIC METHODS TO INTERACT WITH SPARK ####
     @staticmethod
-    def initialize_distances(start_node, network_rdd, cur_iteration):
+    def initialize_distances_static(start_node, network_rdd, cur_iteration):
         network_to_touch = network_rdd.filter(lambda x: x[0] == start_node)
         distance_rdd = network_to_touch.map(lambda x: (x[0], cur_iteration))
         collected_distance_rdd = distance_rdd.collect()
@@ -33,23 +45,22 @@ class BFS(object):
         return collected_distance_rdd
 
     @staticmethod
-    def do_iteration(self):
+    def do_iteration_static(sc, network_rdd, collected_distance_rdd, cur_iteration):
         # Pull the needed info out of the network
-        already_touched = [z[0] for z in self.collected_distance_rdd]
+        already_touched = [z[0] for z in collected_distance_rdd]
         already_touched_set = set(already_touched)
-        broadcasted_touched = self.sc.broadcast(already_touched_set)
-        network_to_touch = self.network_rdd.filter(lambda x: x[0] in broadcasted_touched.value)
+        broadcasted_touched = sc.broadcast(already_touched_set)
+        network_to_touch = network_rdd.filter(lambda x: x[0] in broadcasted_touched.value)
         broadcasted_touched.unpersist()
 
-        old_distance_rdd = self.sc.parallelize(self.collected_distance_rdd)
+        old_distance_rdd = sc.parallelize(collected_distance_rdd)
 
         # Now do the iteration!
         nodes_to_touch = network_to_touch.flatMap(lambda x: x[1])
         unique_nodes_to_touch = nodes_to_touch.distinct()
-        updated_touched_nodes = unique_nodes_to_touch.map(lambda x: (x, self.cur_iteration))
+        updated_touched_nodes = unique_nodes_to_touch.map(lambda x: (x, cur_iteration))
         updated_distance_rdd = old_distance_rdd.union(updated_touched_nodes)
         corrected_distance_rdd = updated_distance_rdd.reduceByKey(get_smaller_value)
 
-        self.collected_distance_rdd = corrected_distance_rdd.collect()
-
-        self.cur_iteration += 1
+        collected_distance_rdd = corrected_distance_rdd.collect()
+        return collected_distance_rdd
