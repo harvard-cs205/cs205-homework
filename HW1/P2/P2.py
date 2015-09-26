eb485a0384f611e1e2a185f04e7b493b71d75213
Jeffrey Shen <shenjeffrey@gmail.com>
2015-09-26 10:35:38 -0400

    Edit p2

--- HW1/P2/P2.py
+++ HW1/P2/P2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
+import findspark; findspark.init()
+import pyspark
 
 def mandelbrot(x, y):
     z = c = complex(x, y)
