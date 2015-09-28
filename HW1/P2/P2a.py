1d0e996bf07a501c97d27e1e2caaa2d93b900ea4
Leonhard Spiegelberg <spiegelberg@g.harvard.edu>
2015-09-28 16:07:32 -0400

    initial computation for mandelbrot

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,37 @@
-from P2 import *
+import findspark
+findspark.init()
 
-# Your code here
+from P2 import *
+from pyspark import SparkContext, SparkConf
+
+# setup spark
+conf = SparkConf().setAppName('Mandelbrot')
+sc = SparkContext(conf=conf)
+
+# create the space
+nx, ny = (2000, 2000)
+x = np.linspace(-2, 2, nx)
+y = np.linspace(-2, 2, ny)
+
+xv, yv = np.meshgrid(x, y)
+
+# compute coordinate list
+coords = zip(list(xv.ravel()), list(yv.ravel()))
+
+# create rdd 
+rdd = sc.parallelize(coords)
+
+# now map using mandelbrot
+mb = lambda c: mandelbrot(c[0], c[1])
+
+rdd = rdd.map(mb)
+
+# collect data & print image
+data = rdd.collect()
+
+# plotting
+im = np.array(data).reshape((nx, ny))
+im = np.log(im + 1) # log intensity makes it easier to see levels
+plt.imshow(im, cmap=cm.gray)
+plt.savefig('mandelbrot.png')
+plt.show()
