ea37d97d331a55f1cf0430fa9708b867a2675669
Avery Faller <avyk37@gmail.com>
2015-09-28 14:11:59 -0400

    P2 work

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,18 @@
 from P2 import *
 
 # Your code here
+import findspark
+findspark.init()
+from pyspark import SparkContext, SparkConf
+sc = SparkContext()
+
+allxys = []
+
+for x in range(0, 2000):
+    for y in range(0, 2000):
+        allxys.append((y, x))
+        
+rdd1 = sc.parallelize(allxys, 100).map(lambda xy: ((xy), mandelbrot((xy[1]/500.)-2,(xy[0]/500.)-2)))
+
+draw_image(rdd1)
+plt.hist(sum_values_for_partitions(rdd1).collect())
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,19 @@
 from P2 import *
 
 # Your code here
+import findspark
+findspark.init()
+from pyspark import SparkContext, SparkConf
+sc = SparkContext()
+import random
+
+allxys = []
+
+for x in range(0, 2000):
+    for y in range(0, 2000):
+        allxys.append((y, x))
+        
+rdd1 = sc.parallelize(allxys).map(lambda xy: ((xy), mandelbrot((xy[1]/500.)-2,(xy[0]/500.)-2))).partitionBy(100, lambda xy: random.randrange(0,100,1))
+
+draw_image(rdd1)
+plt.hist(sum_values_for_partitions(rdd1).collect())
