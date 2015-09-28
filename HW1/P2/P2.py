208fc711926a8b0ad099aeff65906582656cfa71
Anna Whitney <awhitney@college.harvard.edu>
2015-09-28 16:31:03 -0400

    working on HW1 P2

--- HW1/P2/P2.py
+++ HW1/P2/P2.py
@@ -1,6 +1,8 @@
+from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pyspark
 
 def mandelbrot(x, y):
     z = c = complex(x, y)
@@ -29,3 +31,14 @@ def draw_image(rdd):
     im[I, J] = np.log(C + 1)  # log intensity makes it easier to see levels
     plt.imshow(im, cmap=cm.gray)
     plt.show()
+
+sc = pyspark.SparkContext(appName='Spark1')
+
+# create RDD of pixel location tuples
+i_vals = sc.parallelize(range(2000), 10)
+ij_vals = i_vals.cartesian(i_vals)
+
+# associate mandelbrot values with each pixel
+ijxy_vals = ij_vals.zip(ij_vals).mapValues(lambda (i,j): (j/500 - 2, i/500 - 2))
+mandel_vals = ijxy_vals.mapValues(lambda xy: mandelbrot(*xy))
+draw_image(mandel_vals)
