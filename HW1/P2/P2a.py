20f196b3718d1aafa5eedb1f4af12945f88b0cbe
haosutang <haosutang@gmail.com>
2015-09-27 20:47:44 -0400

    finished coding part of P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,16 @@
 from P2 import *
 
 # Your code here
+import pyspark
+import numpy as np
+
+sc = pyspark.SparkContext(appName='myAppName')
+
+i = sc.parallelize(xrange(1,2001), 10)
+j = sc.parallelize(xrange(1,2001), 10)
+
+coordinates = i.cartesian(j)
+
+intensity = coordinates.map(lambda x: [x, mandelbrot(x[1]/500.0-2, x[0]/500.0-2)])
+
+draw_image(intensity)
