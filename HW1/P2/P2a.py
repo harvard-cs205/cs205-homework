d64ca29bae9ab301e34f64b5928a899cd6b4d9ce
Hyunkwang Lee <hyunkwang@Hyunkwangs-MacBook-Air.local>
2015-09-28 23:44:13 -0400

    Intermediate commit after P2a.py and P2b.py done. not yet for my discussion of results in P2.txt

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,25 @@
+# Initialize SC context
+import findspark
+findspark.init()
+import pyspark
+sc = pyspark.SparkContext(appName="Spark 1")
 from P2 import *
 
 # Your code here
+if __name__ == '__main__':
+  # Set up Initial RDDs
+  rdd = sc.parallelize(range(2000),10)
+  rdd1 = sc.parallelize(range(2000),10)
+  rdd2 = rdd1.cartesian(rdd)
+  # Generate RDD for (K,V) = ((i,j),mandelbrot())
+  rdd3 = rdd2.map(lambda (i,j): ((i,j), mandelbrot((j/500.0)-2,(i/500.0)-2)))
+  # Draw a mandelbrot plot
+  rdd4 = draw_image(rdd3)
+  # Save a sequence of per-partition efforts
+  rdd5 = sum_values_for_partitions(rdd3)
+  seq_of_efforts = rdd5.collect()
+  # Plot a histrogram
+  #ind = np.arange(len(seq_of_efforts))
+  #plt.bar(ind+0.2,seq_of_efforts,0.6)
+  plt.hist(seq_of_efforts)
+  plt.savefig("P2a_hist.png")
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,26 @@
+# Initialize SC context
+import findspark
+findspark.init()
+import pyspark
+sc = pyspark.SparkContext(appName="Spark 1")
 from P2 import *
 
 # Your code here
+if __name__ == '__main__':
+  # Set up Initial RDDs
+  rdd = sc.parallelize(range(2000),10)
+  rdd1 = sc.parallelize(range(2000),10)
+  rdd2 = rdd1.cartesian(rdd)
+  # Generate RDD for (K,V) = ((i,j),mandelbrot())
+  rdd2_new = rdd2.partitionBy(100)
+  rdd3_new = rdd2_new.map(lambda (i,j): ((i,j), mandelbrot((j/500.0)-2,(i/500.0)-2)))
+  # Draw a mandelbrot plot
+  rdd4_new = draw_image(rdd3_new)
+  # Save a sequence of per-partition efforts
+  rdd5_new = sum_values_for_partitions(rdd3_new)
+  seq_of_efforts_new = rdd5_new.collect()
+  # Plot a histrogram
+  #ind = np.arange(len(seq_of_efforts_new))
+  #plt.bar(ind+0.2,seq_of_efforts_new,0.6)
+  plt.hist(seq_of_efforts_new)
+  plt.savefig("P2b_hist.png")
