ece064558a4bbb6c69c3d4a9436879a3f8fe3fc0
1Reinier <reiniermaat@msn.com>
2015-09-28 22:21:34 -0400

    Started P6

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -1,5 +1,6 @@
 from P4_bfs import *
 
+
 if __name__ == "__main__":
     N = 16  # Number of partitions
 
@@ -12,6 +13,7 @@ if __name__ == "__main__":
 
     # Generate edges between vertices:
     edges_rdd = tuple_rdd.join(tuple_rdd, numPartitions=N).map(lambda (k, v): v, True)  # preserve only edge pairs
+    edges_rdd = edges_rdd.union(edges_rdd.map(lambda (k, v): (v, k), True)  # undirected graph
     edges_rdd = edges_rdd.distinct(N).filter(lambda (hero1, hero2): hero1 != hero2)  # filter duplicates, links to self
 
     # Now group by character for easy retrieval:
@@ -19,5 +21,8 @@ if __name__ == "__main__":
     graph.cache()
 
     # Distance to nodes in network
-    distance_to_character = distance_to_all_nodes_from("CAPTAIN AMERICA", graph).sortBy(lambda (k, v): v, False)
-    print distance_to_character.take(20)
+    print "DONE BUILDING GRAPH. STARTING BFS..."
+    # distance_to_all_nodes_serial("ORWELL", graph).collect()  # SERIAL VERSION
+    distance_to_character2 = new("ORWELL", graph).collect()
+    print "DONE"
+    print distance_to_character2# , distance_to_character2
--- HW1/P4/P4_bfs.py
+++ HW1/P4/P4_bfs.py
@@ -1,10 +1,41 @@
 import findspark
 findspark.init()
 import pyspark
-import numpy as np
 
 
-def distance_to_all_nodes_from(root_node, graph_rdd):
+# parallel implementation
+def distance_to_all_nodes_spark(root_node, graph_rdd):
+    graph = graph_rdd.map(lambda (k, v): (k, -1, v))  # set all distances to -1
+    checklist = {root_node}  # initial node
+    for iteration in xrange(11):
+        graph = graph.map(lambda (k, n, v): (k, n, v) if (n > -1 or k not in checklist) else (k, iteration, []))
+        neighbors = graph.filter(lambda (k, n, v): n == iteration)
+        if not neighbors.isEmpty():
+            checklist = set(neighbors.map(lambda (k, n, v): v).reduce(lambda a, b: a + b if b else a))
+        else:
+            break  # No neighbors left, done with search
+    graph = graph.map(lambda (k, n, v): (k, n))
+    return graph
+
+
+def new(root_node, graph_rdd):
+    graph = graph_rdd.map(lambda (k, v): (k, -1, v))  # set all distances to -1
+    graph.cache()
+    neighbors = graph.context.parallelize(graph_rdd.lookup(root_node))  # initial node
+    iteration = graph_rdd.context.accumulator(0)
+    while not neighbors.isEmpty():
+        checklist = neighbors.collect()[0]
+        graph = graph.map(lambda (k, n, v): (k, n, v) if (n > -1 or k not in checklist) else (k, iteration, []))
+        print graph.count
+        neighbors = graph.filter(lambda (k, n, v): n == iteration).map(lambda (k, n, v): v)
+        print neighbors.count
+        iteration += 1
+    graph = graph.map(lambda (k, n, v): (k, n))
+    return graph
+
+
+# serial implementation
+def distance_to_all_nodes_serial(root_node, graph_rdd):
     result = {}
     current_nodes = [root_node]
     for iteration in xrange(11):  # max 10 iterations
@@ -17,4 +48,9 @@ def distance_to_all_nodes_from(root_node, graph_rdd):
     print len(result)  # number of nodes touched on
     result_rdd = graph_rdd.keys().map(lambda key: (key, -1) if key not in result else (key, result[key]), True)
     return result_rdd  # -1 means no connection (within 10 steps)
+
+
+
+
+
 
