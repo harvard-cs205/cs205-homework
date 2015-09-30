e2ba3345d260c3924192a1dee5f3994fd742334b
fqian <fqian91@gmail.com>
2015-09-30 00:05:03 -0400

    Finished P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,18 @@
 from P2 import *
+import findspark
+findspark.init()
+import os
+import pyspark
+sc = pyspark.SparkContext()
 
-# Your code here
+# define axis rdd with 10 partitions to prepare for a square matrix with 100 patitions
+axisrdd = sc.parallelize(range(2000),10)
+pixelrdd = axisrdd.cartesian(axisrdd)
+
+# apply the mandelbrot function and draw image
+drawrdd = pixelrdd.map(lambda pixel: [pixel, mandelbrot(pixel[1]/500.0-2, pixel[0]/500.0-2)])
+draw_image(drawrdd)
+
+# plot effort with histogram
+plt.hist(sum_values_for_partitions(drawrdd).collect())
+plt.savefig('P2a_hist.png')
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,21 @@
 from P2 import *
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.cm as cm
 
-# Your code here
+import numpy as np
+
+# define shuffled axis: to ensure randomness, the axes are defined separately
+axis1 = range(2000)
+np.random.shuffle(axis1)
+axis2 = range(2000)
+np.random.shuffle(axis2)
+randomaxis1 = sc.parallelize(axis1,10)
+randomaxis2 = sc.parallelize(axis2,10)
+randomrdd = randomaxis1.cartesian(randomaxis2)
+
+# other parts of the code remains the same
+drawrandomrdd = randomrdd.map(lambda pixel: [pixel, mandelbrot(pixel[1]/500.0-2, pixel[0]/500.0-2)])
+draw_image(drawrandomrdd)
+plt.hist(sum_values_for_partitions(drawrandomrdd).collect())
+plt.savefig('P2b_hist.png')
