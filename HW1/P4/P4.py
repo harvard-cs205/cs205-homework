864ed11cbb5b4989f0d5d24ad4e3b21a83f4e483
1Reinier <reiniermaat@msn.com>
2015-09-29 20:50:12 -0400

    P6 done

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     # Generate edges between vertices:
     edges_rdd = tuple_rdd.join(tuple_rdd, numPartitions=N).map(lambda (k, v): v, True)  # preserve only edge pairs
-    edges_rdd = edges_rdd.union(edges_rdd.map(lambda (k, v): (v, k), True)  # undirected graph
+    edges_rdd = edges_rdd.union(edges_rdd.map(lambda (k, v): (v, k), True))  # undirected graph
     edges_rdd = edges_rdd.distinct(N).filter(lambda (hero1, hero2): hero1 != hero2)  # filter duplicates, links to self
 
     # Now group by character for easy retrieval:
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     # Distance to nodes in network
     print "DONE BUILDING GRAPH. STARTING BFS..."
     # distance_to_all_nodes_serial("ORWELL", graph).collect()  # SERIAL VERSION
-    distance_to_character2 = new("ORWELL", graph).collect()
+    distance_to_character2 = distance_to_all_nodes_spark("ORWELL", graph).collect()
     print "DONE"
     print distance_to_character2# , distance_to_character2
--- HW1/P6/P6.py
+++ HW1/P6/P6.py
@@ -1,10 +1,40 @@
 import findspark
 findspark.init()
 import pyspark
+import numpy as np
 
 
-# helper method to check word validity
+def make_sentence(triplet_rdd):
+    """Builds sentence from dataset randomly"""
+    sentence = []
+    # sentence_length = int(np.round(np.random.normal(24.46, 3.85)))  # Average sentence English length according to [1]
+    sentence_length = 20
+    sample = triplet_rdd.takeSample(True, 1)[0]  # Random first words
+    two_words = sample[0]
+    third_word_list = sample[1]
+    sentence += [two_words[0].title()]  # Captalizes first letter
+    sentence += [two_words[1]]
+    while len(sentence) < sentence_length:
+        # Get and store third word
+        if len(third_word_list) > 1:
+            weighted_third_word_list = reduce(lambda l, (word, n): l + ([word] * n), third_word_list, [])
+            third_word = np.random.choice(weighted_third_word_list)
+        else:
+            third_word = third_word_list[0][0]
+        sentence += [third_word]
+        # Update step
+        two_words = (two_words[1], third_word)
+        third_word_list = triplet_rdd.map(lambda x: x).lookup(two_words)[0]
+        if not third_word_list:
+            break  # if no (two, three) combination is found, end sentence prematurely (very unlikely)
+    sentence = ' '.join(sentence)
+    if sentence[-1] != '.':
+        sentence += '.'  # End sentence nicely with a dot.
+    return sentence
+
+
 def check_word(word):
+    """Helper method to check word validity"""
     try:
         int(word)
         return False
@@ -22,14 +52,15 @@ if __name__ == "__main__":
 
     sc = pyspark.SparkContext("local[4]")
     text_rdd = sc.textFile('shakespeare.txt', N, False)
-
     text_rdd = text_rdd.flatMap(lambda sentence: sentence.split(' '))
     text_rdd = text_rdd.filter(check_word)
 
+    print text_rdd.lookup('lala')
+
     # build word lists with index
     first_rdd = text_rdd.zipWithIndex().map(lambda (k, v): (v, k))
-    second_rdd = first_rdd.map(lambda (k, v): (k - 1, v))
-    third_rdd = second_rdd.map(lambda (k, v): (k - 1, v))
+    second_rdd = first_rdd.map(lambda (i, v): (i - 1, v))
+    third_rdd = second_rdd.map(lambda (i, v): (i - 1, v))
 
     # join and drop index
     word_combo_rdd = first_rdd.join(second_rdd, N).join(third_rdd, N).map(lambda (i, ((w1, w2), w3)): ((w1, w2, w3), 1))
@@ -38,4 +69,12 @@ if __name__ == "__main__":
     # redefine keys and group by word pairs
     result = word_combo_rdd.map(lambda ((w1, w2, w3), n): ((w1, w2), (w3, n))).groupByKey().mapValues(lambda l: list(l))
 
-    print result.takeOrdered(10, key=lambda (k, v): -len(v))
+    sentences = []
+    for i in xrange(10):
+        sentences += [make_sentence(result)]
+
+    for sentence in sentences:  # to make sure they show up after the PySpark output
+        print sentence
+
+# [1] http://hearle.nahoo.net/Academic/Maths/Sentence.html
+
