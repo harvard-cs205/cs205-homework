2ba01ec9195ab9f33c598202e10ce31604888a30
Elena-Zhao <zhaoqing513@126.com>
2015-09-30 21:52:29 -0400

    HW 2-3 finished

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,29 @@
 from P2 import *
 
+import findspark
+findspark.find()
+findspark.init(edit_profile=True)
+import pyspark
+sc = pyspark.SparkContext()
+
+def mandelbrot(x, y):
+    z = c = complex(x, y)
+    iteration = 0
+    max_iteration = 511  # arbitrary cutoff
+    while abs(z) < 2 and iteration < max_iteration:
+        z = z * z + c
+        iteration += 1
+    return iteration
+
 # Your code here
+def form():
+    iteration_count = []
+    for i in range(20):
+        for j in range(20):
+            x = j/5 - 2
+            y = i/5 - 2
+            iteration_count.append(mandelbrot(x, y))
+    return iteration_count
+
+
+rdd = sc.parallelize()
