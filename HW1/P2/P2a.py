32979e79005f3b8c7148fd96a01bc8b89462d51e
Charles Liu <therealcharlesliu@gmail.com>
2015-09-30 22:24:04 -0400

    P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,13 @@
 from P2 import *
+from pyspark import SparkContext
 
 # Your code here
+sc = SparkContext("local", "P2a")
+rdd = sc.parallelize(xrange(0, 2000), 10)
+cross = rdd.cartesian(rdd)
+map_mandelbrot = cross.map(lambda x: (x,mandelbrot(x[0]/500.0 - 2, x[1]/500.0 - 2)))
+sum_partitions = sum_values_for_partitions(map_mandelbrot)
+#print sum_partitions.take(100)
+#draw_image(map_mandelbrot)
+sum_array = sum_partitions.collect()
+plt.hist(sum_array)
