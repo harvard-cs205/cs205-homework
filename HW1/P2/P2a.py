492bdd52a3cb633d98af5e50c4752a505be97af0
Zelong Qiu <zelong430@gmail.com>
2015-09-30 20:09:10 -0400

    P2 script, image done

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,25 @@
 from P2 import *
 
 # Your code here
+
+import findspark
+findspark.init('/home/zelong/spark')
+import pyspark
+
+sc = pyspark.SparkContext()
+i = range(2000)
+j = range(2000)
+
+rdd_i = sc.parallelize(i, 10)
+rdd_j = sc.parallelize(j, 10)
+
+rdd_ij = rdd_i.cartesian(rdd_j)
+rdd_iter = rdd_ij.map(lambda j: (j, mandelbrot ( (j[1]/500.0 ) - 2 , (j[0]/500.0) - 2)))
+
+rdd_sum = sum_values_for_partitions(rdd_iter)
+temp = rdd_sum.collect()
+
+#draw_image(rdd_iter)
+plt.hist(temp)
+plt.savefig('P2a_hist')
+
