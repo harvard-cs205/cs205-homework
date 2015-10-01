6cfd68935687d91aeb5a57ab3bd4f97f4fbc6159
Andrew Petschek <petschek@g.harvard.edu>
2015-10-01 19:03:22 -0400

    P3 commit

---
+++ HW1/P3/P3.py
@@ -0,0 +1,22 @@
+import pyspark
+from pyspark import SparkContext
+sc = SparkContext("local[4]")
+import numpy as np
+sc.setLogLevel("ERROR")
+
+#import words and then split up the letters in each word and alphabetize them
+words = sc.textFile('words.txt')
+wordcharacters = words.map(lambda word: np.sort(list(word)))
+
+#now combine letters back together using join function. 
+SortedLetterSequence = wordcharacters.map(lambda x: "".join(x))
+
+#make two columns of our alphabetized words and group by key to get anagrams
+
+keyvaluerdd = words.map(lambda x: ("".join(np.sort(list(x))),x))
+temp = keyvaluerdd.groupByKey().mapValues(list)
+Anagrams = temp.map(lambda x: (x[0],(len(x[1]),x[1])))
+
+#find maximum where we know max=11, so use filter.
+maximum = Anagrams.filter(lambda x: x[1][0] == 11)
+print maximum.collect()
