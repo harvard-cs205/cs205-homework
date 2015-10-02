6d94b0b8733a83d71aead2d7a50a5395fe504416
Yj Cheng <cheng.yungjen@gmail.com>
2015-10-01 20:45:47 -0400

    add accumulator, discussion

--- HW1/P4/P4_bfs.py
+++ HW1/P4/P4_bfs.py
@@ -1,25 +1,35 @@
 from pyspark import SparkContext
+from pyspark import AccumulatorParam
+
+class setAccPar(AccumulatorParam):
+    def zero(self, initialValue):
+        return initialValue
+
+    def addInPlace(self, s1, s2):
+        s1 |= s2
+        return s1
+
 
 def rdd_bfs(sc, char_adjacent, root):
-    touched = {root}
-    this_level = touched.copy()
-    next_level = {}
-    for i in xrange(10):
+    touched = sc.accumulator( {root}, setAccPar() )
+    this_level = {root}
+    i = 0 # distance
+    while(True):
+        i += 1
+        next_level = sc.accumulator( set(), setAccPar() )    
         next_level_rdd = char_adjacent.filter(lambda p: p[0] in this_level) #select the nodes in this level
-        next_level = set.union(* next_level_rdd.values().collect() ) #select the neighbors of the nodes in this level
-        next_level = next_level - touched #remove visited nodes
-        touched = touched | next_level #include the nodes in next level in touched set
-##            print i,"===================================="
-##            print this_level
-##            print next_level
-        if len(next_level) == 0: #if all neighbors were visited, end this loop
+
+        #next_level = set.union(* next_level_rdd.values().collect() ) #select the neighbors of the nodes in this level
+        next_level_rdd.values().foreach(lambda x: next_level.add(x)) #select the neighbors of the nodes in this level
+##        print next_level.value
+##        print touched.value
+##        print next_level.value - touched.value
+        this_level = next_level.value - touched.value #remove visited nodes
+        next_level_rdd.values().foreach(lambda x: touched.add(x)) #add in touched nodes
+        #touched = touched | next_level #include the nodes in next level in touched set
+        if len(this_level) == 0: #if all neighbors were visited, end this loop
             print "no new node!"
             print root, "diameter:", i
             break
-        this_level = next_level.copy()
-        next_level.clear()
-    print root, "reachable characters:", len(touched)
+    print root, "reachable characters:", len(touched.value)
 
-##    a = char_adjacent.collect()
-##    for i in xrange(5):
-##        print a[i]
