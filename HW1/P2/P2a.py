7dee520b155dbba60a79c11ee60ca19429c045ae
jonahkall <kallenbach@college.harvard.edu>
2015-10-02 17:23:50 -0400

    work on problems 2, 3, 4

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,22 @@
 from P2 import *
 
-# Your code here
+import numpy as np
+
+import findspark
+findspark.init()
+
+import pyspark
+sc = pyspark.SparkContext(appName="P2")
+
+pixels = np.zeros((2000, 2000))
+print pixels
+
+parallel_pix = sc.parallelize(pixels, 100)
+
+#print parallel_pix.take(10)
+
+computed = parallel_pix.map(lambda a: mandelbrot(a[0],a[1]))
+
+print len(computed.collect())
+
+#draw_image(computed)
