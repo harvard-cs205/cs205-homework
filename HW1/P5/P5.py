20e7bdceb783170e896f8ba89be27f175a3614aa
Luong Hoang <lhoang@g.harvard.edu>
2015-10-03 00:49:59 -0400

    some optimizations

--- HW1/P5/P5.py
+++ HW1/P5/P5.py
@@ -8,39 +8,14 @@ default_distance = 99999
 sum_distance = sc.accumulator(0)
 
 
-def quiet_logs(sc):
-    logger = sc._jvm.org.apache.log4j
-    logger.LogManager.getLogger("org").setLevel(logger.Level.WARN)
-    logger.LogManager.getLogger("akka").setLevel(logger.Level.WARN)
-    logger.LogManager.getLogger("amazonaws").setLevel(logger.Level.WARN)
+def copartitioned(RDD1, RDD2):
+    "check if two RDDs are copartitioned"
+    return RDD1.partitioner == RDD2.partitioner
 
 
-def create_graph(fileName, nameFile):
-    """
-    Create a graph of character relationship given a file.
-    :param fileName: The csv file to load where each line = (hero, comic).
-    """
-    src = sc.textFile(fileName)
-
-    # make links symmetric by mapping (hero_i, hero_j) to (hero_j, hero_i)
-    # then reduce to give a list of (hero_i, [neighbors of hero_i])
-    edges = src.map(lambda x: x.split(':')).map(
-        lambda kv: (int(kv[0]), [int(v) for v in kv[1].split()])).partitionBy(
-        partition_size).cache()
-
-    # create list of nodes with initial distance = 99
-    nodes = edges.mapValues(lambda v: (default_distance, ''))
-
-    names = sc.textFile(nameFile).zipWithIndex().map(
-        lambda kv: (kv[1] + 1, kv[0])).partitionBy(
-            partition_size).cache()
-
-    return nodes, edges, names
-
-
-def l_add_distance(kv, d):
-    for v in kv[1][0]:
-        yield (v, (d, kv[1][1][1] + ' ' + repr(kv[0])))
+def l_add_distance(ids_distance_path, d):
+    for page_id in ids_distance_path[0]:
+        yield (page_id, (d, ids_distance_path[1][1] + ' ' + repr(page_id)))
 
 
 def update_accumulator(d):
@@ -56,16 +31,53 @@ def l_min_distance(v1, v2):
         return v2
 
 
+def quiet_logs(sc):
+    logger = sc._jvm.org.apache.log4j
+    logger.LogManager.getLogger("org").setLevel(logger.Level.WARN)
+    logger.LogManager.getLogger("akka").setLevel(logger.Level.WARN)
+    logger.LogManager.getLogger("amazonaws").setLevel(logger.Level.WARN)
+
+
+def create_graph(fileName, nameFile):
+    """
+    Create a graph of character relationship given a file.
+    :param fileName: The csv file to load where each
+    line = (page: [page, ...]).
+    """
+    src = sc.textFile(fileName)
+
+    # make links symmetric by mapping (page_i, page_j) to (page_j, page_i)
+    # then reduce to give a list of (page_i, [neighbors of page_i])
+    edges = src.map(lambda x: x.split(':')).map(
+        lambda kv: (int(kv[0]), [int(v) for v in kv[1].split()])).partitionBy(
+        partition_size).cache()
+
+    # create list of nodes with initial distance = 99
+    # [ (id, (distance, 'path')) ]
+    nodes = edges.map(lambda kv: (kv[0], (
+        default_distance, repr(kv[0]))), preservesPartitioning=True).cache()
+
+    # RDD mapping from 1-based index to page names
+    # [ (index, name) ]
+    names = sc.textFile(nameFile).zipWithIndex().map(
+        lambda kv: (kv[1] + 1, kv[0])).partitionBy(
+            partition_size).cache()
+
+    return nodes, edges, names
+
+
 def bfs_search(nodes, edges, names, source, target):
     """
     Perform breadth-first search to find shortest path from source.
     :param source: The root node to find paths from.
     """
-    # initially root nodes only contains the source node
-    # but will grow to contain more nodes at each iteration
+    # get the source node to find path from
+    # [ (index, (distance, 'path')) ]
     root = names.filter(lambda kv: kv[1] == source).join(
-        nodes).mapValues(lambda kv: kv[1])
+        nodes).mapValues(lambda kv: kv[1]).cache()
 
+    # get the target node to find path to
+    # [ (index, name) ]
     end = names.filter(lambda kv: kv[1] == target).cache()
 
     i = 1
@@ -73,15 +85,20 @@ def bfs_search(nodes, edges, names, source, target):
     while (True):
         begin_sum_distance = sum_distance.value
 
-        # get neighbors and make sure they are copartitioned with edges & nodes
-        # since each partition may contain duplicate keys, we use mapPartitions
-        # to eliminate these without causing a shuffle.
-        neighbors = edges.join(root).flatMap(
-            lambda kv: l_add_distance(kv, i)).distinct().partitionBy(
-              partition_size)
+        assert copartitioned(root, edges)
 
-        nodes = neighbors.union(nodes).reduceByKey(l_min_distance)
+        # [ ([page_ids], (distance, 'path')) ]
+        neighbors = edges.join(root).values().flatMap(
+            lambda kv: l_add_distance(kv, i))
 
+        distinct_neighbors = neighbors.reduceByKey(
+            lambda v1, v2: v1, numPartitions=edges.getNumPartitions())
+
+        assert copartitioned(distinct_neighbors, nodes)
+        nodes = distinct_neighbors.union(nodes).reduceByKey(
+            l_min_distance).cache()
+
+        assert copartitioned(end, nodes)
         # stop if target has been found
         if end.join(nodes).filter(
                 lambda kv: kv[1][0] < default_distance).isEmpty() == False:
@@ -95,11 +112,12 @@ def bfs_search(nodes, edges, names, source, target):
             print 'Converged after ' + repr(i) + ' iterations'
             break
 
-        root = neighbors
+        root = distinct_neighbors.cache()
         i += 1
         previous_sum_distance = sum_distance.value - begin_sum_distance
 
-    root_end_path = end.join(nodes)
+    assert copartitioned(end, nodes)
+    root_end_path = end.join(nodes).cache()
 
     return nodes, edges, root_end_path
 
@@ -110,8 +128,14 @@ def bfs(links_file, titles_file, root, target):
     nodes, edges, names = create_graph(links_file, titles_file)
     _, _, root_end_path = bfs_search(nodes, edges, names, root, target)
 
-    path = root_end_path.flatMap(lambda kv: kv[1][1][1].split()).map(
-        lambda v: (int(v), 0)).join(names).map(lambda kv: kv[1][1]).collect()
+    # get the path containing page ids
+    # [ (id, 0) ]
+    path_page_ids = root_end_path.values().flatMap(
+        lambda kv: kv[1][1].split()).map(
+        lambda v: (int(v), 0)).partitionBy(names.getNumPartitions())
+
+    assert copartitioned(path_page_ids, names)
+    path = path_page_ids.join(names).map(lambda kv: kv[1][1]).collect()
 
     print '-------- FOUND PATH --------'
     print path
@@ -119,6 +143,8 @@ def bfs(links_file, titles_file, root, target):
     print 'TIME ' + repr(time.time() - start)
 
 quiet_logs(sc)
+
+# bfs('links.smp', 'titles.smp', 'stress', 'harvard')
 
 bfs('s3://Harvard-CS205/wikipedia/links-simple-sorted.txt',
     's3://Harvard-CS205/wikipedia/titles-sorted.txt',
