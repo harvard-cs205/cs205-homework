bdc86ec4ead9f5f7ed6f53f25f801d5a77c66853
Feifei <fpeng01@g.harvard.edu>
2015-10-03 13:15:46 -0400

    P2 files added

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,15 @@
 from P2 import *
 
 # Your code here
+import pyspark
+from pyspark import SparkContext, SparkConf
+sc = SparkContext()
+
+x = sc.parallelize(xrange(1,2001,1), 10)
+coord = x.cartesian(x)
+rdd = ( coord.map( lambda s: [s, mandelbrot(s[1]/500.0-2, s[0]/500.0-2)] ) ).persist()
+
+
+draw_image(rdd)
+plt.hist( sum_values_for_partitions(rdd).collect() )
+plt.savefig('P2a_hist.png')
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,23 @@
 from P2 import *
 
 # Your code here
+import pyspark
+from pyspark import SparkContext, SparkConf
+import random
+sc = SparkContext()
+
+my_x = range(0,2000,1)
+my_y = range(0,2000,1)
+random.shuffle(my_x)
+random.shuffle(my_y)
+
+x = sc.parallelize(my_x, 10)
+y = sc.parallelize(my_y, 10)
+coord = x.cartesian(x)
+rdd = ( coord.map( lambda s: [s, mandelbrot(s[1]/500.0-2, s[0]/500.0-2)] ) ).persist()
+
+
+#draw_image(rdd)
+plt.hist( sum_values_for_partitions(rdd).collect() )
+plt.savefig('P2b_hist.png')
+
