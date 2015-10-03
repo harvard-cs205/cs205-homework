8a63f88349b774c53d9ae137e4e0c7082d5c802f
Charles Liu <therealcharlesliu@gmail.com>
2015-10-03 11:37:06 -0400

    remove collect

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -11,21 +11,22 @@ def add_to_path(node, acc, index1, index2):
     new_paths = []
     last_node, pair = node
     neighbors, curr_path = pair
+    neighbors = neighbors[0]
     for neighbor in neighbors:
         new_paths.append((neighbor, curr_path + [neighbor]))
         if index1==curr_path[0] and index2 == neighbor:
             acc.add(1)
     return new_paths
 
-def shortest_path(index1, index2, links_rdd):
+def shortest_path(index1, index2, links_rdd, numPartitions = 64):
     solutions_found = links_rdd.context.accumulator(0)
-    nodes_to_search = set([index1])
-    path_rdd = links_rdd.filter(lambda x: x[0] in nodes_to_search).map(lambda x: (x[0], [x[0]]))
+    nodes_to_search = links_rdd.filter(lambda x: x[0] == index1).partitionBy(numPartitions)
+    path_rdd = nodes_to_search.map(lambda x: (x[0], [x[0]])).partitionBy(numPartitions)
     solution_rdd = path_rdd
     while solutions_found.value == 0:
-        join_rdd = links_rdd.filter(lambda x: x[0] in nodes_to_search).join(path_rdd)
-        path_rdd = join_rdd.flatMap(lambda x: add_to_path(x, solutions_found, index1, index2))
-        nodes_to_search = set(path_rdd.map(lambda x: x[0]).collect())
+        join_rdd = links_rdd.join(nodes_to_search).join(path_rdd)
+        path_rdd = join_rdd.flatMap(lambda x: add_to_path(x, solutions_found, index1, index2)).partitionBy(numPartitions)
+        nodes_to_search = path_rdd.map(lambda x: x[0]).distinct().map(lambda x: (x, [])).partitionBy(numPartitions)
         solution_rdd = path_rdd.filter(lambda x: x[0] == index2 and x[1][0] == index1).map(lambda x: x[1]).collect()
     return solution_rdd
 
@@ -39,11 +40,14 @@ def get_names_path(paths, rdd):
 
 if __name__ == '__main__':
     sc = SparkContext(appName="P5")
+
+    partitions = 64
     links = sc.textFile('s3://Harvard-CS205/wikipedia/links-simple-sorted.txt')
     titles = sc.textFile('s3://Harvard-CS205/wikipedia/titles-sorted.txt')
-    index_titles = titles.zipWithIndex().map(lambda x: (x[1] + 1, x[0])).cache()
-    links_kv = links.map(split_link).cache()
-    harvard_index, bacon_index = title_index('Harvard_University', 'Kevin_Bacon', index_titles)
-    print get_names_path(shortest_path(harvard_index, bacon_index, links_kv), index_titles)
-    print get_names_path(shortest_path(bacon_index, harvard_index, links_kv), index_titles)
 
+    index_titles = titles.zipWithIndex().map(lambda x: (x[1] + 1, x[0])).cache()
+    links_kv = links.map(split_link).partitionBy(partitions).cache()
+    harvard_index, bacon_index = title_index('Harvard_University', 'Kevin_Bacon', index_titles)
+
+    print get_names_path(shortest_path(harvard_index, bacon_index, links_kv, partitions), index_titles)
+    print get_names_path(shortest_path(bacon_index, harvard_index, links_kv, partitions), index_titles)
