157088c2af5d1a8c2e996b235deb25daaf9a2f27
Andrew Petschek <petschek@g.harvard.edu>
2015-10-03 14:50:59 -0400

    added accumulator

--- HW1/P4/P4_bfs.py
+++ HW1/P4/P4_bfs.py
@@ -4,13 +4,22 @@ def bfs(SOURCE,Graph,sc):
     final_rdd1 = node1
     i = 0
     
-    while i < 10:        
+    accumulator = sc.accumulator(0)
+    i = 0
+    while accumulator.value == i:        
         #find neighbors of input node        
         node1 = Graph.join(node1).distinct().values().partitionBy(8) 
         
         #new node only includes neighbors not already searched/in Final RDD        
         node1 = node1.mapValues(lambda x: x+1).subtractByKey(final_rdd1)
-
+        
+        #if node1 not empty, then increment accumulator to align with i
+        #otherwise accumulator stays the same and while loop ends        
+        if node1.isEmpty():
+            pass
+        else:
+            accumulator.add(1)
+        
         #Add neighbors searched (and their distance value) to the final RDD        
         final_rdd1 = final_rdd1.union(node1).cache()
         i+=1    
