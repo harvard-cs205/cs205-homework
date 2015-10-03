fc15c7fef43e4f53bb4f615317a701f20e9db04b
Andrew Mauboussin <ammaub@college.harvard.edu>
2015-10-02 22:12:07 -0400

    worked on first 3 problems

---
+++ HW1/P3/P3.py
@@ -0,0 +1,17 @@
+import findspark
+findspark.init()
+import pyspark
+sc = pyspark.SparkContext(appName='mandelbrot')
+
+with open('EOWL_words.txt', 'r') as rfile:
+	words = rfile.read().split('\n')
+
+lex_order = lambda s: ''.join(sorted(list(s)))
+
+words_rdd = sc.parallelize(words, 10)
+lex_to_word = words_rdd.map(lambda w: (lex_order(w), w))
+lex_to_wordlist = lex_to_word.groupByKey().mapValues(list)
+result_rdd = lex_to_wordlist.map(lambda (lex, wordlist) : (lex, len(wordlist), wordlist))
+
+max_entry = result_rdd.max(key = lambda (lex, n, wordlist): n)
+print max_entry
