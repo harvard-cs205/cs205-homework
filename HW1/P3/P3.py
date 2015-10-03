8bca4f7f9af802976551509505642deb6d4bf9a6
Kai Sheng <ksheng@Kais-MacBook-Pro.local>
2015-10-03 17:43:20 -0400

    add brief comments

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -8,9 +8,9 @@ sc = SparkContext(conf = conf)
 if __name__ == '__main__':
     wordlist = sc.textFile(wordfile)
     
-    letterize = lambda word: ''.join(sorted(list(word)))
+    letterize = lambda word: ''.join(sorted(list(word)))  # value as sorted word
     word_letter = wordlist.map(lambda w: (letterize(w), w))
 
-    word_join = word_letter.groupByKey().mapValues(list)
-    word_final = word_join.map(lambda w: (w[0], len(w[1]), w[1]))
+    word_join = word_letter.groupByKey().mapValues(list)  # group by key and get the list of the same sorted word
+    word_final = word_join.map(lambda w: (w[0], len(w[1]), w[1]))  # the final format
     print word_final.takeOrdered(1, key = lambda w: -w[1])
