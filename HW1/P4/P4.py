8ab18d8d027ce173ff7f4ea670bdad9661442f2b
ChristianJunge <christianjunge@g.harvard.edu>
2015-10-03 22:13:45 -0400

    Just short of implementing the accumulator in 4

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -2,24 +2,47 @@ import findspark
 findspark.init()
 import pyspark
 import itertools
-
+from P4_bfs import BFS
 
 if __name__ == "__main__":  
     sc = pyspark.SparkContext()
 
-    # Load the lines of the text file and switch the order of the hero, issue so that I can use
-    #    the issue as the key
-    source = sc.textFile("source.csv").map(lambda x: x.split(',')).map(lambda x: (x[1], x[0]))
+    # Load the lines of the text file 
+    source = sc.textFile("source.csv")
 
-    # Group by issue, and create tuples of (hero, all other heros in that issue) for that issue.
-    #     FlatMap to take all the tuples out of being in lists by issue.  
-    source1 = source.groupByKey().mapValues(lambda heros: [(hero1, [hero2 for hero2 in heros if hero2 != hero1]) for hero1 in heros]).values().flatMap(lambda x: x)
+    # Split the issue id from the superhero's name
+    source = source.map(lambda x: x.split('","'))
+
+    # Trim the extra quotation marks and reverse the order so that the issue ID is used as a key
+    source = source.map(lambda (x,y): (y[:-1],x[1:]))
+
+    # Group by issue, and create tuples of (hero, all other heros in that issue) for that issue.  
+    source = source.groupByKey().mapValues(lambda heros: [(hero1, [hero2 for hero2 in heros if hero2 != hero1]) for hero1 in heros])
+
+    # FlatMap to take all the tuples out of being in lists by issue.
+    source = source.flatMap(lambda (x,y): y)
+
 
     # Define a function to take all the lists that are returned by reduceByKey and flatten them,
     #    then use list(set()) to take only unique values
     def func(*args): return list(set(itertools.chain(*args)))
 
     # Group by each superhero as a key, and take only unique edges
-    source2 = source1.reduceByKey(func)
+    source = source.reduceByKey(func)
+    
+    # Partition and cache the source
+    source = source.partitionBy(4).cache()
 
-    ###def source(): return source2
+    # Verify that Captain America has 1933 edges... but this returns 1906.  Good enough for now
+    # print len(source.lookup(u'CAPTAIN AMERICA')[0])    
+    
+    # Verify that there are 6449 nodes... but this returns 6444.  Good enough for now
+    # print len(source.collect())
+
+    # Call the BFS function to demonstrate that it works
+    res1 = BFS(sc, source, u'CAPTAIN AMERICA').values().collect()
+    res2 = BFS(sc, source, u'MISS THING/MARY').values().collect()
+    res3 = BFS(sc, source, u'ORWELL').values().collect()
+    print("Captain America is connected to " + str(len(res1)) + " nodes, with a maximum distance of " + str(max(res1)) + ".")
+    print("Miss Thing/Mary is connected to " + str(len(res2)) + " nodes, with a maximum distance of " + str(max(res2)) + ".")
+    print("Orwell is connected to " + str(len(res3)) + " nodes, with a maximum distance of " + str(max(res3)) + ".")
--- HW1/P4/P4_bfs.py
+++ HW1/P4/P4_bfs.py
@@ -1,11 +1,50 @@
-import findspark
-findspark.init()
-import pyspark
-import itertools
-from P4 import *
+#import findspark
+#findspark.init()
+#import pyspark
+#import itertools
+#from P4 import *
 
 
-if __name__ == "__main__":  
-    sc = pyspark.SparkContext()
+# as I understand it, this file should be just a function that is called in P4.py
+
+def BFS(sc, graph, name):
     
-    print source().lookup("LANN")
+    # Rdd of name, shortest distance tuples that will be returned.  In each iteration, heros are added to this.  Initialize with the source node, and partition into 4 partitions to match the graph.
+    rdd = sc.parallelize([(name, 0)], 4)
+    
+    # This RDD is used intermediately to build a collection of connections to build on
+    namesRdd = rdd
+
+    # Iterator used in the while loop, capped at 10
+    i = 0
+    
+    while (i < 10) and not namesRdd.isEmpty(): # Terminate if nothing was added in the last iteration
+    
+        i = i+1    
+    
+        # Look up the connections to the last added and take a flat map of just the connections
+        #namesRdd = graph.filter(lambda (x,y): x in names).flatMap(lambda (x,y): y)
+ 
+        # Use join to look up the connections of the next round of connections
+        namesRdd = graph.join(namesRdd)
+
+        # Use mapValues to take just the list of connections and remove the distance
+        namesRdd = namesRdd.mapValues(lambda (x,y): x)
+
+        # Take the connections and flatMap them into one big pool to consider
+        namesRdd = namesRdd.values().flatMap(lambda x: x)
+
+        # Reduce the connections to distinct entries
+        namesRdd = namesRdd.distinct()
+        
+        # Subtract out the names that are already in the rdd
+        namesRdd = namesRdd.subtract(rdd.keys())
+
+        # create tuples of the connection name and the current distance of the iteration
+        namesRdd = namesRdd.map(lambda x: (x, i)).partitionBy(4)  
+
+        # Union the connection tuples to the existing list, and repartition
+        rdd = rdd.union(namesRdd).partitionBy(4)
+    
+    return rdd
+        
