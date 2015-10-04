2d4658122f2ae7dc5d568ea9e8f1ef68ca67c9a0
Virgile Audi <vaudi@g.harvard.edu>
2015-10-04 14:35:13 -0400

    P2

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,26 @@
 from P2 import *
+from  pyspark import SparkContext
+import time
 
-# Your code here
+sc = SparkContext()
+sc.setLogLevel("ERROR")
+
+#Creating the pixel rdd
+pix2 = sc.parallelize(np.random.permutation(range(1,2001,1)),10)
+pix3 = sc.parallelize(np.random.permutation(range(1,2001,1)),10)
+pixels2 =  pix2.cartesian(pix3)
+
+xy_rdd2 = pixels2.map(lambda p : (p,((p[1]/500.0)-2,(p[0]/500.0)-2)))
+print xy_rdd2.getNumPartitions()
+mandel_rdd2 = xy_rdd2.mapValues(lambda val : mandelbrot(val[0],val[1]))
+
+#draw_image(mandel_rdd2)
+st = time.time()
+to_plot = sum_values_for_partitions(mandel_rdd2).collect()
+end = time.time()-st
+
+print end 
+# Around 50s
+plt.hist(to_plot,bins=20)
+plt.show()
+
