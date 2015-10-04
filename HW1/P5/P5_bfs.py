df402358afe7d0678619a8b9c13cafc549e1fdbb
Andrew Petschek <petschek@g.harvard.edu>
2015-10-04 16:17:29 -0400

    P5 update

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -1,38 +1,51 @@
-
-
-
 #NOTE: File P5.py should be used to call this function below to find
 #shortest path between two nodes
 
-
 import numpy as np
 
+def copartitioned(RDD1, RDD2):
+    "check if two RDDs are copartitioned"
+    return RDD1.partitioner == RDD2.partitioner
+    
+sequence = []    
 def bfs(SOURCE,Graph,sc,DEST=False):
     node1 = [(SOURCE,0)]
-    node1 = sc.parallelize(node1).partitionBy(32)
-    final_rdd1 = node1
-    
-    #while not node1.isEmpty():       
-        
-        #node1 = Graph.join(node1).distinct().values().partitionBy(16)       
-        #node1 = node1.mapValues(lambda x: x+1).subtractByKey(final_rdd1)        
-        #final_rdd1 = final_rdd1.union(node1).partitionBy(16).cache()
-    
-    #final_rdd_g1 = final_rdd1.groupByKey().partitionBy(16).mapValues(list).cache()
-    
-    #if DEST:
-        #return np.min(final_rdd_g1.lookup(DEST)), final_rdd_g1.count()
-    #else:
-        #return final_rdd_g1.count()
-
+    node1 = sc.parallelize(node1).partitionBy(128)
+    final_rdd1 = node1    
+    sequence.append(SOURCE) 
     at_destination = False
     while not at_destination:
-        node1 = Graph.join(node1).partitionBy(32)
-        node1 = node1.distinct().values().mapValues(lambda x: x+1)
-        node1 = node1.subtractByKey(final_rdd1)        
-        final_rdd1 = final_rdd1.union(node1).partitionBy(32).cache()
+        
+        node1 = Graph.join(node1)
+        assert copartitioned(node1, Graph)        
+        
+        node1 = node1.distinct().values().mapValues(lambda x: x+1).partitionBy(128)
+        
+        node2 = node1.subtractByKey(final_rdd1)                   
+        assert copartitioned(node2,node1)
+        
+        node1 = node2
+        
+        final_rdd2 = final_rdd1.union(node1)
+        assert copartitioned(final_rdd2,final_rdd1)
+        
+        final_rdd1 = final_rdd2
+        final_rdd1.cache()
+        
         if final_rdd1.lookup(DEST):
            at_destination = True
-    final_rdd_g1 = final_rdd1.groupByKey().mapValues(list).partitionBy(32)
-    return np.min(final_rdd_g1.lookup(DEST))
     
+    final_rdd_g1 = final_rdd1.groupByKey().mapValues(list).cache()
+    
+    #to get sequence from Kevin Bacon to Harvard
+    distance = np.min(final_rdd_g1.lookup(DEST))
+    
+    RDD1 = final_rdd1.filter(lambda (K,V): V == (distance - 1)).partitionBy(128)
+    RDD2 = Graph.join(RDD1)
+    RDD3 = RDD2.filter(lambda (K,V): V[0] == DEST)
+    RDD4 = RDD3.keys()
+
+    sequence.append(RDD4.collect())
+    sequence.append(DEST)
+    
+    return np.min(final_rdd_g1.lookup(DEST)), sequence
--- HW1/P5/P5_connected_components.py
+++ HW1/P5/P5_connected_components.py
@@ -1,31 +1,98 @@
 
 #NOTE: File P5.py should be used to call this function below to find
-#shortest path between two nodes
-
-
+#connected components
 import numpy as np
+
+def copartitioned(RDD1, RDD2):
+    "check if two RDDs are copartitioned"
+    return RDD1.partitioner == RDD2.partitioner
 
 def connected_components(SOURCE,Graph,sc,):
     node1 = [(SOURCE,0)]
-    node1 = sc.parallelize(node1).partitionBy(32)
+    node1 = sc.parallelize(node1).partitionBy(128)
     final_rdd1 = node1
-    components = 1
+    Components = []
+    Components_sym = []
+    Graph2 = Graph.map(lambda (K,V): (V,K))
+    Graph_sym = Graph.union(Graph2).distinct().groupByKey().partitionBy(128).cache()
+    
     #run this while there are always new neighbors to search for
     
     while Graph.subtractByKey(final_rdd1) != 0:
         #find all neighbors and conitnue iterating until no new neighbors found
         while not node1.isEmpty():       
-            node1 = Graph.join(node1).distinct().values().partitionBy(32)       
-            node1 = node1.mapValues(lambda x: x+1).subtractByKey(final_rdd1)        
-            final_rdd1 = final_rdd1.union(node1).partitionBy(32).cache()
+            
+            node1 = Graph.join(node1)
+            
+            assert copartitioned(node1, Graph)   
+            
+            node1 = node1.distinct().values().partitionBy(128)
+            
+            node2 = node1.subtractByKey(final_rdd1)
+             
+            assert copartitioned(node2, node1)   
+            
+            node1 = node2
+                 
+            final_rdd2 = final_rdd1.union(node1)
+            assert copartitioned(final_rdd2, final_rdd1)   
+            
+            final_rdd1=final_rdd2
+            final_rdd1.cache()
+            
         #check to see if characters from graph  not accounted for
         remaining_components = Graph.SubtractByKey(final_rdd1)
-        components +=1
-        SOURCE = remaining_components.takeSample(False,1)[0]
-        node1 = [(SOURCE,0)]
-        node1 = sc.parallelize(node1).partitionBy(32)
-    return components 
+        
+        assert copartitioned(remaining_components,Graph) 
+        
+        Components = Components.append(final_rdd1.groupByKey().count())
 
- 
+        SOURCE = remaining_components.takeSample(False,1)[0]
+        
+        node1 = [(SOURCE,0)]
+        node1 = sc.parallelize(node1).partitionBy(128).cache()
+
+
+
+    final_rdd1_sym = node1
+    node1_sym = node1
+    while Graph_sym.subtractByKey(final_rdd1_sym) != 0:
+        #find all neighbors and conitnue iterating until no new neighbors found
+        while not node1_sym.isEmpty():       
+            
+            node1_sym = Graph_sym.join(node1_sym)
+            
+            assert copartitioned(node1_sym, Graph_sym)   
+            
+            node1_sym = node1.distinct().values().partitionBy(128)
+            
+            node2_sym = node1_sym.subtractByKey(final_rdd1_sym)
+             
+            assert copartitioned(node2_sym, node1_sym)   
+            
+            node1_sym = node2_sym
+                 
+            final_rdd2_sym = final_rdd1_sym.union(node1_sym)
+            assert copartitioned(final_rdd2_sym, final_rdd1_sym)   
+            
+            final_rdd1_sym =final_rdd2_sym
+            final_rdd1_sym.cache()
+            
+            
+        #check to see if characters from graph  not accounted for
+        remaining_components_sym = Graph_sym.SubtractByKey(final_rdd1_sym)
+        
+        assert copartitioned(remaining_components_sym,Graph_sym) 
+        
+        Components_sym = Components_sym.append(final_rdd1_sym.groupByKey().count())
+
+        SOURCE = remaining_components_sym.takeSample(False,1)[0]
+        
+        node1_sym = [(SOURCE,0)]
+        node1_sym = sc.parallelize(node1_sym).partitionBy(128).cache()
+
+    return len(Components), max(Components), len(Components_sym), max(Components_sym)     
+    
+   
 
   
