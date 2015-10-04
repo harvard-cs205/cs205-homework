ca72a2ae356fcbff912e5d6ef3c3648c01d9b56a
Abhishek Malali <abhishekmalali@gmail.com>
2015-10-04 19:58:51 -0400

    Adding answers for P2 HW1

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,18 @@
 from P2 import *
+import findspark
+import matplotlib.pyplot as plt
+findspark.init()
+import pyspark
+sc = pyspark.SparkContext()
 
 # Your code here
+if __name__ == "__main__":
+    init_rdd = sc.parallelize(range(0,2000),10)
+    mand_rdd = init_rdd.cartesian(init_rdd)
+    mand_rdd = mand_rdd.map(lambda x: (x,mandelbrot((float(x[1])/500)-2, (float(x[0])/500)-2)))
+    partition_val = sum_values_for_partitions(mand_rdd).collect()
+    plt.hist(partition_val)
+    plt.xlabel("No. of Computations")
+    plt.ylabel("No. of Partitions")
+    plt.title("Histogram for distribution of computations over partitions")
+    plt.savefig("P2a.png")
