7e7f551441fb46747e14c08aab040e2e175158a8
JEEWON HWANG <jeewonhwang@g.harvard.edu>
2015-10-04 17:44:42 -0400

    P2b modified

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -16,7 +16,7 @@ rdd_j = sc.parallelize(list_j, 10)
 product_rdd = rdd_i.cartesian(rdd_j)
 mandelbrot_rdd = product_rdd.map(lambda (i, j): ((i, j), mandelbrot(j/500.0 - 2, i/500.0 - 2)))
 
-#draw_image(mandelbrot_rdd)
+draw_image(mandelbrot_rdd)
 
 sum_rdd = sum_values_for_partitions(mandelbrot_rdd)
 plt.hist(sum_rdd.collect())
