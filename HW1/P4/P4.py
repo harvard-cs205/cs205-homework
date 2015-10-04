980a6581c5c2953f2c978183911b9caccabeac90
Stephen Fang <stephenshifang@gmail.com>
2015-10-03 23:19:58 -0400

    Finished P4

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -1,11 +1,13 @@
+from P4_bfs import *
 from pyspark import SparkContext
 from urllib2 import urlopen
 import os
 
 if __name__=='__main__':
+    NPART = 16
+    characters = [u'CAPTAIN AMERICA', u'ORWELL', u'MISS THING/MARY']
     # initialize spark
     sc = SparkContext()
-    sc.setLogLevel('ERROR')
 
     # save text file
     datafile = 'source.csv'
@@ -15,11 +17,36 @@ if __name__=='__main__':
         with open(datafile, 'wb') as f:
             f.write(urlopen(url).read())
 
-    # load data into a rdd (issue character)
+    # load data into a rdd (issue, character)
     data = sc.textFile(datafile).map(
-        lambda line: tuple([w.strip('"').strip() for w in 
+        lambda line: tuple([w.strip('"') for w in 
             line.split('","')])[::-1])
 
+    # self join to create edges
+    edges = data.join(data)
+
+    # remove duplicates, i.e. character with itself
+    edges = edges.values()
+    edges = edges.filter(lambda (chr1, chr2): chr1 != chr2)
+
+    # create graph rdd (character, {all linked characters})
+    graph = edges.groupByKey().mapValues(set).repartition(NPART)
+    graph.cache()
+
+    # serial implemention
+    # print 'Serial implemention'
+    # for root in characters:
+    #     num_traversed = bfs_serial(graph, root)
+    #     print root
+    #     print 'Nodes touched:', num_traversed
+
+    # parallel implementation
+    print 'Parallel implemention'
+    for root in characters:
+        bfs_parallel(sc, graph, root)
+        num_traversed = bfs_parallel(sc, graph, root)
+        print root
+        print 'Nodes touched:', num_traversed
+        print
 
 
-    # print data.takeSample(False, 20)
---
+++ HW1/P4/P4_bfs.py
@@ -0,0 +1,48 @@
+from pyspark import SparkContext, AccumulatorParam
+
+def bfs_serial(graph, root, iteration=10):
+    '''
+    Serial implementation of BFS
+    Assuming diameter is 10
+    '''
+    paths = {}
+    current = {root}
+    for step in range(0, iteration+1):
+        next = set()
+        filtered = graph.filter(lambda (k, v): k in current).cache()
+        for n in current:
+            if n not in paths:
+                paths[n] = step
+                next |= filtered.lookup(n)[0]
+        current = next - set(paths.keys())
+
+    # return results
+    numtraversed = len(paths) - 1 # excl. root
+    return numtraversed
+
+class AccumNodes(AccumulatorParam):
+    def addInPlace(self, value1, value2):
+        value1 |= value2
+        return value1
+
+    def zero(self, value):
+        return value
+
+def bfs_parallel(sc, graph, root):
+    '''
+    Parallel implementation
+    '''
+    traversed = set()
+    current = {root}
+    step = 0
+
+    while current:
+        filtered = graph.filter(lambda (k, v): k in current)
+        traversed |= current
+        counter = sc.accumulator(set(), AccumNodes())
+        filtered.values().foreach(lambda x: counter.add(x))
+        current = counter.value - traversed
+        step += 1
+
+    numtraversed = len(traversed) - 1 # excl. root
+    return numtraversed
