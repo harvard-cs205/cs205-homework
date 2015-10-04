7e503c4ea71e69def09369bb0ba92a05d337d674
Chengchengqin <Chengchengqin@g.harvard.edu>
2015-10-04 17:47:46 -0400

    Modified Version

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -5,4 +5,4 @@ rdd=sc.textFile("/home/memory/cs205-homework/HW1/P3/EOWL_words.txt")
 
 final=rdd.flatMap(lambda x: x.split()).map(lambda x: (''.join(sorted(list(x))),x)).groupByKey().mapValues(list).map(lambda x: (x[0],len(x[1]),x[1]))
 
-print final.takeOrdered(1, lambda x: -x[1])
+print final.takeOrdered(5, lambda x: -x[1])
