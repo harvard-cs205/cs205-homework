dd2a63378b2cdb7c90b5c0b0556698186e1889b5
Chengchengqin <Chengchengqin@g.harvard.edu>
2015-10-04 19:00:53 -0400

    This is HW1/P2

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,17 @@
+from pyspark import SparkContext
+import random
 from P2 import *
 
-# Your code here
+sc = SparkContext()
+x=range(2000)
+random.shuffle(x)
+y=range(2000)
+random.shuffle(y)
+xx=sc.parallelize(x,10)
+yy=sc.parallelize(x,10)
+final=xx.cartesian(yy)
+rdd=final.map(lambda r: [r,mandelbrot(r[1]/500.0-2,r[0]/500.0-2)])
+
+val=sum_values_for_partitions(rdd)
+plt.hist(val.collect())
+plt.savefig('P2b_hist.png')
