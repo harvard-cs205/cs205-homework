789507a2ea069abe7e3fe4c5fd632ad16a0f134e
Vivek Jayaram <vivekjayaram@college.harvard.edu>
2015-10-04 18:36:28 -0400

    Done

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -6,20 +6,20 @@ sc = SparkContext("local", "Simple App")
 [2729536, 3554817, 3554854, 2152782]
 "National_Lampoon's_Animal_House"
 'National_Lampoon_(magazine)'
-graph = sc.parallelize([(1,(0, None, [2,3])), (2, (float("inf"), None, [5,3])), (3, (float("inf"), None, [5,1,10])), (10, (float("inf"), None, [2])), (5, (float("inf"), None, []))])
 
+# Testing the algorithm
+graph = sc.parallelize([(1,(0, None, [2,3])), (2, (float("inf"), None, [5,3])), (3, (float("inf"), None, [5,1,10])), (10, (float("inf"), None, [2])), (5, (float("inf"), None, []))])
 print search_bfs_new(1,10, graph, sc)
 
-#links = sc.textFile('s3://Harvard-CS205/wikipedia/links-simple-sorted.txt', 32)
+links = sc.textFile('s3://Harvard-CS205/wikipedia/links-simple-sorted.txt', 32)
 neighbor_graph = links.map(construct_node(2152782))
 neighbor_graph = neighbor_graph.partitionBy(256).cache()
 page_names = sc.textFile('s3://Harvard-CS205/wikipedia/titles-sorted.txt', 32)
 page_names = page_names.zipWithIndex().map(lambda (n, id) : (id+1, n))
 page_names = page_names.sortByKey().cache()
 
-
-# page_names.filter(lambda x: x == "Harvard_University").take(1)
 def construct_node(start):
+	# Needs to take into account the start node. 
 	def helper(line):
 		src, dests = line.split(': ')
 		dests = [int(to) for to in dests.split(' ')]
@@ -29,22 +29,19 @@ def construct_node(start):
 			return (int(src), (float("inf"), None, dests))
 	return helper
 
-# def find_index(page_names, input):
-# 	i = 1
-# 	while True:
-# 		if page_names[i-1] == input:
-# 			print i
-# 			break
-# 		i += 1
+def find_index(page_names, input):
+	i = 1
+	while True:
+		if page_names[i-1] == input:
+			print i
+			break
+		i += 1
 
 def get_name(page_names, index):
 	return page_names[index-1]
 
-# # links = links.map(lambda x: ((int(x.split()[0][:-1])), (x.split()[1:])))
-# harvard = links.filter(lambda x: (int(x.split()[0][:-1]) == 2152782))
-
-# Harvard = find_index(my_list, "Harvard_University") #2152782
-# Kevin_Bacon = find_index(my_list, "Kevin_Bacon") #2729536
+Harvard = find_index(my_list, "Harvard_University") #2152782
+Kevin_Bacon = find_index(my_list, "Kevin_Bacon") #2729536
 
 print search_bfs_new(2152782, 2729536, neighbor_graph, sc)
 
