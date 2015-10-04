c6976f442080c721350c50f2a48abe057cce4d70
Andrew Petschek <petschek@g.harvard.edu>
2015-10-04 17:21:41 -0400

    p5

--- HW1/P5/P5_connected_components.py
+++ HW1/P5/P5_connected_components.py
@@ -11,15 +11,13 @@ def copartitioned(RDD1, RDD2):
 def connected_components(SOURCE,Graph,sc,):
     node1 = [(SOURCE,0)]
     node1 = sc.parallelize(node1).partitionBy(128)
-    final_rdd1 = node1
-    Components = []
     Components_sym = []
     Graph2 = Graph.map(lambda (K,V): (V,K))
     Graph_sym = Graph.union(Graph2).distinct().groupByKey().partitionBy(128).cache()
-    
-    #run this while there are always new neighbors to search for
     final_rdd1_sym = node1
     node1_sym = node1
+    
+    #run this while there are always new neighbors to search for    
     while Graph_sym.subtractByKey(final_rdd1_sym) != 0:
         #find all neighbors and conitnue iterating until no new neighbors found
         while not node1_sym.isEmpty():       
@@ -40,8 +38,7 @@ def connected_components(SOURCE,Graph,sc,):
             assert copartitioned(final_rdd2_sym, final_rdd1_sym)   
             
             final_rdd1_sym =final_rdd2_sym
-            final_rdd1_sym.cache()
-            
+            final_rdd1_sym.cache()            
             
         #check to see if characters from graph  not accounted for
         remaining_components_sym = Graph_sym.SubtractByKey(final_rdd1_sym)
