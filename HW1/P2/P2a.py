4009d0af4d35d325e6826edb02ddad92534dd5a5
ytang14 <tang01@g.harvard.edu>
2015-10-04 01:04:38 -0400

    finished P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -5,7 +5,7 @@ import pyspark
 sc = pyspark.SparkContext(appName="Spark1")
 my_rdd = sc.parallelize(xrange(2000),10)
 rdd = my_rdd.cartesian(my_rdd)
-ans = rdd.map(lambda x: [x,mandelbrot((x[0]/500.0-2),(x[1]/500.0-2))])
+ans = rdd.map(lambda x: [x,mandelbrot((x[1]/500.0-2),(x[0]/500.0-2))])
 draw_image(ans)
 
 ans1 = sum_values_for_partitions(ans)
