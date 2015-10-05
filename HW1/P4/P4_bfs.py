81a8c33a90dfa01371c0eb11b82ee578377d2b19
Taylor Killian <taylorkillian@g.harvard.edu>
2015-10-04 21:04:16 -0400

    Added accumulator to P4_bfs.py

--- HW1/P4/P4_bfs.py
+++ HW1/P4/P4_bfs.py
@@ -3,14 +3,19 @@ from itertools import chain
 
 def P4_bfs(grph,source,sc):
 	
+	LoopBreaker = sc.accumulator(0)
+	
 	nodes = sc.parallelize([(source,0)])
 	visitedNodes = sc.parallelize([(source,0)])
 	i_dist = 0
-	while not nodes.isEmpty():
+	while LoopBreaker.value == 0:
+		i_dist+=1
 		nodes = grph.join(nodes).values().distinct().partitionBy(8)
 		nodes = nodes.mapValues(lambda v: v+1).subtractByKey(visitedNodes)
+		if nodes.isEmpty():
+			LoopBreaker.add(1)
+			continue
 		visitedNodes = visitedNodes.union(nodes).cache()
-		i_dist+=1
 		
 	print "Exhausted graph at a distance of:", i_dist
 	print "With "+source+" as your source, you touched "+ str(visitedNodes.count()) + " nodes."
