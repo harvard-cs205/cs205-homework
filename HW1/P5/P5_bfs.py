e3485970361377dea0ce4eec8ded291c7db8c30d
Anna Whitney <awhitney@college.harvard.edu>
2015-10-05 14:31:43 -0400

    P5 working on AWS

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -12,7 +12,7 @@ def get_by_key(k, rdd_dict):
     return rdd_dict.lookup(k)[0]
 
 def to_touch_untouched(to_touch):
-    return (lambda (k,(prev,ns)): k in to_touch)
+    return (lambda (k,ns): k in to_touch)
 
 def bfs_path(sc, graph, origin, destination):
     '''
@@ -30,35 +30,36 @@ def bfs_path(sc, graph, origin, destination):
     ttrdd = sc.parallelize([(origin, None)])
     touched = sc.emptyRDD()
 
-    # all nodes start with no previous node
-    graph_prevs = graph.mapValues(lambda v: (None, v))
-
     # accumulator to keep track of whether we found destination
     found = sc.accumulator(0)
 
     # keep going until we find our destination or run out of graph
     while not ttrdd.isEmpty() and found.value == 0:
+        print 'hi from {0} to {1}'.format(str(origin),str(destination))
 
         # find untouched nodes from our list of nodes to touch
-        ttns = graph_prevs.filter(to_touch_untouched(to_touch))
+        ttns = graph.filter(to_touch_untouched(to_touch))
 
         # update set of neighbors to touch and check if we're at destination
         def neighbors_and_check_dest(k, ns):
             if k == destination:
                 found.add(1)
             return [(n,k) for n in ns]
-        ttrdd = ttns.flatMap(lambda (k,(p,ns)): neighbors_and_check_dest(k,ns)).subtractByKey(touched)
+        ttrdd = ttns.flatMap(lambda x: neighbors_and_check_dest(*x)).subtractByKey(touched)
         to_touch = set(ttrdd.keys().collect())
 
         # set previous nodes
-        graph_prevs = graph_prevs.leftOuterJoin(ttrdd, 8).mapValues(lambda ((p,ns),prev): (prev,ns) if prev else (p,ns)).cache()
         touched = touched.union(ttrdd)
 
     # trace destination path backwards
     path = [destination]
     current = destination
-    while current and current != origin:
-        current = touched.lookup(current)[0]
+    while current != origin:
+        lst = touched.lookup(current)
+        if lst:
+            current = lst[0]
+        else:
+            break
         path.append(current)
 
-    return path[::-1] if current else None
+    return path[::-1] if current and current != destination else None
