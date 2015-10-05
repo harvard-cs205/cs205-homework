5c83cf487f8e48916a67eeebe3a49d8d3bd4972f
kevin qi <kevinkeyjkw@gmail.com>
2015-10-04 21:04:50 -0400

    Changed P3.py, removed cartesian.py from P4

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -3,12 +3,9 @@ import pdb
 
 sc = SparkContext()
 textFile = sc.textFile("EOWL_words.txt")
+#Sort the letters of each word and make that key, and the original word the value
 sortedTf = textFile.map(lambda word: (''.join(sorted(word)),word))
-#pdb.set_trace()
-groupedTf = sortedTf.groupByKey().map(lambda x: (x[0],len(list(x[1])),list(x[1])))
-# maxTf = groupedTf.reduce(lambda a,b: a[0] if (a[0]>b[0]) else b[0])
-# maxTf = groupedTf.reduce(lambda a,b: a+b)
-#maxTf.take(10)
-r=groupedTf.map(lambda x:(x[1],x[0],x[2]))
-v=r.reduce(lambda a,b:max(a,b))
+#Group words that are the same anagrams together with the count
+v = sortedTf.groupByKey().map(lambda x: (len(list(x[1])),x[0],list(x[1]))).reduce(lambda a,b:max(a,b))
+
 print v
