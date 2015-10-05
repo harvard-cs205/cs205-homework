23611a64e5bdefe990be94b83994ce31e828fe0a
Feifei <fpeng01@g.harvard.edu>
2015-10-05 19:55:33 -0400

    P5 files added again w/o P5_bfs_old.py

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -7,99 +7,120 @@ page_names = sc.textFile('s3://Harvard-CS205/wikipedia/titles-sorted.txt')
 # links  = sc.textFile('links-simple-sorted.txt')
 # page_names = sc.textFile('titles-sorted.txt')
 
+
+
+#pair names with their indexes. 
+#notice that indexes are shifted to right by 1
+#also swap the position of name and index
+index_names = page_names.zipWithIndex().map(lambda tpl: (tpl[1]+1, tpl[0]))
+
+names_index = index_names.map(lambda tpl: (tpl[1],tpl[0]))
+
 def link_string_to_KV(s):
     name, indexes = s.split(': ')
     indexes = [int(index) for index in indexes.split(' ')]
     return (int(name), indexes)
 links_final = links.map(link_string_to_KV)
 
+def KV_further(tpl):
+    return (tpl[0], (10**6, tpl[0], tpl[1]))
+links_further = links_final.map(KV_further)
 
 
-##########functions for treating links##########
-def bfs(graph, starting_char):
-    char_list = [starting_char]
-    BFS_list  = []
-    
-    while len(char_list)!=0:
-        neighbors = graph.filter(lambda tpl: tpl[0] in char_list).flatMap(lambda tpl: tpl[1]).collect()
-        BFS_list  = BFS_list + char_list
-        char_list = []
-        char_list = char_list + list(set(neighbors)-set(BFS_list))                                     
-        print len(list(set(neighbors)-set(BFS_list)))
-    #print len(BFS_list)
-    
 
 
-def bfs_new(graph, starting_char, end_char):
-    char_list = [(starting_char,starting_char)]
-    BFS_list  = []
-    
-    while len(char_list)!=0:
-        neighbors = graph.filter(lambda tpl: tpl[0] in [c for (p,c) in char_list]).flatMap(lambda tpl: [(key,value) for key, value in zip([tpl[0]]*len(tpl[1]), tpl[1])] ).collect()
-        BFS_list  = BFS_list + char_list   #all visited nodes
-        char_list = []
-        children = [c for (p,c) in BFS_list]
-        for tpl in neighbors:
-        	if tpl[1] == end_char:
-        		char_list.append(tpl)
-        		BFS_list += char_list
-        		return #trace_path(BFS_list, end_char, starting_char)
-        	if tpl[1] not in children:
-        		char_list.append(tpl)
-    return []
+
+def combine_dist_parent_with_children(a,b):
+    '''we have nodes like (B, (1,A,[]) ). Also we have (B, (10**6,B,[neighbors of B]) ).
+       this function combines these information together.
+       Here a is with form (1,A,[]) and b is with form (10**6,B,[neighbors of B])'''
+    a_dist = a[0]
+    b_dist = b[0]
+
+    if a_dist < b_dist:
+        return (a_dist, a[1], [set(a[2]+b[2])]) 
+    else:
+        return (b_dist, b[1], [set(a[2]+b[2])]) 
 
 
+
+def set_starting_char_dist(node, start):
+    '''used at the beginning of bfs.
+       it finds the node that we start bfs with and set its distance to 0.'''
+    if node[0] == start:
+        #set dist=0 if node is the starting_char node
+        return (node[0],(0, node[0], node[1][2]))
         
-def trace_path(BFS_list, target, start):
-	result = [target]
-	while (target != start):
-		for tpl in BFS_list:
-			if tpl[1] == target:
-				target = tpl[0]
-				result = [target] + result
-				break
-	return result    
-##########end of functions for treating links##########
+    else:
+        return node
 
 
 
-#pair names with their indexes. 
-#notice that indexes are shifted to right by 1
-#also swap the position of name and index
-names_index = page_names.zipWithIndex().map(lambda tpl: (tpl[1]+1, tpl[0]))
+def set_parent_and_dist(node, n):
+    '''getting in a node with form (char, (dist, parent, [children]))
+       return a list of (child, (dist+1, char, [])) + node itself'''
+    children = node[1][2]
+    #parent = node[1][1] #just for information, did not use
+    dist = node[1][0]
+    self = node[0]
+    to_return = [node]
+    
+    if dist == n:
+        for child in children:
+            new = (child, (dist+1, self, []))
+            to_return.append(new)
+        #don't forget to append the node that we are working with
+
+    return to_return
 
 
 
-##########functions for treating page_names##########
-def name_to_index(name_string, rdd):
-    return rdd.filter(lambda x: x[1]==name_string).collect()[0][0]
+def bfs_new(starting_char, end_char, graph):
+    dist_traversed = 0
+    #set the starting_char node dist = 0
+    graph = graph.map(lambda x: (set_starting_char_dist(x, starting_char)) )
+
+    while True:
+        graph = graph.flatMap(lambda y: (set_parent_and_dist(y, dist_traversed)) )
+        #after we update the distances of children, we "step" further
+        dist_traversed += 1
+        graph = graph.reduceByKey(lambda a,b: (combine_dist_parent_with_children(a,b)))
+        
+        #check if end_char has been touched
+        #if so, bfs has been done
+        check_dist = graph.lookup(end_char)[0][0]
+        if check_dist < 10**6:
+            return (graph, dist_traversed)
 
 
 
-def index_to_name(index, rdd):
-    return rdd.filter(lambda x: x[0]==index).collect()[0][1]
+
+
+def trace_path(start_name, target_name, graph):
+    start_num = names_index.lookup(start_name)[0]
+    target_num = names_index.lookup(target_name)[0]
+    g_rdd = bfs_new(start_num, target_num, graph)[0]
+    
+    length = g_rdd.lookup(target_num)[0][0]
+    path_list = [target_num]
+    for i in length:
+        parent = g_rdd.lookup(target_num)[0][1]
+        path_list = [parent] + path_list
+        target_num = parent
+    
+    path_list_name = []
+    for j in path_list:
+        name = index_names.lookup(j)[0]
+        path_list_name.append(name)
+    return path_list_name
+
+ 
 
 
 
-def print_sequential_path(path_list, rdd):
-    result = []
-    for i in path_list:
-        result.append(index_to_name(i, rdd))
-    return result
-##########end of functions for treating page_names##########
-
-
-
-Harvard_University_index = name_to_index('Harvard_University', names_index)
-Kevin_Bacon_index = name_to_index('Kevin_Bacon', names_index)
-
-path_list_H_to_K = bfs_new(links_final, Harvard_University_index, Kevin_Bacon_index)
-path_list_K_to_H = bfs_new(links_final, Kevin_Bacon_index, Harvard_University_index)
-
-H_to_K = print_sequential_path(path_list_H_to_K, names_index)
-K_to_H = print_sequential_path(path_list_K_to_H, names_index)
-
-
-
-print 'path from Harvard_University to Kevin_Bacon:', H_to_K
-print 'path from Kevin_Bacon to Harvard_University:', K_to_H
+H_to_K = trace_path('Harvard University','Kevin Bacon',links_further)
+K_to_H = trace_path('Kevin Bacon','Harvard University',links_further)
+myfile = open('P5.txt', 'w')
+myfile.write(H_to_K)
+myfile.write(K_to_H)
+myfile.close()       
