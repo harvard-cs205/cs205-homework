5da81ddaf03aec8d9fb3cb08b162fedf1ad444de
toby <tobytoyuito@gmail.com>
2015-10-04 21:13:57 -0400

    finish P3

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -1,18 +1,16 @@
-from os import walk
 import findspark
 findspark.init('/home/toby/spark')
 
 import pyspark
-import numpy as np
 
-sc = pyspark.SparkContext(appName="Spark 2")
-myfiles = "./words/*.csv"
-words = sc.textFile(myfiles)
+if __name__ == '__main__':
+    sc = pyspark.SparkContext(appName="Spark 2")
+    sc.setLogLevel('WARN') 
+    myfiles = "./words/*.csv"
+    words = sc.textFile(myfiles) # read files
 
-words = words.map(lambda word: ["".join(sorted((word))), [word]])
-words = words.reduceByKey(lambda a, b: a+b)
-print words.take(10)[0]
+    words = words.map(lambda word: ["".join(sorted((word))), [word]]) # add sorted word as key
+    words = words.reduceByKey(lambda a, b: a+b) # reduce by sorted word
 
-words = words.map(lambda element: [element[0], len(element[1]), element[1]])
-#print words.take(10)
-print words.takeOrdered(1, key=lambda x: -x[1])
+    words = words.map(lambda element: [element[0], len(element[1]), element[1]])
+    print words.takeOrdered(1, key=lambda x: -x[1]) # print out most frequent one
