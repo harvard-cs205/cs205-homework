e91160e1a4b53dd3fd278599a590a1384a460f38
Peilin-D <peilin2duan@gmail.com>
2015-10-04 22:31:41 -0400

    Update P5_bfs.py

--- HW1/P5/P5_bfs.py
+++ HW1/P5/P5_bfs.py
@@ -4,10 +4,7 @@ def BFS_Graph(graph,startPt,endPt):
     currLevel=0
     
     def DistrFunc((src,(p,dests,d))):
-        #dist=d
-        #currNode=src
         listChildren=dests
-        #parent=p
         result=[(src,(p,dests,d))]
         if d==currLevel:
             for child in listChildren:
@@ -31,22 +28,14 @@ def BFS_Graph(graph,startPt,endPt):
 
     # map each node in graph rdd to tuple
     SG=graph.map(lambda node: (node[0],(node[0],node[1],0)) if node[0]==startPt else (node[0],(node[0],node[1],1000)))
-    #parents=[]
-    #numVisitedNodes = sc.accumulator(1)
-    #if limitedDiam:
     while True:
         SG=SG.flatMap(DistrFunc)
         SG=SG.reduceByKey(reduceFunc)
         tmp=SG.filter(lambda sg: sg[0]==endPt and sg[1][2]<1000).collect()
-        #print tmp.count()
-        #print tmp.take(1)
         if len(tmp)==1:
             break
         else:
             currLevel=currLevel+1
             print currLevel
-
-    #parent=SG.filter(lambda sg: sg[0]==tmp[0][1][0]).collect()
-    #parents.extend(parent[0])
 
     return SG
