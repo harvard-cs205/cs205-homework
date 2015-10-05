762e0399d50bd46c211a09a0de39cdaffeea4093
Virgile Audi <vaudi@g.harvard.edu>
2015-10-05 18:39:17 -0400

    Trying to make everything push

--- HW1/P5/P5.py
+++ HW1/P5/P5.py
@@ -8,22 +8,22 @@ page_names = sc.textFile('s3://Harvard-CS205/wikipedia/titles-sorted.txt')
 
 def link_string_to_KV(s):
     src, dests = s.split(': ')
-    dests = [to.encode('utf-8') for to in dests.split(' ')]
-    return (src.encode('utf-8'), dests)
+    dests = [to for to in dests.split(' ')]
+    return (src, dests)
 
-links = sc.textFile('s3://Harvard-CS205/wikipedia/links-simple-sorted.txt', 32)
-page_names = sc.textFile('s3://Harvard-CS205/wikipedia/titles-sorted.txt', 32)
 page_names = page_names.zipWithIndex().mapValues(lambda V: V+1)
-links = links.map(lambda x: link_string_to_KV(x))
+links = links.map(link_string_to_KV)
 edgelist = links.flatMap(lambda (K,V): [(K,V[i]) for i in range(len(V))])
+edgelist.partitionBy(256).cache()
 edgelist.take(1) 
-edgelist.partitionBy(128).cache()
 
 
-node_Kevin = page_names.lookup(u'Kevin_Bacon')
-node_harvard = page_names.lookup("Harvard_University")
+node1 = str(page_names.lookup(u'Kevin_Bacon')[0])
+node2 = str(page_names.lookup("Harvard_University")[0])
 
 print "Edgelist ready"
-print node_Kevin
 
-print bfs_look2(edgelist,node_Kevin,node_harvard,10,sc)
+print bfs_look(edgelist,node1,node2,10,sc)
+print bfs_look(edgelist,node2,node1,10,sc)
+
+#
--- HW1/P5/P5_BFS.py
+++ HW1/P5/P5_BFS.py
@@ -1,4 +1,19 @@
-def bfs_look2(edgelist,from_,to_,dmax,sc):
+#BFS algorithm with find path feature:
+# Function takes 5 argument:
+# - The graph in the form of an edgelist
+# - The starting link (integer parsed as string)
+# - The target link (integer parsed as string)
+# - The maximum number of iteration in case there is an issue, should normally stop when no more nodes can be touched
+# - the spark context
+
+#Output of the function is a quite intricate tuple (sorry couldn't figure a way to make it more readable):
+# - K: (target,distance)
+# - V: [(node_to,([intermediate_node_n,…[node_from,<-]],distance)…)]
+#
+
+
+
+def bfs_look(edgelist,from_,to_,dmax,sc):
     global accum 
     dis = 1
     accum = sc.accumulator(1)
@@ -7,35 +22,45 @@ def bfs_look2(edgelist,from_,to_,dmax,sc):
         global accum
         accum += 1
     
+    #Function used to format the paths rdd
     def swap((K,V)):
         V = list(V)
         return (V[0],[K]+V[1:])
-        
+     
     nextneigh = sc.parallelize([(from_,0)])
     distance = sc.parallelize([(from_,0)])
     paths = sc.parallelize([(from_,'<-')])
 
 
     while dis <= dmax:
+        print dis
         oldaccum = accum.value
         
-        temp = edgelist.join(nextneigh).partitionBy(64)
+        temp = edgelist.join(nextneigh).partitionBy(256)
         
         nextneigh = temp.map(lambda (K,V): (V[0],V[1]+1)).distinct().subtractByKey(distance)
         nextneigh.foreach(increase_accum)
         
+        #Up to here same as in the regular BFS in P4
+
         temp2 = temp.map(lambda (K,V): (V[0],K)).subtractByKey(distance).map(lambda (K,V): (V,K))
+        # Find the previous edges going to the members of nextneigh and verify that nodes haven't already been hit
+        
         paths = temp2.join(paths).map(lambda (K,V): swap((K,V)))
+        # Update the paths rdd by adding the new valid steps and formating it
+     
         
         if oldaccum == accum.value:
             result = "NO PATH"
             return result
-        distance = distance.union(nextneigh).cache()
-        result = distance.join(sc.parallelize([(to_,dis)])).partitionBy(64)
+        distance = distance.union(nextneigh).partitionBy(256).cache()
         
+        #Result checks if the target node has been hit in order to stop the loop earlier:
+        result = distance.join(sc.parallelize([(to_,dis)])).partitionBy(256)
+
         if not result.isEmpty():
             result = result.mapValues(lambda v: v[0]).collect()
-            result2 = paths.join(sc.parallelize([(to_,dis)])).partitionBy(64).collect()
+            result2 = paths.join(sc.parallelize([(to_,dis)])).partitionBy(256).collect()
             break
         dis += 1
         
