db6e5f99f71c7f930bbd5f779178d55f6245607d
Elena Zhao <zhaoqing513@126.com>
2015-10-05 01:02:38 -0400

    Update P4.py

--- HW1/P4/P4.py
+++ HW1/P4/P4.py
@@ -17,37 +17,32 @@ def generatePairs(node_set):
                 edges.append((i, j))
     return edges
 
-if __name__ == "__main__":
+rawData = sc.textFile("source.csv").map(lambda line: line.split('","')).map(lambda x: [x[0]+'"', '"'+x[1]]).cache()
 
-	sc = pyspark.SparkContext()
+# Get the list of all characters
+charactersRDD = rawData.map(lambda x: x[0]).distinct()
 
-	rawData = sc.textFile("source.csv").map(lambda line: line.split('","')).map(lambda x: [x[0]+'"', '"'+x[1]]).cache()
+graphRDD = charactersRDD.map(lambda x: (x, []))
 
-	# Get the list of all characters
-	charactersRDD = rawData.map(lambda x: x[0]).distinct()
+# Get a list of connected characters, each element integrate all the characters in a single issue
+# [[name1, name2,... ],...,[name_n, name_m]]
+edges_tmp = rawData.map(lambda x: (x[1], x[0])).groupByKey().map(lambda x: list(x[1]))
 
-	#
-	graphRDD = charactersRDD.map(lambda x: (x, []))
+# Generate MC_graph based on edges_tmp, here the MC_graph only contains nodes that are connected 
+MC_Graph = edges_tmp.flatMap(generatePairs).groupByKey().mapValues(lambda x: list(set(x)))
 
-	# Get a list of connected characters, each element integrate all the characters in a single issue
-	# [[name1, name2,... ],...,[name_n, name_m]]
-	edges_tmp = rawData.map(lambda x: (x[1], x[0])).groupByKey().map(lambda x: list(x[1]))
+# filter out connected nodes in charactersRDD and get isolated nodes
+connected_nodes = MC_Graph.keys().collect()
+isolated_nodes = charactersRDD.filter(lambda K: K not in connected_nodes).map(lambda x: (x, []))
 
-	# Generate MC_graph based on edges_tmp, here the MC_graph only contains nodes that are connected 
-	MC_Graph = edges_tmp.flatMap(generatePairs).groupByKey().mapValues(lambda x: list(set(x)))
+#Finalize MC_Graph by unioning both connected and isolated nodes
+MC_Graph = MC_Graph.union(isolated_nodes).cache()
 
-	# filter out connected nodes in charactersRDD and get isolated nodes
-	connected_nodes = MC_Graph.keys().collect()
-	isolated_nodes = charactersRDD.filter(lambda K: K not in connected_nodes).map(lambda x: (x, []))
+# apply BFS algorithm to get the number of touched nodes in each search
 
-	#Finalize MC_Graph by unioning both connected and isolated nodes
-	MC_Graph = MC_Graph.union(isolated_nodes).cache()
-
-	# apply BFS algorithm to get the number of touched nodes in each search
-
-	# CAPTAIN AMERICA: 6408
-	ca_touched_count = BFS(MC_Graph, 'CAPTAIN AMERICA')
-	# MISS THING/MARY: 7
-	mtm_touched_count = BFS(MC_Graph, 'MISS THING/MARY')
-	# ORWELL: 9
-	o_touched_count = BFS(MC_Graph, 'ORWELL')
+# CAPTAIN AMERICA: 6408
+ca_touched_count = BFS(MC_Graph, 'CAPTAIN AMERICA')
+# MISS THING/MARY: 7
+mtm_touched_count = BFS(MC_Graph, 'MISS THING/MARY')
+# ORWELL: 9
+o_touched_count = BFS(MC_Graph, 'ORWELL')
