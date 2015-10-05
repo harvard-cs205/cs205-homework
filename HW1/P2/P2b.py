8ec56ab3142ef092eb9fd81547722919c5c5318f
Neil Chainani <chainani@g.harvard.edu>
2015-10-05 01:19:11 -0400

    added text files
    Former-commit-id: d504ff91a3a2d61fac48940ceb08e4b30645981c

--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,7 @@
-from P2 import *
-
-# Your code here
+import random
+def main():
+	i=sc.parallelize(range(2000),10)
+	j=sc.parallelize(range(2000),10)
+	mandle2=i.cartesian(j).partitionBy(100,lambda x: random.randint(0,1999)).map(lambda x: (x,mandelbrot(x[1]/500.0-2,x[0]/500.0-2))).cache()
+	draw_image(mandle)
+	plt.hist(sum_values_for_partitions(mandle2).collect())
