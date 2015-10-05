4b12e1610fd3b9996c77b3b3dcef8fb51c052475
Peilin-D <peilin2duan@gmail.com>
2015-10-04 22:35:52 -0400

    Update P6.py

--- HW1/P6/P6.py
+++ HW1/P6/P6.py
@@ -7,10 +7,10 @@ def filter_out_Capital(word):
     for l in word:
         if l in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             count=count+1
-
+    # filter out words only containing capital letters
     if count==wlen:
         return False
-
+    # filter out words only containing capital letters and also a period at the end
     if count==wlen-1 and word[-1]=='.':
         return False
 
@@ -30,16 +30,17 @@ words=words.filter(filter_out_Capital)
 words0=words.zipWithIndex().map(lambda (w,id): (id,w))
 words1=words.zipWithIndex().map(lambda (w,id): (id+1,w))
 words2=words.zipWithIndex().map(lambda (w,id): (id+2,w))
+
 words_rdd=words2.join(words1).join(words0)
 words_rdd=words_rdd.map(lambda (c,((w1,w2),w3)): ((w1,w2,w3),1))
 words_rdd=words_rdd.reduceByKey(lambda x,y: x+y)
 words_rdd=words_rdd.map(lambda ((w1,w2,w3),c): ((w1,w2),(w3,c))).groupByKey().mapValues(list)
 
-
 # generate text
 # randomly take 10 samples
 randWords=words_rdd.takeSample(False,10,1)
-#print randWords
+
+# open file to write
 text_file=open('P6.txt','w')
 
 for RW in randWords:
@@ -50,7 +51,6 @@ for RW in randWords:
     loop=0
     while loop<18:
         appendWords=words_rdd.map(lambda x:x).lookup(startingWords)[0]
-        #print appendWords
         apword=sc.parallelize(appendWords)
         apw=apword.sortBy(lambda aw: -aw[1]).collect()[0][0]
         startingWords=(startingWords[1],apw)
