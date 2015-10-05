7fc8c04193e3c762a581e9100413d0181d6c7c02
scheng95 <samuelcheng@college.harvard.edu>
2015-10-05 18:48:46 -0400

    finished P6

--- HW1/P6/P6.py
+++ HW1/P6/P6.py
@@ -7,23 +7,7 @@ import random
 NUM_PHRASES = 10
 WORDS_PER_PHRASE = 20
 
-sc = SparkContext("local", "Shakespeare")
-
-shakes = sc.textFile("Shakespeare.txt")
-
-shakes_words = shakes.map(lambda l: l.split(" ")).flatMap(lambda l: l)
-shakes_words = shakes_words.filter(lambda w: not w.isdigit())
-shakes_words = shakes_words.filter(lambda w: len(w) > 0)
-shakes_words = shakes_words.filter(lambda w: not w.isupper())
-
-word_index0 = shakes_words.zipWithIndex().map(lambda (w, i): (i, w))
-word_index1 = word_index0.map(lambda (i, w): (i + 1, w))
-word_index2 = word_index0.map(lambda (i, w): (i - 1, w))
-
-pair_words = word_index1.join(word_index0)
-phrases = pair_words.join(word_index2).map(lambda (k, (w2, w1)): (w2, [w1]))
-grouped_phrases = phrases.reduceByKey(lambda x, y: x + y).map(lambda x: x)
-
+# helper to convert list to dictionary
 def to_dict(words):
     c = Counter(words)
     l = []
@@ -31,13 +15,31 @@ def to_dict(words):
         l.append((k, c[k]))
     return l
 
+sc = SparkContext("local", "Shakespeare")
+
+shakes = sc.textFile("Shakespeare.txt")
+
+# take out strings that are only numbers, empty, or only upper
+shakes_words = shakes.map(lambda l: l.split(" ")).flatMap(lambda l: l)
+shakes_words = shakes_words.filter(lambda w: not w.isdigit())
+shakes_words = shakes_words.filter(lambda w: len(w) > 0)
+shakes_words = shakes_words.filter(lambda w: not w.isupper())
+
+# create RDDs with offset keys to prepare for joining
+word_index0 = shakes_words.zipWithIndex().map(lambda (w, i): (i, w))
+word_index1 = word_index0.map(lambda (i, w): (i + 1, w))
+word_index2 = word_index0.map(lambda (i, w): (i - 1, w))
+
+# group by keys of (word1, word2) and count elements in each list
+pair_words = word_index1.join(word_index0)
+phrases = pair_words.join(word_index2).map(lambda (k, (w2, w1)): (w2, [w1]))
+grouped_phrases = phrases.reduceByKey(lambda x, y: x + y).map(lambda x: x)
+
 counted_phrases = grouped_phrases.map(lambda (p, l): (p, to_dict(l)))
 
+# sample for beginning of each phrase
 seed = grouped_phrases.takeSample(False, NUM_PHRASES)
-
-# seed_rdd = sc.parallelize(seed)
-# seed_rdd = seed_rdd.map(lambda (k, v): [x for x in k])
-
+# slow because there are many calls to lookup
 prose = []
 for i in range(0, NUM_PHRASES):
     current = seed[i][0]
