a898a854b1139770c3ca456fdb00304a93f960fa
Nithin Tumma <nithin@Nithins-MacBook-Pro.local>
2015-10-05 19:52:24 -0400

    problem 3

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -1,13 +1,20 @@
 import findspark 
 findspark.init()
-
 import pyspark
-sc = pyspark.SparkContext()
 
-wlist = sc.textFile('EOWL_words.txt')
-alpha_words = wlist.map(lambda x: ("".join(sorted(x)), [x]))
-alpha_anagrams = alpha_words.reduceByKey(lambda x, y: x + y)
-anagram_list = alpha_anagrams.map(lambda x: (x[0], len(x[1]), x[1]))
-anagram_list.collect()
-max_anagram = anagram_list.sortBy(lambda x: x[1], ascending=False).take(1)
-print max_anagram
+sc = pyspark.SparkContext()
+sc.setLogLevel("WARN")
+
+def main():
+    # read in the words list
+    wlist = sc.textFile('EOWL_words.txt')
+    # turn into RDD of alphabetical order -> word
+    alpha_words = wlist.map(lambda x: ("".join(sorted(x)), [x]))
+    # RDD of alpha order -> [word1, word2, ...]
+    alpha_anagrams = alpha_words.reduceByKey(lambda x, y: x + y)
+    anagram_list = alpha_anagrams.map(lambda x: (x[0], len(x[1]), x[1]))
+    max_anagram = anagram_list.sortBy(lambda x: x[1], ascending=False).take(1)
+    print max_anagram
+
+if __name__=="__main__":
+    main()
