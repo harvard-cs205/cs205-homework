a23237947f6c6c3aeb50f657be4e7f7f8bf15633
Andrew Petschek <petschek@g.harvard.edu>
2015-10-05 13:02:02 -0400

    P3 final

--- HW1/P3/P3.py
+++ HW1/P3/P3.py
@@ -11,12 +11,15 @@ wordcharacters = words.map(lambda word: np.sort(list(word)))
 #now combine letters back together using join function. 
 SortedLetterSequence = wordcharacters.map(lambda x: "".join(x))
 
-#make two columns of our alphabetized words and group by key to get anagrams
-
+#make two columns of our alphabetized words and .groupByKey to get anagrams
 keyvaluerdd = words.map(lambda x: ("".join(np.sort(list(x))),x))
 temp = keyvaluerdd.groupByKey().mapValues(list)
 Anagrams = temp.map(lambda x: (x[0],(len(x[1]),x[1])))
 
 #find maximum where we know max=11, so use filter.
 maximum = Anagrams.filter(lambda x: x[1][0] == 11)
-print maximum.collect()
+
+#there are three words with max=11. printing them out below.
+print maximum.collect()[0]
+print maximum.collect()[1]
+print maximum.collect()[2]
