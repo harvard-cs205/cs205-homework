4a89c8e6708aff024e33466026304566fac1dd5d
Andrew Reece <reece@g.harvard.edu>
2015-10-06 23:17:04 +0200

    Updated P6 to RDD-only, admitted defeat on 1.5 pt2

--- HW1/P6/P6.py
+++ HW1/P6/P6.py
@@ -47,26 +47,23 @@ def countit(wlist):
 	- from there we can groupByKey and format.
 '''
 
-words  = shakespeare.flatMap(format_corpus(), preservesPartitioning=True).cache()
+words  = shakespeare.flatMap(format_corpus()).zipWithIndex().partitionBy(n_part).cache()
 wct    = words.count()
-wlist1 = words.zipWithIndex().filter( lambda x: x[1] < wct-2 ).map( lambda x: tuple(reversed(x)) ).partitionBy(n_part).cache()
-wlist2 = words.zipWithIndex().filter( lambda x: (x[1] > 0) and (x[1] < wct-1) ).map( lambda x: tuple(reversed(x)) ).map( lambda x: (x[0]-1,x[1]) ).partitionBy(n_part).cache()
-wlist3 = words.zipWithIndex().filter( lambda x: x[1] > 1 ).map( lambda x: tuple(reversed(x)) ).map( lambda x: (x[0]-2,x[1]) ).partitionBy(n_part).cache()
-rdd    = (wlist1.join(wlist2)
-				.join(wlist3)
+wlist1 = words.filter(lambda x: x[1] < wct-2).map(lambda x: tuple(reversed(x))).partitionBy(n_part)
+wlist2 = words.filter(lambda x: (x[1] > 0) and (x[1] < wct-1)).map(lambda x: tuple(reversed(x))).map(lambda x: (x[0]-1,x[1])).partitionBy(n_part)
+wlist3 = words.filter(lambda x: x[1] > 1).map( lambda x: tuple(reversed(x)) ).map(lambda x: (x[0]-2,x[1])).partitionBy(n_part)
+rdd    = (wlist1.join(wlist2, numPartitions=n_part)
+				.join(wlist3, numPartitions=n_part)
 				.map(lambda x: x[1])
-				.groupByKey()
-				.map(lambda x: ( x[0],countit(x[1]) ))
+				.partitionBy(n_part)
+				.groupByKey(numPartitions=n_part)
+				.map(lambda x: ( x[0],countit(x[1]) ), preservesPartitioning=True)
 				.cache()
 	   )
 	
 ''''''''''''''''''''
 ''' markov model '''
 ''''''''''''''''''''
-
-''' Note: Here are 2 versions of the Markov model: RDD-only and RDD/normal Python hybrid '''
-
-''' COMMON SETUP FOR BOTH IMPLEMENTATIONS '''
 
 def get_third(third_words):
 	''' creates empirical PMF from 3rd word counts, draws one word by sampling '''
@@ -79,65 +76,43 @@ n_samp = 10
 n_iter = 20
 starts = rdd.takeSample(False,n_samp)       # draw n_samp (first_word,second_word) tuples
 
-
-''' PURE RDD IMPLEMENTATION '''
 def start_filter(s):
-	return lambda x: x[0] in [start[0] for start in s]
+	return lambda x: x[0] in [w[0] for w in s]
 
-def run_pure():
-	timeA=time.time()
 
-	start  = rdd.filter( start_filter(starts) ) # filter rdd down to 10 starts
-	phrase = (start.zipWithIndex()              # initialize phrase as (idx, (word1,word2))
-				   .map( lambda x: tuple(reversed(x)) )
-				   .map( lambda x: (x[0],[el for el in x[1][0]]) )
-				   .partitionBy(n_part)
-			  )
+timeA=time.time()
 
-	for i in range(n_iter):   # we want n_iter-word phrases (target:20)
-		if i:                 # after initial round, build start based on prior keys
-			start = (rdd.join(key.map(lambda x: (x,['',0]))) # add dummy [(word3,freq)] to key for join
-						 .partitionBy(n_part)
-						 .map(lambda x: (x[0],x[1][0]))      # drop dummy (necessary?)
-						 .partitionBy(n_part)
-						 .cache()
-					)
-			
-		key    = (start.map(lambda x: (x[0][1], get_third(x[1]))) # draw word3 from PMF, build new key
-					   .partitionBy(n_part)
-				  )
-		phrase = (phrase.union( key.zipWithIndex()                      # union new word3 to phrase 
-								   .map( lambda x: tuple(reversed(x)) ) # make index into key for union
-								   .map( lambda x: (x[0],x[1][1]) )     # take only word3
-								   .partitionBy(n_part) 
-							  )
-						.reduceByKey(lambda a,b: a+[b])                 # reduce to list of phrase words
-						.partitionBy(n_part)
+start  = rdd.filter( start_filter(starts) ) # filter rdd down to 10 starts
+phrase = (start.zipWithIndex()              # initialize phrase as (idx, (word1,word2))
+			   .map( lambda x: tuple(reversed(x)) )
+			   .map( lambda x: (x[0],[el for el in x[1][0]]) )
+			   .partitionBy(n_part)
+		  )
+
+for i in range(n_iter):   # we want n_iter-word phrases (target:20)
+	if i:                 # after initial round, build start based on prior keys
+		start = (rdd.join(key.map(lambda x: (x,['',0])).partitionBy(n_part), numPartitions=n_part) # add dummy [(word3,freq)] to key for join
+					 .map(lambda x: (x[0],x[1][0]), preservesPartitioning=True)      # drop dummy (necessary?)
+					 .partitionBy(n_part)
+					 .cache()
 				)
 		
-	print phrase.map(lambda x: ' '.join(x[1])).collect() # print each phrase as string (space separator)
+	key    = (start.map(lambda x: (x[0][1], get_third(x[1]))) # draw word3 from PMF, build new key
+				   .partitionBy(n_part)
+				   .cache()
+			  )
+	phrase = (phrase.union( key.zipWithIndex()                      # union new word3 to phrase 
+							   .map( lambda x: tuple(reversed(x)) ) # make index into key for union
+							   .partitionBy(n_part)
+							   .map( lambda x: (x[0],x[1][1]), preservesPartitioning=True ) # take only word3
+						  )
+					.reduceByKey(lambda a,b: a+[b], numPartitions=n_part) # reduce to list of phrase words
+			)
+	
+print phrase.map(lambda x: ' '.join(x[1])).collect() # print each phrase as string (space separator)
 
-	timeB=time.time()
-	print "total time (pure):",timeB-timeA
+timeB=time.time()
+print "total time:",timeB-timeA
 
-run_pure() # ~293s
+# ~150s
 
-
-''' HYBRID RDD/PYTHON IMPLEMENTATION '''
-def run_hybrid():
-	timeA=time.time()
-
-	for start in starts:
-		key    = start[0]
-		phrase = [key[0],key[1]]
-		for i in range(n_iter): # we want n_iter-word phrases (target:20)
-			second = key[1] 
-			third  = get_third(rdd.lookup(key)[0]) 	# use new (first,second) to get 3rd word draw
-			key    = (second,third)					# make new (first,second) from current (second,third)
-			phrase.append(third)  					# append drawn 3rd to phrase
-		print ' '.join(phrase)
-
-	timeB=time.time()
-	print "total time (hybrid):",timeB-timeA
-
-run_hybrid() # ~210s
