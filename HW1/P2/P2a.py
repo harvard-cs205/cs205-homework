dbe0ff396a5f5617be0d3517ea15288a01ff1628
Sam Daulton <sdaulton@g.harvard.edu>
2015-10-06 16:47:50 -0400

    Made the P2 histograms prettier and finished the writeup

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -14,21 +14,30 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
 
-# In[3]:
 from P2 import mandelbrot, sum_values_for_partitions, draw_image
 #Clean Version
 arr = xrange(2000)
-rdd_single = sc.parallelize(arr,100)
+rdd_single = sc.parallelize(arr,10)
 rdd = rdd_single.cartesian(rdd_single)
-#print dir(rdd)
+
 iters = rdd.map(lambda (i,j): ((i,j), mandelbrot(j / 500.0 - 2, i / 500.0 - 2)))
 num_per_part = sum_values_for_partitions(iters)
-#draw_image(iters)
-#fig = plt.figure()
-#ax = fig.add_axes()
-plt.hist(num_per_part.collect())
-#MAKE HISTOGRAM PRETTIER
-#ax.set_xlabel('Compute Per Partition')
-#ax.set_ylabel('Frequency')
+draw_image(iters)
+num_per_part_list = num_per_part.collect()
+
+
+
+# In[9]:
+
+#plot histogram
+plt.hist(num_per_part_list, color="black")
+plt.title("Compute per Partition")
+plt.xlabel("Compute (Effort)")
+plt.ylabel("Frequency")
 plt.savefig("P2a_hist.png")
+
+
+# In[ ]:
+
+
 
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -14,31 +14,25 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
 from P2 import mandelbrot, sum_values_for_partitions, draw_image
-'''
-#RePartitioning
-arr = xrange(2000)
-rdd_single = sc.parallelize(arr,10)
-rdd = rdd_single.cartesian(rdd_single)
-rdd = rdd.repartition(100)
-iters = rdd.map(lambda (i,j): ((i,j), mandelbrot(j / 500.0 - 2, i / 500.0 - 2)))
-#print iters.take(10)
-num_per_part = sum_values_for_partitions(iters)
-#draw_image(iters)
-plt.hist(num_per_part.collect())
-#MAKE HISTOGRAM PRETTIER
-plt.savefig("P2c_hist.png")
-'''
+
 #Improved Partitioning
 arr = xrange(2000)
 rdd_single = sc.parallelize(arr,10)
 rdd = rdd_single.cartesian(rdd_single)
 rdd = rdd.partitionBy(100)
 iters = rdd.map(lambda (i,j): ((i,j), mandelbrot(j / 500.0 - 2, i / 500.0 - 2)))
-#print iters.take(10)
 num_per_part = sum_values_for_partitions(iters)
 #draw_image(iters)
-plt.hist(num_per_part.collect())
-#MAKE HISTOGRAM PRETTIER
+num_per_part_list = num_per_part.collect()
+
+
+# In[2]:
+
+#plot histogram
+plt.hist(num_per_part_list, color="black")
+plt.title("Compute per Partition")
+plt.xlabel("Compute (Effort)")
+plt.ylabel("Frequency")
 plt.savefig("P2b_hist.png")
 
 
