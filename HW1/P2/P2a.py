bda223339f28adebdb2c8ee24c51e494df47a55d
Serguei Balanovich <sbalanovuch@college.harvard.edu>
2015-10-06 05:55:59 +0500

    Retroactively committing P2

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,21 @@
 from P2 import *
+from pyspark import SparkContext
+
+N = 2000
 
 # Your code here
+sc = SparkContext("local", "P2a")
+
+rdd = sc.parallelize(range(N), 10)
+plot = rdd.cartesian(rdd)
+
+mb_image = plot.map(lambda x: (x, mandelbrot((x[0]/500.0-2), (x[1]/500.0-2))))
+draw_image(mb_image)
+
+mb_data = sum_values_for_partitions(mb_image)
+mb_plot = mb_data.collect()
+plt.hist(mb_plot)
+plt.xlabel("Partition")
+plt.ylabel("Number of Iterations")
+plt.title("Standard Sequential Partition Histogram")
+plt.savefig("P2a_hist.png")
--- HW1/P2/P2b.py
+++ HW1/P2/P2b.py
@@ -1,3 +1,22 @@
 from P2 import *
+from pyspark import SparkContext
+import random
+
+N = 2000
 
 # Your code here
+sc = SparkContext("local", "P2b")
+
+rdd = sc.parallelize(sorted(range(N), key=lambda k: random.random()), 10)
+plot = rdd.cartesian(rdd)
+
+mb_image = plot.map(lambda x: [x, mandelbrot((x[0]/500.0-2), (x[1]/500.0-2))])
+draw_image(mb_image)
+
+mb_data = sum_values_for_partitions(mb_image)
+mb_plot = mb_data.collect()
+plt.hist(mb_plot)
+plt.xlabel("Partition")
+plt.ylabel("Number of Iterations")
+plt.title("New Random Partition Histogram")
+plt.savefig("P2b_hist.png")
