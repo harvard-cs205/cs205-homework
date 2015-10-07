5784d9a3b36aceedd4f1936baca836cfb0180ece
Dennis Grishin <dgrishin@g.harvard.edu>
2015-10-06 22:01:01 -0400

    HW1 Part 2 added

--- HW1/P2/P2a.py
+++ HW1/P2/P2a.py
@@ -1,3 +1,31 @@
 from P2 import *
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from random import shuffle
 
-# Your code here
+import findspark
+findspark.init()
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName('P2').setMaster('local')
+sc = SparkContext(conf=conf)
+
+# Calculate a mandelbrot set in parallel manner (without load balancing)
+
+data = list(itertools.permutations(range(1, 2001), 2))
+distData = sc.parallelize(data, 100)
+point_intensity = distData.map(lambda (x, y): ((x, y), mandelbrot((x/500.0) - 2, (y/500.0) - 2))).cache()
+
+draw_image(point_intensity)
+
+partition_load = sum_values_for_partitions(point_intensity).collect()
+
+plt.hist(partition_load)
+
+plt.title('Distribution of computing load between partitions')  
+plt.xlabel('Computing load') 
+plt.ylabel('Number of partitions')
+plt.xticks(rotation='vertical')
+plt.subplots_adjust(bottom=0.30)
+plt.savefig('P2a_hist.png')
