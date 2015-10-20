2dee4fdb66b4834981eaae1c66385c19dc6055dd
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-10-19 22:34:33 -0400

    Fixed more terrible indentation problems.

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -65,7 +65,6 @@ cpdef move_data_serial(np.int32_t[:] counts,
                     counts[dest[idx]] += 1
                     counts[src[idx]] -= 1
 
-
 cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
@@ -105,6 +104,7 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                     acquire(&locks[destination])
                     acquire(&locks[source])
 
+                # Do the transfer
                 if counts[src[idx]] > 0:
                     counts[destination] += 1
                     counts[source] -= 1
@@ -127,22 +127,48 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] dest,
                                int repeat,
                                int N):
-   cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+    cdef:
+        int idx, r
+        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+        omp_lock_t *locks = get_N_locks(num_locks)
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for every N adjacent elements of counts
-   # to parallelize data movement.  Be sure to avoid deadlock, as well as
-   # double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    ##########
+    # Your code here
+    # Use parallel.prange() and a lock for every N adjacent elements of counts
+    # to parallelize data movement.  Be sure to avoid deadlock, as well as
+    # double-locking.
+    ##########
+    with nogil:
+        for r in prange(repeat):
+            for idx in range(src.shape[0]):
+                source = src[idx]
+                destination = dest[idx]
 
-   free_N_locks(num_locks, locks)
+                if source < destination: case = -1
+                elif source > destination: case=1
+                else: case=0
+
+                if case==-1:
+                    acquire(&locks[source])
+                    acquire(&locks[destination])
+                elif case==0:
+                    acquire(&locks[source])
+                elif case==1:
+                    acquire(&locks[destination])
+                    acquire(&locks[source])
+
+                if counts[source] > 0:
+                    counts[destination] += 1
+                    counts[source] -= 1
+
+                # Now release in the appropriate order
+                if case==-1:
+                    release(&locks[source])
+                    release(&locks[destination])
+                elif case==0:
+                    release(&locks[source])
+                elif case==1:
+                    release(&locks[destination])
+                    release(&locks[source])
+
+        free_N_locks(num_locks, locks)
