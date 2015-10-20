59a514ef08199bdd4527947fff3e72de8961490c
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-10-19 21:50:58 -0400

    Fixed terrible indentation problems in P2.

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -8,8 +8,8 @@
 
 from cython.parallel import parallel, prange
 from openmp cimport omp_lock_t, \
-    omp_init_lock, omp_destroy_lock, \
-    omp_set_lock, omp_unset_lock, omp_get_thread_num
+	omp_init_lock, omp_destroy_lock, \
+	omp_set_lock, omp_unset_lock, omp_get_thread_num
 from libc.stdlib cimport malloc, free
 
 import numpy as np
@@ -18,10 +18,10 @@ cimport numpy as np
 
 # lock helper functions
 cdef void acquire(omp_lock_t *l) nogil:
-    omp_set_lock(l)
+	omp_set_lock(l)
 
 cdef void release(omp_lock_t *l) nogil:
-    omp_unset_lock(l)
+	omp_unset_lock(l)
 
 # helper function to fetch and initialize several locks
 cdef omp_lock_t *get_N_locks(int N) nogil:
@@ -32,18 +32,18 @@ cdef omp_lock_t *get_N_locks(int N) nogil:
     if not locks:
         with gil:
             raise MemoryError()
-    for idx in range(N):
-        omp_init_lock(&(locks[idx]))
+	for idx in range(N):
+		omp_init_lock(&(locks[idx]))
 
-    return locks
+	return locks
 
 cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
-    cdef int idx
+	cdef int idx
 
-    for idx in range(N):
-        omp_destroy_lock(&(locks[idx]))
+	for idx in range(N):
+		omp_destroy_lock(&(locks[idx]))
 
-    free(<void *> locks)
+	free(<void *> locks)
 
 
 ##################################################
@@ -51,53 +51,57 @@ cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
 ##################################################
 
 cpdef move_data_serial(np.int32_t[:] counts,
-                       np.int32_t[:] src,
-                       np.int32_t[:] dest,
-                       int repeat):
-   cdef:
-       int idx, r
+					   np.int32_t[:] src,
+					   np.int32_t[:] dest,
+					   int repeat):
 
-   assert src.size == dest.size, "Sizes of src and dest arrays must match"
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    cdef:
+        int idx, r
+    assert src.size == dest.size, "Sizes of src and dest arrays must match"
+    with nogil:
+        for r in range(repeat):
+            for idx in range(src.shape[0]):
+                if counts[src[idx]] > 0:
+                    counts[dest[idx]] += 1
+                    counts[src[idx]] -= 1
 
 
 cpdef move_data_fine_grained(np.int32_t[:] counts,
-                             np.int32_t[:] src,
-                             np.int32_t[:] dest,
-                             int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+							 np.int32_t[:] src,
+							 np.int32_t[:] dest,
+							 int repeat):
+    cdef:
+        int idx, r
+        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
    ##########
    # Your code here
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
 
-   free_N_locks(counts.shape[0], locks)
+    with nogil:
+        for r in prange(repeat):
+            for idx in range(src.shape[0]):
+                # We need to lock here
+                acquire(locks[idx])
+                if counts[src[idx]] > 0:
+                    counts[dest[idx]] += 1
+                    counts[src[idx]] -= 1
+                release(locks[idx])
+
+    free_N_locks(counts.shape[0], locks)
 
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
-                               np.int32_t[:] src,
-                               np.int32_t[:] dest,
-                               int repeat,
-                               int N):
+							   np.int32_t[:] src,
+							   np.int32_t[:] dest,
+							   int repeat,
+							   int N):
    cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+	   int idx, r
+	   int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+	   omp_lock_t *locks = get_N_locks(num_locks)
 
    ##########
    # Your code here
@@ -106,10 +110,10 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # double-locking.
    ##########
    with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+	   for r in range(repeat):
+		   for idx in range(src.shape[0]):
+			   if counts[src[idx]] > 0:
+				   counts[dest[idx]] += 1
+				   counts[src[idx]] -= 1
 
    free_N_locks(num_locks, locks)
