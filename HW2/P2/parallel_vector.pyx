f82cf81702d093b7575409264269f9562afc55ec
Nicholas Boffi <nick.boffi@gmail.com>
2015-10-21 17:54:07 -0400

    Finished problem 2, working on problem 3

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -71,7 +71,7 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] dest,
                              int repeat):
    cdef:
-       int idx, r
+       int idx, r, si, di, first_lock, second_lock, same_lock
        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
    ##########
@@ -81,10 +81,44 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
+
+               # Use new variables just to clear up the syntax
+               si = src[idx]
+               di = dest[idx]
+
+               # Boolean value to let us know when si = di
+               same_lock = 0
+
+               # We want to always lock in order of smallest index
+               if si < di:
+                   first_lock = si
+                   second_lock = di
+               elif di < si:
+                   first_lock = di
+                   second_lock = si
+               else:
+                   # And if they're the same, we only lock once
+                   same_lock = 1
+                   first_lock = si
+
                if counts[src[idx]] > 0:
+                   # Get out locks to modify the data
+                   acquire(&locks[first_lock])
+                   if not same_lock:
+                       acquire(&locks[second_lock])
+
+                   # Increment and decrement accordingly
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+
+                   # Release locks, depending on whether we locked once or twice
+                   # In the same order, but it shouldn't actually matter at this point
+                   if not same_lock:
+                       release(&locks[second_lock])
+                       release(&locks[first_lock])
+                   else:
+                       release(&locks[first_lock])
 
    free_N_locks(counts.shape[0], locks)
 
@@ -95,7 +129,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, si, di, first_lock, second_lock, same_lock
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
@@ -107,9 +141,34 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
+               si = src[idx] % num_locks
+               di = dest[idx] % num_locks
+
+               same_lock = 0
+               if si < di:
+                   first_lock = si
+                   second_lock = di
+               elif di < si:
+                   first_lock = di
+                   second_lock = si
+               else:
+                   first_lock = si
+                   same_lock = 1
+
                if counts[src[idx]] > 0:
+
+                   acquire(&locks[first_lock])
+                   if not same_lock:
+                       acquire(&locks[second_lock])
+                       
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+
+                   if not same_lock:
+                       release(&locks[second_lock])
+                       release(&locks[first_lock])
+                   else:
+                       release(&locks[first_lock])
 
    free_N_locks(num_locks, locks)
