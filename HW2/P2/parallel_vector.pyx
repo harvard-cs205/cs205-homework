2e965fbe2c30851decb5c98070f09da630ae65b9
Dana Katzenelson <dkatzenelson@college.harvard.edu>
2015-10-22 15:46:23 -0500

    P2 complete I think

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -70,23 +70,46 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
                              int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+    cdef:
+        int idx, r, src_index, dest_index, locks_count
+        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for each element of counts to parallelize
-   # data movement.  Be sure to avoid deadlock, and double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    ##########
+    # Your code here
+    # Use parallel.prange() and a lock for each element of counts to parallelize
+    # data movement.  Be sure to avoid deadlock, and double-locking.
+    ##########
+    with nogil:
+        for r in range(repeat):
+            for idx in prange(src.shape[0], num_threads=4):
+                src_index = src[idx]
+                dest_index = dest[idx]
+                locks_count = 0
+                
+                #Acquire locks in consistent order prior to checking value > 0
+                if src_index < dest_index:
+                    acquire(&locks[src_index])
+                    acquire(&locks[dest_index])
+                    locks_count = 2
+                elif dest_index < src_index:
+                    acquire(&locks[dest_index])
+                    acquire(&locks[src_index])
+                    locks_count = 2
+                else:
+                    acquire(&locks[src_index])
+                    locks_count = 1
+                
+                #Move value
+                if counts[src_index] > 0:
+                    counts[dest_index] += 1
+                    counts[src_index] -= 1
+                
+                #Release locks
+                release(&locks[src_index])
+                if locks_count == 2:
+                    release(&locks[dest_index])
 
-   free_N_locks(counts.shape[0], locks)
+    free_N_locks(counts.shape[0], locks)
 
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
@@ -95,7 +118,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, src_lock_num, dest_lock_num, locks_count
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
@@ -107,9 +130,32 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+            for idx in prange(src.shape[0], num_threads=4):
+                src_lock_num = src[idx]/N
+                dest_lock_num = dest[idx]/N
+                locks_count = 0
+                
+                #Acquire locks in consistent order prior to checking value > 0
+                if src_lock_num < dest_lock_num:
+                    acquire(&locks[src_lock_num])
+                    acquire(&locks[dest_lock_num])
+                    locks_count = 2
+                elif dest_lock_num < src_lock_num:
+                    acquire(&locks[dest_lock_num])
+                    acquire(&locks[src_lock_num])
+                    locks_count = 2
+                else:
+                    acquire(&locks[src_lock_num])
+                    locks_count = 1
+                
+                #Move value
+                if counts[src_lock_num] > 0:
+                    counts[dest_lock_num] += 1
+                    counts[src_lock_num] -= 1
+                
+                #Release locks
+                release(&locks[src_lock_num])
+                if locks_count == 2:
+                    release(&locks[dest_lock_num])
 
    free_N_locks(num_locks, locks)
