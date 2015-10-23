a8879dc2f84aa029415dd99840b648f4925d6774
k-ye <kuangye.bj@gmail.com>
2015-10-23 15:46:27 -0400

    P4 full credit done, extra not impl yet

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -21,7 +21,16 @@ def py_median_3x3(image, iterations=10, num_threads=1):
     tmpB = np.empty_like(tmpA)
 
     for i in range(iterations):
-        filtering.median_3x3(tmpA, tmpB, 0, 1)
+        threadList = []
+        for threadidx in xrange(num_threads):
+            th = threading.Thread(target=filtering.median_3x3, 
+                                    args=(tmpA, tmpB, threadidx, num_threads))
+            th.start()
+            threadList.append(th)
+
+        for th in threadList:
+            th.join()
+        # filtering.median_3x3(tmpA, tmpB, 0, 1)
         # swap direction of filtering
         tmpA, tmpB = tmpB, tmpA
 
