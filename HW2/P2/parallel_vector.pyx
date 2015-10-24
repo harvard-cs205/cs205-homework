e52348714e2f1a70eaf0a542cd1019a904beb782
Yj Cheng <cheng.yungjen@gmail.com>
2015-10-23 22:45:28 -0400

    P2

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -81,10 +81,14 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
                if counts[src[idx]] > 0:
+                   acquire(&locks[dest[idx]])
                    counts[dest[idx]] += 1
+                   release(&locks[dest[idx]])
+                   acquire(&locks[src[idx]])
                    counts[src[idx]] -= 1
+                   release(&locks[src[idx]])               
 
    free_N_locks(counts.shape[0], locks)
 
@@ -107,9 +111,13 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
                if counts[src[idx]] > 0:
+                   acquire(&locks[dest[idx]/N])
                    counts[dest[idx]] += 1
+                   release(&locks[dest[idx]/N])
+                   acquire(&locks[src[idx]/N])
                    counts[src[idx]] -= 1
+                   release(&locks[src[idx]/N]) 
 
    free_N_locks(num_locks, locks)
