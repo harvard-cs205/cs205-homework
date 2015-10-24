39bfcba7810ed9fa957c3c41ed484b8f970a6ffd
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-10-24 12:04:57 -0400

    Solved the simpler problem, i.e. non-extra credit. The extra credit one is ridiculously hard as we found yesterday...unless I am missing something!

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -9,117 +9,43 @@ import pyximport
 pyximport.install()
 
 import numpy as np
+import matplotlib
+matplotlib.use('qt4agg')
 import pylab
 
 import filtering
 from timer import Timer
 import threading
 
-from or_event import OrEvent
-from threadpool import ThreadPool
+class Worker(threading.Thread):
 
-class Row_Handler():
-    def __init__(self, row_num, row_lock, tmpA, tmpB, num_iterations=10):
-        self.i = 0
-
-        self.row_num = row_num
-        self.row_lock = row_lock
-
-        self.above_handler = None
-        self.below_handler = None
-
-        # Run when you are ready to go!
-        self.i_updated = threading.Event()
+    def __init__(self, offset, step, tmpA, tmpB):
+        # Initialize the super class
+        threading.Thread.__init__(self)
+        self.offset = offset
+        self.step = step
 
         self.tmpA = tmpA
         self.tmpB = tmpB
-        self.num_iterations = num_iterations
 
-
-    def go(self): # Locks only depend on above you
-        print 'Go!' , self.row_num
-        go_cond_1 = True
-        if self.above_handler is not None:
-            go_cond_1 = (self.i == self.above_handler.i)
-        go_cond_2 = True
-        if self.below_handler is not None:
-            go_cond_2 = (self.i == self.below_handler.i)
-        if go_cond_1 and go_cond_2:
-            print 'Acquring my own lock!' , self.row_num
-            self.row_lock.acquire()
-            if self.above_handler is not None:
-                print 'Acquiring upper lock!', self.row_num
-                self.above_handler.row_lock.acquire()
-
-            print 'Running the median filter', self.row_num
-
-            # Do stuff
-            filtering.median_3x3_row(self.tmpA, self.tmpB, self.row_num)
-            # Swap tmpA and tmpB; doesn't recreate arrays, just swaps pointers!
-            potatoA = self.tmpA
-            potatoB = self.tmpB
-
-            self.tmpA = potatoB
-            self.tmpB = potatoA
-
-            self.i += 1
-
-            print 'Releasing my own lock'
-            self.row_lock.release()
-            if self.above_handler is not None:
-                print 'Releasing upper lock'
-                self.above_handler.row_lock.release()
-
-            # Give other threads a chance to grab locks and update
-            self.i_updated.set()
-            self.i_updated.clear()
-        elif self.i != self.num_iterations: # you are not done yet!
-            self.wait()
-
-    def wait(self):
-        """Wait until one of your neighbors updates their iteration"""
-        if self.above_handler is None:
-            print 'Waiting for below handler to update...' , self.row_num
-            self.below_handler.i_updated.wait()
-            print 'Below handler updated!' , self.row_num
-        elif self.below_handler is None:
-            print 'Waiting for above handler to update...' , self.row_num
-            self.above_handler.i_updated.wait()
-            print 'Above handler updated!' , self.row_num
-        else:
-            print 'Waiting for above or below to update...' , self.row_num
-            OrEvent(self.above_handler.i_updated, self.below_handler.i_updated).wait()
-            print 'Below or Above handler updated!' , self.row_num
-        self.go()
+    def run(self):
+        filtering.median_3x3(self.tmpA, self.tmpB, self.offset, self.step)
 
 def py_median_3x3(image, iterations=10, num_threads=1):
     ''' repeatedly filter with a 3x3 median '''
     tmpA = image.copy()
     tmpB = np.empty_like(tmpA)
 
-    # Create all row_handlers
-    handler_list = []
-    row_lock = threading.Lock() # Assign neighbors all the same lock
-    for cur_row in range(image.shape[0]):
-        handler_list.append(Row_Handler(cur_row, row_lock, tmpA, tmpB, num_iterations=iterations))
-        if (cur_row - 2) % 3 == 0:
-            row_lock = threading.Lock()
+    # Create numthreads for each iteration...they only run once.
+    for i in range(iterations):
+        thread_list = []
+        for i in range(num_threads):
+            thread_list.append(Worker(i, num_threads, tmpA, tmpB))
 
-    # Add neighbors
-    for i in range(len(handler_list)):
-        if i != len(handler_list) - 1:
-            handler_list[i].below_handler =handler_list[i+1]
-        if i != 0:
-            handler_list[i].above_handler = handler_list[i-1]
+        map(lambda x: x.start(), thread_list)
+        map(lambda x: x.join(), thread_list)
 
-    # The handlers must each be updated ten times...if we had a thread for each handler we would be all set...
-    # we can probably subdivide the jobs up.
-
-    pool = ThreadPool(num_threads)
-    for handler in handler_list:
-        pool.add_task(handler.go)
-
-    pool.wait_completion()
+        tmpA, tmpB = tmpB, tmpA
 
     return tmpA
 
