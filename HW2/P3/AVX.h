e93aca47afed773517c6270086d1c0e572eaedd8
TmyJYGL8rAFhe9XFZUkD <e6eddc26@opayq.com>
2015-10-24 21:29:12 -0400

    Completed problem 3

--- HW2/P3/AVX.h
+++ HW2/P3/AVX.h
@@ -25,6 +25,7 @@ typedef __m256 float8;
 #define sqrt(val)             _mm256_sqrt_ps(val)
 #define mul(a, b)             _mm256_mul_ps((a), (b))
 #define add(a, b)             _mm256_add_ps((a), (b))
+#define sub(a, b)             _mm256_sub_ps((a), (b))
 #define div(a, b)             _mm256_div_ps((a), (b))
 //Bitwise
 #define bitwise_and(a, b)     _mm256_and_ps((a), (b))
