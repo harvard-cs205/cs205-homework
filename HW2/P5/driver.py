dfe3a8d143bdfd50163854e892ba058dd890ec44
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-10-25 16:39:44 -0400

    For my sanity, I am going to add code to sort the colors.

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -24,8 +24,8 @@ def randcolor():
     return np.random.uniform(0.0, 0.89, (3,)) + 0.1
 
 if __name__ == '__main__':
-    num_balls = 10**4
-    radius = 0.002
+    num_balls = 500
+    radius = 0.01
     positions = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
 
