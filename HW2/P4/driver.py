6f1945fcb6208eae60cbee3a0f2779fcab2d242d
Charles Liu <therealcharlesliu@gmail.com>
2015-10-24 22:37:29 -0400

    writeup P4

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -21,22 +21,21 @@ def py_median_3x3(image, iterations=10, num_threads=1):
     tmpB = np.empty_like(tmpA)
     locks = [None]*num_threads
     calc_count = [0]*num_threads
-    copy_count = [0]*num_threads
+    swap_count = [0]*num_threads
     threads = [None]*num_threads
     for i in range(num_threads):
         locks[i] = threading.Condition()
     
     for i in range(num_threads):
-        threads[i] = threading.Thread(target=median_worker, args=(i, iterations, tmpA, tmpB, locks, calc_count, copy_count))
+        threads[i] = threading.Thread(target=median_worker, args=(i, iterations, tmpA, tmpB, locks, calc_count, swap_count))
         threads[i].start()
         
     for t in threads:
         t.join()
-    print calc_count
-    print copy_count
+        
     return tmpA
 
-def median_worker(thread_num, iterations, tmpA, tmpB, locks, calc_count, copy_count):
+def median_worker(thread_num, iterations, tmpA, tmpB, locks, calc_count, swap_count):
     neighbor_threads = set([thread_num-1 if thread_num > 0 else len(locks)-1, thread_num + 1 if thread_num < len(locks) - 1 else 0])
     for i in range(iterations):  
         #can't calculate until neighbor threads have been copied to the right iteration
@@ -45,7 +44,7 @@ def median_worker(thread_num, iterations, tmpA, tmpB, locks, calc_count, copy_co
             if j != thread_num:
                 locks[j].acquire()
                 while True:
-                    if copy_count[j] == copy_count[thread_num]:
+                    if swap_count[j] == swap_count[thread_num]:
                         break
                     locks[j].wait()  
         for j in neighbor_threads:
@@ -55,7 +54,7 @@ def median_worker(thread_num, iterations, tmpA, tmpB, locks, calc_count, copy_co
             filtering.median_3x3(tmpA, tmpB, thread_num, len(locks))
             calc_count[thread_num] += 1
             locks[thread_num].notifyAll()
-            print "{}: Calculated {}".format(thread_num, i)
+            #print "{}: Calculated {}".format(thread_num, i)
         
         #can't copy until neighbor threads have calculated to the right iteration
         #if they haven't been calculated yet, wait on the corresponding locks until notified
@@ -72,9 +71,9 @@ def median_worker(thread_num, iterations, tmpA, tmpB, locks, calc_count, copy_co
         with locks[thread_num]:
             for j in range(thread_num, len(tmpA), len(locks)):
                 tmpA[j],tmpB[j] = tmpB[j],tmpA[j]
-            copy_count[thread_num] +=1
+            swap_count[thread_num] +=1
             locks[thread_num].notifyAll()
-            print "{}: Copied {}".format(thread_num, i)
+            #print "{}: Copied {}".format(thread_num, i)
 
 def numpy_median(image, iterations=10):
     ''' filter using numpy '''
@@ -91,28 +90,36 @@ def numpy_median(image, iterations=10):
 if __name__ == '__main__':
     input_image = np.load('image.npz')['image'].astype(np.float32)
 
-    #pylab.gray()
+    pylab.gray()
 
-    #pylab.imshow(input_image)
-    #pylab.title('original image')
+    pylab.imshow(input_image)
+    pylab.title('original image')
 
-    #pylab.figure()
-    #pylab.imshow(input_image[1200:1800, 3000:3500])
-    #pylab.title('before - zoom')
+    pylab.figure()
+    pylab.imshow(input_image[1200:1800, 3000:3500])
+    pylab.title('before - zoom')
 
     # verify correctness
-    from_cython = py_median_3x3(input_image,2, 5)
-    from_numpy = numpy_median(input_image, 2)
+    from_cython = py_median_3x3(input_image,10, 20)
+    from_numpy = numpy_median(input_image, 10)
     assert np.all(from_cython == from_numpy)
 
-    
-    with Timer() as t:
-        new_image = py_median_3x3(input_image, 10, 8)
+    thread_arr = [1,2,4,8]
+    thread_times = []
+    for threads in thread_arr:
+        with Timer() as t:
+            new_image = py_median_3x3(input_image, 10, threads)    
+        print("{} seconds for 10 filter passes.".format(t.interval))
+        thread_times.append(t.interval)
 
     pylab.figure()
     pylab.imshow(new_image[1200:1800, 3000:3500])
     pylab.title('after - zoom')
-
-    print("{} seconds for 10 filter passes.".format(t.interval))
     pylab.show()
     
+    pylab.figure()
+    pylab.plot(thread_arr, thread_times)
+    pylab.xlabel("Threads")
+    pylab.ylabel("Seconds")
+    pylab.title('Thread Performance')
+    pylab.show()
