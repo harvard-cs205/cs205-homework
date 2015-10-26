697c2be9d195f709aef9e74e8fce90b3c4393e38
patday9 <pday9@g.harvard.edu>
2015-10-26 18:38:12 -0400

    Adding completed HW2.P2

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -1,12 +1,10 @@
 # turn off bounds checking & wraparound for arrays
-#cython: boundscheck=False, wraparound=False
+# cython: boundscheck=False, wraparound=False
 
 ##################################################
 # setup and helper code
 ##################################################
-
-
-from cython.parallel import parallel, prange
+from cython.parallel import parallel, prange, threadid
 from openmp cimport omp_lock_t, \
     omp_init_lock, omp_destroy_lock, \
     omp_set_lock, omp_unset_lock, omp_get_thread_num
@@ -14,7 +12,6 @@ from libc.stdlib cimport malloc, free
 
 import numpy as np
 cimport numpy as np
-
 
 # lock helper functions
 cdef void acquire(omp_lock_t *l) nogil:
@@ -45,11 +42,13 @@ cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
 
     free(<void *> locks)
 
+# cdef omp_lock_t check_lock(idx, src, dest) nogil:
+#     if src[idx]>dest[idx]
+
 
 ##################################################
 # Your code below
 ##################################################
-
 cpdef move_data_serial(np.int32_t[:] counts,
                        np.int32_t[:] src,
                        np.int32_t[:] dest,
@@ -65,7 +64,7 @@ cpdef move_data_serial(np.int32_t[:] counts,
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
 
-
+# Updated move_data_fine_grained for parallel implementation
 cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
@@ -79,12 +78,31 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
+
+   for r in prange(repeat, nogil=True, num_threads=4):
+       for idx in xrange(src.shape[0]): 
+           if counts[src[idx]] > 0:
+               if src[idx] > dest[idx]:
+                   acquire(&locks[src[idx]])
+                   acquire(&locks[dest[idx]])
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+                   release(&locks[src[idx]])
+                   release(&locks[dest[idx]])
+         
+               elif src[idx] < dest[idx]:
+                   acquire(&locks[dest[idx]])
+                   acquire(&locks[src[idx]])
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+                   release(&locks[src[idx]])
+                   release(&locks[dest[idx]])
+
+               else:
+                   acquire(&locks[src[idx]]) 
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+                   release(&locks[src[idx]])
 
    free_N_locks(counts.shape[0], locks)
 
@@ -105,11 +123,42 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
+   
+   # print (num_locks)
+   for r in prange(repeat, nogil=True, num_threads=4):
+       for idx in xrange(src.shape[0]): 
+           if counts[src[idx]] > 0:
+               if src[idx] > dest[idx]:
+                   with gil:
+                       print ("src > dest", src[idx]/N, dest[idx]/N)
+                   acquire(&locks[src[idx]/N])
+                   acquire(&locks[dest[idx]/N])
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+                   release(&locks[src[idx]/N])
+                   release(&locks[dest[idx]/N])
+         
+               elif src[idx] < dest[idx]:
+                   acquire(&locks[dest[idx]/N])
+                   acquire(&locks[src[idx]/N])
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+                   release(&locks[src[idx]/ N])
+                   release(&locks[dest[idx]/ N])
+
+               else:
+                   acquire(&locks[src[idx]/ N]) 
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+                   release(&locks[src[idx]/ N])
+
+   # with nogil:
+   #     for r in range(repeat):
+   #         for idx in range(src.shape[0]):
+   #             if counts[src[idx]] > 0:
+   #                 counts[dest[idx]] += 1
+   #                 counts[src[idx]] -= 1
+
+
 
    free_N_locks(num_locks, locks)
