6db1ed8be532ac3231acb8fb68b34df57438d90b
Yj Cheng <cheng.yungjen@gmail.com>
2015-10-27 03:47:04 -0400

    subproblem 2

--- HW2/P5/physics.pyx
+++ HW2/P5/physics.pyx
@@ -196,7 +196,53 @@ cpdef update(FLOAT[:, ::1] XY,
             cuttail(XYG, grid_size)
             Grid[XYG[0], XYG[1]] = i
 
+## Hilbert_curve
+#convert (x,y) to d
+cdef int xy2d (int n, int x, int y) {
+    cdef:
+        int rx, ry, s, d=0;
+    for (s=n/2; s>0; s/=2) {
+        rx = (x & s) > 0;
+        ry = (y & s) > 0;
+        d += s * s * ((3 * rx) ^ ry);
+        rot(s, &x, &y, rx, ry);
+    }
+    return d;
+}
 
+#convert d to (x,y)
+cdef void d2xy(int n, int d, int *x, int *y) {
+    cdef:
+        int rx, ry, s, t=d;
+    *x = *y = 0;
+    for (s=1; s<n; s*=2) {
+        rx = 1 & (t/2);
+        ry = 1 & (t ^ rx);
+        rot(s, x, y, rx, ry);
+        *x += s * rx;
+        *y += s * ry;
+        t /= 4;
+    }
+}
+
+#rotate/flip a quadrant appropriately
+cdef void rot(int n, int *x, int *y, int rx, int ry) {
+    cdef:
+        int t
+    if (ry == 0) {
+        if (rx == 1) {
+            *x = n-1 - *x;
+            *y = n-1 - *y;
+        }
+
+        #Swap x and y
+        t  = *x;
+        *x = *y;
+        *y = t;
+    }
+}
+            
+            
 def preallocate_locks(num_locks):
     cdef omp_lock_t *locks = get_N_locks(num_locks)
     assert 0 != <uintptr_t> <void *> locks, "could not allocate locks"
