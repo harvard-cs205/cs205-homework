7fceb6b199cd29bb507013ae0e0ca3bd7a7a7ab4
Kai Sheng <ksheng@kais-mbp.attlocal.net>
2015-10-29 16:03:08 -0400

    solution to P4

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -27,6 +27,33 @@ def py_median_3x3(image, iterations=10, num_threads=1):
 
     return tmpA
 
+# the multithread version
+def py_median_3x3_multi(image, iterations=10, num_threads=4):
+    tmpA = image.copy()
+    tmpB = np.empty_like(tmpA)
+    
+    def py_median_3x3_single(tmpA, tmpB, idx):
+        filtering.median_3x3(tmpA, tmpB, idx, num_threads)
+    
+    for i in range(iterations):
+        thlist = []
+        for idx in range(num_threads):
+            # create threads
+            th = threading.Thread(target = py_median_3x3_single,
+                                  args = (tmpA, tmpB, idx))
+            th.daemon = True
+            thlist.append(th)
+            # start to run
+            th.start()
+
+        # join all threads to main when finished
+        for th in thlist:
+            th.join()
+
+        tmpA, tmpB = tmpB, tmpA
+
+    return tmpA
+
 def numpy_median(image, iterations=10):
     ''' filter using numpy '''
     for i in range(iterations):
@@ -52,16 +79,23 @@ if __name__ == '__main__':
     pylab.title('before - zoom')
 
     # verify correctness
-    from_cython = py_median_3x3(input_image, 2, 5)
+    from_cython = py_median_3x3_multi(input_image, 2, 5)
     from_numpy = numpy_median(input_image, 2)
     assert np.all(from_cython == from_numpy)
 
     with Timer() as t:
         new_image = py_median_3x3(input_image, 10, 8)
 
+    print("{} seconds for serial 10 filter passes.".format(t.interval))
+    pylab.show()
+
+    for num_threads in [1, 2, 4]:
+        with Timer() as t:
+            new_image = py_median_3x3_multi(input_image, 10, num_threads)
+        print 'num_threads =', num_threads
+        print("{} seconds for multithread 10 filter passes.".format(t.interval))
+
     pylab.figure()
     pylab.imshow(new_image[1200:1800, 3000:3500])
     pylab.title('after - zoom')
-
-    print("{} seconds for 10 filter passes.".format(t.interval))
     pylab.show()
