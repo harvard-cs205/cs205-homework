8265711911f11128048289115de0af107255dfeb
Sami Ghoche <samighoche@college.harvard.edu>
2015-10-29 16:50:24 -0400

    almost done with P2, still have to make a graph that depends on N

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -51,65 +51,93 @@ cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
 ##################################################
 
 cpdef move_data_serial(np.int32_t[:] counts,
-                       np.int32_t[:] src,
-                       np.int32_t[:] dest,
-                       int repeat):
-   cdef:
-       int idx, r
+                      np.int32_t[:] src,
+                      np.int32_t[:] dest,
+                      int repeat):
+  cdef:
+      int idx, r
 
-   assert src.size == dest.size, "Sizes of src and dest arrays must match"
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+  assert src.size == dest.size, "Sizes of src and dest arrays must match"
+  with nogil:
+      for r in range(repeat):
+          for idx in range(src.shape[0]):
+              if counts[src[idx]] > 0:
+                  counts[dest[idx]] += 1
+                  counts[src[idx]] -= 1
 
 
 cpdef move_data_fine_grained(np.int32_t[:] counts,
-                             np.int32_t[:] src,
-                             np.int32_t[:] dest,
-                             int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+                            np.int32_t[:] src,
+                            np.int32_t[:] dest,
+                            int repeat):
+  cdef:
+      int idx, r, same, src_index, dest_index
+      omp_lock_t *locks = get_N_locks(counts.shape[0])
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for each element of counts to parallelize
-   # data movement.  Be sure to avoid deadlock, and double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+  ##########
+  # Your code here
+  # Use parallel.prange() and a lock for each element of counts to parallelize
+  # data movement.  Be sure to avoid deadlock, and double-locking.
+  ##########
+  for r in range(repeat):
+      for idx in prange(src.shape[0], nogil=True, num_threads=4, schedule=dynamic):
+          same = 0
+          src_index = src[idx]
+          dest_index = dest[idx]
+          if src_index < dest_index:
+            acquire(&(locks[src_index]))
+            acquire(&(locks[dest_index]))
+          elif dest_index < src_index:
+            acquire(&(locks[dest_index]))
+            acquire(&(locks[src_index]))
+          else:
+            acquire(&(locks[src_index]))
+            same = 1
+          if counts[src_index] > 0:
+              counts[dest_index] += 1
+              counts[src_index] -= 1
+          release(&(locks[src_index]))
+          if not same:
+            release(&(locks[dest_index]))
 
-   free_N_locks(counts.shape[0], locks)
+  free_N_locks(counts.shape[0], locks)
 
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
-                               np.int32_t[:] src,
-                               np.int32_t[:] dest,
-                               int repeat,
-                               int N):
-   cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+                              np.int32_t[:] src,
+                              np.int32_t[:] dest,
+                              int repeat,
+                              int N):
+  cdef:
+      int idx, r, same, src_index, dest_index
+      int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+      omp_lock_t *locks = get_N_locks(num_locks)
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for every N adjacent elements of counts
-   # to parallelize data movement.  Be sure to avoid deadlock, as well as
-   # double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+  ##########
+  # Your code here
+  # Use parallel.prange() and a lock for every N adjacent elements of counts
+  # to parallelize data movement.  Be sure to avoid deadlock, as well as
+  # double-locking.
+  ##########
+  for r in range(repeat):
+      for idx in prange(src.shape[0], nogil=True, num_threads=4, schedule=dynamic):
+          same = 0
+          src_index = src[idx]/N
+          dest_index = dest[idx]/N
+          if src_index < dest_index:
+            acquire(&(locks[src_index]))
+            acquire(&(locks[dest_index]))
+          elif dest_index < src_index:
+            acquire(&(locks[dest_index]))
+            acquire(&(locks[src_index]))
+          else:
+            acquire(&(locks[src_index]))
+            same = 1
+          if counts[src_index] > 0:
+              counts[dest_index] += 1
+              counts[src_index] -= 1
+          release(&(locks[src_index]))
+          if not same:
+            release(&(locks[dest_index]))
 
-   free_N_locks(num_locks, locks)
+  free_N_locks(num_locks, locks)
