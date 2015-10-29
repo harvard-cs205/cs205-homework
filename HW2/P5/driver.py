fef82b83762210d6ea7c4b2fd76f9f1ea9a2c7d0
Taylor Killian <taylorkillian@g.harvard.edu>
2015-10-29 05:03:31 -0400

    Finished P5, with deadlocked errors in the final part... Will return and address if I can before due date

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -16,9 +16,25 @@ from physics import update, preallocate_locks
 def randcolor():
     return np.random.uniform(0.0, 0.89, (3,)) + 0.1
 
+######################################## Z-order Curve###################################
+def cmp_zorder(a, b): #Pulled from https://en.wikipedia.org/wiki/Z-order_curve
+    j = 0
+    k = 0
+    x = 0
+    for k in range(2):
+        y = a[k] ^ b[k]
+        if less_msb(x, y):
+            j = k
+            x = y
+    return a[j] - b[j]
+    
+def less_msb(x, y):
+        return x < y and x < (x ^ y)
+#########################################################################################
+
 if __name__ == '__main__':
-    num_balls = 10000
-    radius = 0.002
+    num_balls = 10000 #500
+    radius = 0.002 #0.01
     positions = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
 
@@ -64,7 +80,7 @@ if __name__ == '__main__':
     while True:
         with Timer() as t:
             update(positions, velocities, grid,
-                   radius, grid_size, locks_ptr,
+                   radius, grid_spacing, locks_ptr,
                    physics_step)
 
         # udpate our estimate of how fast the simulator runs
@@ -80,3 +96,21 @@ if __name__ == '__main__':
             # SUBPROBLEM 3: sort objects by location.  Be sure to update the
             # grid if objects' indices change!  Also be sure to sort the
             # velocities with their object positions!
+            
+            # Get Positions and sort the points according to morton ordering
+            grid_positions = (positions / grid_spacing).astype(int) # Maintain grid spacing
+            zOrderCurve = map(lambda x, y: cmp_zorder(x,y), grid_positions, np.array(list(grid_positions[1:]) + list(grid_positions[:1])))
+            positions_index = np.argsort(zOrderCurve)
+
+            # With np.argsort() change Grid and velocity indices
+            positions = np.array(positions)[positions_index]
+            velocities = np.array(velocities)[positions_index]
+            
+            # Reset Grid
+            grid[:,:] = -1
+            
+            # Set new grid locations
+            for i in range(num_balls):
+                if positions[i,0]>=0 and positions[i,1]>=0 and positions[i,0]<=1 and positions[i,1]<=1:
+                    grid[(positions[i,0] / grid_spacing).astype(int),
+                         (positions[i,1] / grid_spacing).astype(int)] = i
