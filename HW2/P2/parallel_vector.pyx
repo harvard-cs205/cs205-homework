f5dd34a89b5f539b30f1aec35c106f4efc5d5491
xdai1091 <xdai1091@gmail.com>
2015-10-29 23:23:19 -0400

    just get the p2 done, so sad

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -79,13 +79,17 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
-
+   for r in range(repeat):
+       for idx in prange(src.shape[0],num_threads=4, nogil= True, schedule=dynamic):
+           #acquire locks here
+           if counts[src[idx]] > 0:
+               acquire(&(locks[dest[idx]]));
+               counts[dest[idx]] += 1
+               release(&(locks[dest[idx]]));
+               acquire(&(locks[src[idx]]));
+               counts[src[idx]] -= 1
+               #free the lock here
+               release(&(locks[src[idx]]));
    free_N_locks(counts.shape[0], locks)
 
 
@@ -95,7 +99,8 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, sidx, didx
+
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
@@ -105,11 +110,26 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
+   for r in range(repeat):
+       for idx in prange(src.shape[0],num_threads=4, nogil= True, schedule=dynamic):
+           #check the idx
+
+           sidx = src[idx]/N
+           didx = dest[idx]/N
+
+           if (sidx != didx):
                if counts[src[idx]] > 0:
+                   acquire(&(locks[didx]))
+                   counts[dest[idx]] += 1
+                   release(&(locks[didx]))
+                   acquire(&(locks[sidx]))
+                   counts[src[idx]] -= 1
+                   release(&(locks[sidx]))
+           else:
+               if counts[src[idx]] > 0:
+                   acquire(&(locks[didx]))
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+                   release(&(locks[didx]))
 
    free_N_locks(num_locks, locks)
