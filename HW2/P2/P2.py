56d26fde9359b383832543aea31e210ec10107b4
ymiyamot <yohsuke.miyamoto@gmail.com>
2015-10-30 02:06:21 -0400

    Finished P2

--- HW2/P2/P2.py
+++ HW2/P2/P2.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# %load P2.py
 import sys
 sys.path.append('../util')
 
@@ -5,78 +11,151 @@ import set_compiler
 set_compiler.install()
 
 import pyximport
-pyximport.install()
+pyximport.install(reload_support=True)
 
 import numpy as np
 from timer import Timer
 from parallel_vector import move_data_serial, move_data_fine_grained, move_data_medium_grained
 
-if __name__ == '__main__':
-    ########################################
-    # Generate some test data, first, uncorrelated
-    ########################################
-    orig_counts = np.arange(1000, dtype=np.int32)
-    src = np.random.randint(1000, size=1000000).astype(np.int32)
-    dest = np.random.randint(1000, size=1000000).astype(np.int32)
 
-    total = orig_counts.sum()
+# In[150]:
 
-    # serial move
-    counts = orig_counts.copy()
-    with Timer() as t:
-        move_data_serial(counts, src, dest, 100)
-    assert counts.sum() == total, "Wrong total after move_data_serial"
-    print("Serial uncorrelated: {} seconds".format(t.interval))
-    serial_counts = counts.copy()
+import parallel_vector
+import matplotlib.pyplot as plt
+import seaborn
 
-    # fine grained
+
+# In[130]:
+
+# if __name__ == '__main__':
+########################################
+# Generate some test data, first, uncorrelated
+########################################
+count_num = 1000
+orig_counts = np.arange(count_num, dtype=np.int32)
+src = np.random.randint(count_num, size=count_num**2).astype(np.int32)
+dest = np.random.randint(count_num, size=count_num**2).astype(np.int32)
+
+total = orig_counts.sum()
+
+# serial move
+counts = orig_counts.copy()
+
+
+# In[131]:
+
+with Timer() as t_serial:
+    move_data_serial(counts, src, dest, 100)
+assert counts.sum() == total, "Wrong total after move_data_serial"
+print("Serial uncorrelated: {} seconds".format(t_serial.interval))
+serial_counts = counts.copy()
+
+
+# In[132]:
+
+# fine grained
+counts[:] = orig_counts
+with Timer() as t_fine:
+    parallel_vector.move_data_fine_grained(counts, src, dest, 100)
+assert counts.sum() == total, "Wrong total after move_data_fine_grained"
+print("Fine grained uncorrelated: {} seconds".format(t_fine.interval))
+
+
+# In[134]:
+
+########################################
+# Explore different values for the number of locks
+########################################
+N_list = np.logspace(0, 3, 20)
+t_medium = []
+for N in N_list:
     counts[:] = orig_counts
     with Timer() as t:
-        move_data_fine_grained(counts, src, dest, 100)
-    assert counts.sum() == total, "Wrong total after move_data_fine_grained"
-    print("Fine grained uncorrelated: {} seconds".format(t.interval))
-
-    ########################################
-    # You should explore different values for the number of locks in the medium
-    # grained locking
-    ########################################
-    N = 10
-    counts[:] = orig_counts
-    with Timer() as t:
-        move_data_medium_grained(counts, src, dest, 100, N)
+        parallel_vector.move_data_medium_grained(counts, src, dest, 100, N)
     assert counts.sum() == total, "Wrong total after move_data_medium_grained"
-    print("Medium grained uncorrelated: {} seconds".format(t.interval))
+    t_medium.append(t.interval)
+#     print("Medium grained uncorrelated: {} seconds".format(t.interval))
 
-    ########################################
-    # Now use correlated data movement
-    ########################################
-    dest = src + np.random.randint(-10, 11, size=src.size)
-    dest[dest < 0] += 1000
-    dest[dest >= 1000] -= 1000
-    dest = dest.astype(np.int32)
 
-    # serial move
+# In[138]:
+
+########################################
+# Now use correlated data movement
+########################################
+dest = src + np.random.randint(-10, 11, size=src.size)
+dest[dest < 0] += 1000
+dest[dest >= 1000] -= 1000
+dest = dest.astype(np.int32)
+
+# serial move
+counts[:] = orig_counts
+with Timer() as t_corr_serial:
+    move_data_serial(counts, src, dest, 100)
+assert counts.sum() == total, "Wrong total after move_data_serial"
+print("Serial correlated: {} seconds".format(t_corr_serial.interval))
+serial_counts = counts.copy()
+
+
+# In[139]:
+
+# fine grained
+counts[:] = orig_counts
+with Timer() as t_corr_fine:
+    parallel_vector.move_data_fine_grained(counts, src, dest, 100)
+assert counts.sum() == total, "Wrong total after move_data_fine_grained"
+print("Fine grained correlated: {} seconds".format(t_corr_fine.interval))
+
+
+# In[140]:
+
+########################################
+# You should explore different values for the number of locks in the medium
+# grained locking
+########################################
+N_list = np.logspace(0, 3, 20)
+t_corr_medium = []
+for N in N_list:
     counts[:] = orig_counts
     with Timer() as t:
-        move_data_serial(counts, src, dest, 100)
-    assert counts.sum() == total, "Wrong total after move_data_serial"
-    print("Serial correlated: {} seconds".format(t.interval))
-    serial_counts = counts.copy()
-
-    # fine grained
-    counts[:] = orig_counts
-    with Timer() as t:
-        move_data_fine_grained(counts, src, dest, 100)
-    assert counts.sum() == total, "Wrong total after move_data_fine_grained"
-    print("Fine grained correlated: {} seconds".format(t.interval))
-
-    ########################################
-    # You should explore different values for the number of locks in the medium
-    # grained locking
-    ########################################
-    N = 10
-    counts[:] = orig_counts
-    with Timer() as t:
-        move_data_medium_grained(counts, src, dest, 100, N)
+        parallel_vector.move_data_medium_grained(counts, src, dest, 100, N)
     assert counts.sum() == total, "Wrong total after move_data_medium_grained"
-    print("Medium grained correlated: {} seconds".format(t.interval))
+    t_corr_medium.append(t.interval)
+# print("Medium grained correlated: {} seconds".format(t.interval))
+
+
+# In[158]:
+
+plt.figure()
+
+# Plot correlated transfer results
+plt.plot(1, t_corr_fine.interval / 100, 'ro')
+plt.plot(N_list, [x / 100 for x in t_corr_medium], 'k-')
+plt.plot([1, 1e3], 2 * [t_corr_serial.interval / 100], 'g-',
+        linewidth = 3)
+plt.xscale('log')
+plt.xlim([10**-0.2, 1500])
+plt.xlabel('Size of lock')
+plt.ylabel('Avg time to complete a million moves (sec)')
+plt.legend(['Fine-grained', 
+            'Medium-grained (with different lock sizes)', 
+            'Serial'], loc='lower center')
+
+# Plot uncorrelated transfer results
+plt.plot(1, t_fine.interval / 100, 'rs')
+plt.plot(N_list, [x / 100 for x in t_medium], 'k--')
+plt.plot([1, 1e3], 2 * [t_serial.interval / 100], 'g:',
+        linewidth = 3)
+plt.xscale('log')
+plt.xlim([10**-0.2, 1500])
+plt.xlabel('Log size of lock')
+plt.ylabel('Avg time to complete a million moves (sec)')
+plt.legend(['Fine-grained (correlated)', 
+            'Medium-grained (correlated)', 
+            'Serial (correlated)',
+            'Fine-grained (uncorrelated)', 
+            'Medium-grained (uncorrelated)', 
+            'Serial (uncorrelated)'], loc='upper left')
+plt.title('Comparison of different grain sizes\n on correlated and uncorrelated memory transfer')
+# plt.show()
+plt.savefig('P2_transfer_times.png')
+
--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -65,51 +65,96 @@ cpdef move_data_serial(np.int32_t[:] counts,
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
 
-
+############# FINE GRAINED #############
 cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
                              int repeat):
    cdef:
-       int idx, r
+       int idx, r, src_loc, dest_loc
+
+       int lock_num_src, lock_num_dest, N = 1 # Useless variables
+        
        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for each element of counts to parallelize
-   # data movement.  Be sure to avoid deadlock, and double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+#   print("Number of locks: {}".format(counts.shape[0]))
+   for r in range(repeat):
+       for idx in prange(src.shape[0], 
+                         nogil=True, 
+                         num_threads=4):
+            src_loc = src[idx]
+            dest_loc = dest[idx]
+
+            # Perform a useless computation here to match the computation 
+            # of the medium grained version
+            lock_num_src = (src_loc / N)
+            lock_num_dest = (dest_loc / N)
+
+            # If source and destination are the same, 
+            # there's nothing to do.
+            if src_loc != dest_loc: 
+                # To prevent deadlock, let's lock from the lower index
+                # This costs some overhead
+                if src_loc < dest_loc:
+                    acquire(&(locks[src_loc]))
+                    acquire(&(locks[dest_loc]))
+                elif src_loc > dest_loc:
+                    acquire(&(locks[dest_loc]))
+                    acquire(&(locks[src_loc]))
+                if counts[src_loc] > 0:
+                    counts[src_loc] -= 1
+                    counts[dest_loc] += 1
+                release(&(locks[src_loc]))
+                release(&(locks[dest_loc]))
 
    free_N_locks(counts.shape[0], locks)
 
-
+############# MEDIUM GRAINED #############
 cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] src,
                                np.int32_t[:] dest,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, src_loc, dest_loc, lock_num_src, lock_num_dest
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for every N adjacent elements of counts
-   # to parallelize data movement.  Be sure to avoid deadlock, as well as
-   # double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+#   print("Number of locks: {}".format(num_locks))
+
+        
+   for r in range(repeat):
+       for idx in prange(src.shape[0], 
+                         nogil=True, 
+                         num_threads=4):
+            src_loc = src[idx]
+            dest_loc = dest[idx]
+
+            lock_num_src = (src_loc / N)
+            lock_num_dest = (dest_loc / N)
+
+            # If source and destination are the same, 
+            # there's nothing to do.
+            if src_loc != dest_loc: 
+                # Lock, starting with the smaller index
+                if lock_num_src < lock_num_dest:
+                    acquire(&(locks[lock_num_src]))
+                    acquire(&(locks[lock_num_dest]))
+                elif lock_num_src > lock_num_dest:
+                    acquire(&(locks[lock_num_dest]))
+                    acquire(&(locks[lock_num_src]))
+                else:
+                    # Sometimes both source and destination are in the same lock
+                    acquire(&(locks[lock_num_src]))
+                if counts[src_loc] > 0:
+                    counts[src_loc] -= 1
+                    counts[dest_loc] += 1
+                    
+                # Release locks
+                if lock_num_src != lock_num_dest:
+                    release(&(locks[lock_num_src]))
+                    release(&(locks[lock_num_dest]))
+                else:
+                    release(&(locks[lock_num_src]))
 
    free_N_locks(num_locks, locks)
