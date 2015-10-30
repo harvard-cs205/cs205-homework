1281220119fa8d5430ccdea6a99b5a00bfd035f5
haosutang <haosutang@gmail.com>
2015-10-30 01:23:24 -0400

    Finished p2, working on p3.

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -1,6 +1,5 @@
 # turn off bounds checking & wraparound for arrays
 #cython: boundscheck=False, wraparound=False
-
 ##################################################
 # setup and helper code
 ##################################################
@@ -81,10 +80,15 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           #for idx in prange(src.shape[0]
+           for idx in prange(src.shape[0], num_threads=4):  
                if counts[src[idx]] > 0:
+                   acquire(&locks[dest[idx]])
                    counts[dest[idx]] += 1
+                   release(&locks[dest[idx]])
+                   acquire(&locks[src[idx]])
                    counts[src[idx]] -= 1
+                   release(&locks[src[idx]])
 
    free_N_locks(counts.shape[0], locks)
 
@@ -95,7 +99,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, destidx, srcidx
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
@@ -107,9 +111,21 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
                if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+                   if dest[idx]/N==src[idx]/N:
+                     acquire(&locks[dest[idx]/N])
+                     counts[dest[idx]] += 1
+                     counts[src[idx]] -= 1
+                     release(&locks[src[idx]/N])
+
+                   else:
+                     acquire(&locks[dest[idx]/N])
+                     counts[dest[idx]] += 1
+                     release(&locks[dest[idx]/N])
+                     acquire(&locks[src[idx]/N])
+                     counts[src[idx]] -= 1
+                     release(&locks[src[idx]/N])
+
 
    free_N_locks(num_locks, locks)
--- HW2/P3/mandelbrot.pyx
+++ HW2/P3/mandelbrot.pyx
@@ -17,6 +17,81 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
     cdef:
        int i, j, iter
        np.complex64_t c, z
+       AVX.float8 real_avx, img_avx, real_part, img_part, counter_avx, mask_avx, criteria_avx, increment_avx, c1, c2
+
+       # To declare AVX.float8 variables, use:
+       # cdef:
+       #     AVX.float8 v1, v2, v3
+       #
+       # And then, for example, to multiply them
+       #     v3 = AVX.mul(v1, v2)
+       #
+       # You may find the numpy.real() and numpy.imag() fuctions helpful.
+
+    assert in_coords.shape[1] % 8 == 0, "Input array must have 8N columns"
+    assert in_coords.shape[0] == out_counts.shape[0], "Input and output arrays must be the same size"
+    assert in_coords.shape[1] == out_counts.shape[1],  "Input and output arrays must be the same size"
+
+    temp_out_counts = np.zeros_like(out_counts).astype(float)
+
+
+    real = numpy.real(in_coords)
+    img = numpy.imag(in_coords)
+
+    print real.shape
+    with nogil:
+        for i in prange(4000, schedule='static', chunksize=1, num_threads=2):
+            for j in prange(4000/8, schedule='static', chunksize=1, num_threads=2):
+                with gil:
+                    print(i)
+                    print(j)
+                c1 = AVX.float_to_float8(numpy.real(in_coords[i, j]))
+                c2 = AVX.float_to_float8(numpy.imag(in_coords[i, j]))
+                real_avx = AVX.make_float8(real[i, j*8],
+                                           real[i, j*8+1],
+                                           real[i, j*8+2],
+                                           real[i, j*8+3],
+                                           real[i, j*8+4],
+                                           real[i, j*8+5],
+                                           real[i, j*8+6],
+                                           real[i, j*8+7])
+                img_avx = AVX.make_float8(img[i, j*8],
+                                          img[i, j*8+1],
+                                          img[i, j*8+2],
+                                          img[i, j*8+3],
+                                          img[i, j*8+4],
+                                          img[i, j*8+5],
+                                          img[i, j*8+6],
+                                          img[i, j*8+7])
+                counter_avx = AVX.float_to_float8(0)
+                criteria_avx = AVX.float_to_float8(4)
+
+                for iter in range(max_iterations):
+                    real_part = AVX.add(AVX.sub(AVX.mul(real_avx, real_avx), AVX.mul(real_avx, real_avx)), c1)
+                    img_part = AVX.add(AVX.mul(AVX.mul(real_avx, img_avx), AVX.float_to_float8(2)), c2)
+                    mask_avx = AVX.less_tan(AVX.add(AVX.mul(real_part, real_part), AVX.mul(img_part, img_part)), 
+                                            AVX.float_to_float8(4))
+                    if AVX.sign(mask_avx)==0.0:
+                        break
+                    increment_avx = AVX.bitwise_andnot(AVX.float_to_float8(1), mask_avx)
+                    counter_avx = AVX.add(counter_avx, increment_avx)
+                    real_avx = real_part
+                    img_avx = img_part
+
+                AVX.to_mem(counter_avx, &(temp_out_counts[i, j*8]))
+    out_counts = temp_out_counts.astype(int)
+
+
+
+
+
+
+cpdef mandelbrot_origin(np.complex64_t [:, :] in_coords,
+                 np.uint32_t [:, :] out_counts,
+                 int max_iterations=511):
+    cdef:
+       int i, j, iter
+       np.complex64_t c, z
 
        # To declare AVX.float8 variables, use:
        # cdef:
