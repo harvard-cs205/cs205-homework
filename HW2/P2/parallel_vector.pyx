c9833c72688763816c63ff27d8c8b4523dfde65e
Rohit Ahuja <rahuja95@gmail.com>
2015-10-30 21:34:31 -0400

    Finished P2. Still need to plot

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -73,6 +73,7 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    cdef:
        int idx, r
        omp_lock_t *locks = get_N_locks(counts.shape[0])
+       np.int32_t small, big
 
    ##########
    # Your code here
@@ -81,10 +82,18 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
+               small = min(src[idx], dest[idx])
+               big = max(src[idx], dest[idx])
+               if small == big:
+                 continue
+               acquire(&(locks[small]))
+               acquire(&(locks[big]))
                if counts[src[idx]] > 0:
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+               release(&(locks[small]))
+               release(&(locks[big]))
 
    free_N_locks(counts.shape[0], locks)
 
@@ -98,6 +107,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
        int idx, r
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
+       np.int32_t small, big
 
    ##########
    # Your code here
@@ -107,9 +117,20 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    ##########
    with nogil:
        for r in range(repeat):
-           for idx in range(src.shape[0]):
+           for idx in prange(src.shape[0], num_threads=4):
+               small = min(src[idx], dest[idx])
+               big = max(src[idx], dest[idx])
+               if small == big:
+                 continue
+               small = small/N
+               big = big/N
+               acquire(&(locks[small]))
+               if small != big:
+                acquire(&(locks[big]))
                if counts[src[idx]] > 0:
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
-
+               release(&(locks[small]))
+               if small != big:
+                release(&(locks[big]))
    free_N_locks(num_locks, locks)
