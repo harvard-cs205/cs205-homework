bf510a42979de5382318196c662cb03f15c9734b
George Lok <george.j.lok@gmail.com>
2015-10-31 23:06:36 -0400

    P2 Done

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -71,7 +71,7 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] dest,
                              int repeat):
    cdef:
-       int idx, r
+       int idx, r, min_idx, max_idx
        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
    ##########
@@ -79,12 +79,33 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+   for r in range(repeat):
+       for idx in prange(src.shape[0], nogil=True, schedule=dynamic, num_threads=4):
+           #  Corner case.  This doesn't do anything to the counts, but
+           #  will cause a double lock.
+           if src[idx] == dest[idx] :
+               continue
+
+           # Prevent deadlock by acquiring both potential locks
+           # in order of index size
+           if src[idx] < dest[idx] :
+               min_idx = src[idx]
+               max_idx = dest[idx]
+           else :
+               min_idx = dest[idx]
+               max_idx = src[idx]
+
+           acquire(&(locks[min_idx]))
+           acquire(&(locks[max_idx]))
+
+           if counts[src[idx]] > 0:
+               counts[dest[idx]] += 1
+               counts[src[idx]] -= 1
+
+           # Release locks.  Order of release doesn't matter
+           # in order to prevent deadlock
+           release(&(locks[max_idx]))
+           release(&(locks[min_idx]))
 
    free_N_locks(counts.shape[0], locks)
 
@@ -95,7 +116,7 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                int repeat,
                                int N):
    cdef:
-       int idx, r
+       int idx, r, l1, l2
        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
        omp_lock_t *locks = get_N_locks(num_locks)
 
@@ -105,11 +126,32 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
+   for r in range(repeat):
+       for idx in prange(src.shape[0], nogil=True, schedule=dynamic, num_threads=4):
+           if src[idx] == dest[idx] :
+               continue
+
+           # Prevent deadlock by acquiring both potential locks
+           # in order of index size
+           if src[idx] < dest[idx] :
+               l1 = src[idx] / N
+               l2 = dest[idx] / N
+           else :
+               l1 = dest[idx] / N
+               l2 = src[idx] / N
+           if l1 == l2 :
+               acquire(&(locks[l1]))
                if counts[src[idx]] > 0:
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
+               release(&(locks[l1])) 
+           else :
+               acquire(&(locks[l1]))
+               acquire(&(locks[l2])) 
+               if counts[src[idx]] > 0:
+                   counts[dest[idx]] += 1
+                   counts[src[idx]] -= 1
+               release(&(locks[l2]))
+               release(&(locks[l1]))
 
    free_N_locks(num_locks, locks)
