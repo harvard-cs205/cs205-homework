51cc21a6cc70832dc59edbe1464e68811564bbd5
jonahkall <kallenbach@college.harvard.edu>
2015-11-01 04:27:24 -0500

    P2 working, on to P3

--- HW2/P2/P2.py
+++ HW2/P2/P2.py
@@ -40,12 +40,12 @@ if __name__ == '__main__':
     # You should explore different values for the number of locks in the medium
     # grained locking
     ########################################
-    N = 10
-    counts[:] = orig_counts
-    with Timer() as t:
-        move_data_medium_grained(counts, src, dest, 100, N)
-    assert counts.sum() == total, "Wrong total after move_data_medium_grained"
-    print("Medium grained uncorrelated: {} seconds".format(t.interval))
+    for N in range(10, 51, 10):
+        counts[:] = orig_counts
+        with Timer() as t:
+            move_data_medium_grained(counts, src, dest, 100, N)
+        assert counts.sum() == total, "Wrong total after move_data_medium_grained"
+        print("Medium grained uncorrelated: {} seconds".format(t.interval))
 
     ########################################
     # Now use correlated data movement
@@ -74,9 +74,9 @@ if __name__ == '__main__':
     # You should explore different values for the number of locks in the medium
     # grained locking
     ########################################
-    N = 10
-    counts[:] = orig_counts
-    with Timer() as t:
-        move_data_medium_grained(counts, src, dest, 100, N)
-    assert counts.sum() == total, "Wrong total after move_data_medium_grained"
-    print("Medium grained correlated: {} seconds".format(t.interval))
+    for N in range(2, 40, 5):
+        counts[:] = orig_counts
+        with Timer() as t:
+            move_data_medium_grained(counts, src, dest, 100, N)
+        assert counts.sum() == total, "Wrong total after move_data_medium_grained"
+        print("Medium grained correlated: {} seconds".format(t.interval))
--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -70,23 +70,36 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
                              int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+  cdef:
+    int idx, r, minlock, maxlock
+    omp_lock_t *locks = get_N_locks(counts.shape[0])
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for each element of counts to parallelize
-   # data movement.  Be sure to avoid deadlock, and double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+  ##########
+  # Your code here
+  # Use parallel.prange() and a lock for each element of counts to parallelize
+  # data movement.  Be sure to avoid deadlock, and double-locking.
+  ##########
+  with nogil:
+    for r in range(repeat):
+      for idx in prange(src.shape[0], num_threads=4):
+        minlock = min(src[idx], dest[idx])
+        maxlock = max(src[idx], dest[idx])
+        if minlock == maxlock:
+          omp_set_lock(&locks[minlock])
+          if counts[src[idx]] > 0:
+            counts[dest[idx]] += 1
+            counts[src[idx]] -= 1
+          omp_unset_lock(&locks[minlock])
+        else:
+          omp_set_lock(&locks[minlock])
+          omp_set_lock(&locks[maxlock])
+          if counts[src[idx]] > 0:
+            counts[dest[idx]] += 1
+            counts[src[idx]] -= 1
+          omp_unset_lock(&locks[minlock])
+          omp_unset_lock(&locks[maxlock])
 
-   free_N_locks(counts.shape[0], locks)
+  free_N_locks(counts.shape[0], locks)
 
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
@@ -94,22 +107,35 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] dest,
                                int repeat,
                                int N):
-   cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+  cdef:
+    int idx, r, minlock, maxlock
+    int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+    omp_lock_t *locks = get_N_locks(num_locks)
 
-   ##########
-   # Your code here
-   # Use parallel.prange() and a lock for every N adjacent elements of counts
-   # to parallelize data movement.  Be sure to avoid deadlock, as well as
-   # double-locking.
-   ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+  ##########
+  # Your code here
+  # Use parallel.prange() and a lock for every N adjacent elements of counts
+  # to parallelize data movement.  Be sure to avoid deadlock, as well as
+  # double-locking.
+  ##########
+  with nogil:
+    for r in range(repeat):
+      for idx in prange(src.shape[0], num_threads=4):
+        minlock = min(src[idx]/N, dest[idx]/N)
+        maxlock = max(src[idx]/N, dest[idx]/N)
+        if minlock == maxlock:
+          omp_set_lock(&locks[minlock])
+          if counts[src[idx]] > 0:
+            counts[dest[idx]] += 1
+            counts[src[idx]] -= 1
+          omp_unset_lock(&locks[minlock])
+        else:
+          omp_set_lock(&locks[minlock])
+          omp_set_lock(&locks[maxlock])
+          if counts[src[idx]] > 0:
+            counts[dest[idx]] += 1
+            counts[src[idx]] -= 1
+          omp_unset_lock(&locks[minlock])
+          omp_unset_lock(&locks[maxlock])
 
-   free_N_locks(num_locks, locks)
+  free_N_locks(num_locks, locks)
