8ba234c9a13e7830c42804e533a2fa514c611e09
jonahkall <kallenbach@college.harvard.edu>
2015-11-01 15:01:32 -0500

    P2 touches

--- HW2/P2/P2.py
+++ HW2/P2/P2.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     orig_counts = np.arange(1000, dtype=np.int32)
     src = np.random.randint(1000, size=1000000).astype(np.int32)
     dest = np.random.randint(1000, size=1000000).astype(np.int32)
+    x = range(2,60,2)
 
     total = orig_counts.sum()
 
@@ -40,12 +41,14 @@ if __name__ == '__main__':
     # You should explore different values for the number of locks in the medium
     # grained locking
     ########################################
-    for N in range(10, 51, 10):
+    uncorr = []
+    for N in x:
         counts[:] = orig_counts
         with Timer() as t:
             move_data_medium_grained(counts, src, dest, 100, N)
         assert counts.sum() == total, "Wrong total after move_data_medium_grained"
         print("Medium grained uncorrelated: {} seconds".format(t.interval))
+        uncorr.append(t.interval)
 
     ########################################
     # Now use correlated data movement
@@ -74,9 +77,21 @@ if __name__ == '__main__':
     # You should explore different values for the number of locks in the medium
     # grained locking
     ########################################
-    for N in range(2, 40, 5):
+    corr = []
+    for N in x:
         counts[:] = orig_counts
         with Timer() as t:
             move_data_medium_grained(counts, src, dest, 100, N)
         assert counts.sum() == total, "Wrong total after move_data_medium_grained"
         print("Medium grained correlated: {} seconds".format(t.interval))
+        corr.append(t.interval)
+
+    import matplotlib.pyplot as plt
+    a = plt.plot(x, corr, label = 'Correlated')
+    b = plt.plot(x, uncorr, label = 'Uncorrelated')
+   # plt.legend(bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.xlabel('N')
+    plt.ylabel('Time(s)')
+    plt.legend(loc='upper left')
+    plt.title('Correlated and Uncorrelated Medium-grained Performance')
+    plt.savefig('medium.svg', format='svg', dpi=800)
