7f1dae5b092cf49923bad2b7e639b966146ac0c9
Rohit Ahuja <rahuja95@gmail.com>
2015-10-31 22:25:52 -0400

    Finished write up for P3

--- HW2/P3/mandelbrot.pyx
+++ HW2/P3/mandelbrot.pyx
@@ -8,6 +8,7 @@ from cython.parallel import prange
 cdef np.float64_t magnitude_squared(np.complex64_t z) nogil:
     return z.real * z.real + z.imag * z.imag
 
+# Stores iteration counts to memory
 cdef void counts_to_output(AVX.float8 iterations, np.uint32_t [:, :] out_counts, int i, int j) nogil:
     cdef:
         float temp_counts[8]
@@ -38,18 +39,15 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
     assert in_coords.shape[1] == out_counts.shape[1],  "Input and output arrays must be the same size"
 
     with nogil:
+        # Set the limit of the mandelbrot function
         max_mag = AVX.float_to_float8(4.0)
+
+        # Set one floats to add to iterations for coordinates
         one_bits = AVX.float_to_float8(1.0)
+
         for i in prange(in_coords.shape[0], num_threads=4, schedule='static', chunksize=1):
             for j in range(0, in_coords.shape[1], 8):
-                coords_real = AVX.make_float8(in_coords[i,j+7].real,
-                                              in_coords[i,j+6].real,
-                                              in_coords[i,j+5].real,
-                                              in_coords[i,j+4].real,
-                                              in_coords[i,j+3].real,
-                                              in_coords[i,j+2].real,
-                                              in_coords[i,j+1].real,
-                                              in_coords[i,j].real)
+                # Grab imaginary coordinates 
                 coords_imag = AVX.make_float8(in_coords[i,j+7].imag,
                                               in_coords[i,j+6].imag,
                                               in_coords[i,j+5].imag,
@@ -58,26 +56,52 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
                                               in_coords[i,j+2].imag,
                                               in_coords[i,j+1].imag,
                                               in_coords[i,j].imag)
+                
+                # Grab real coordinates
+                coords_real = AVX.make_float8(in_coords[i,j+7].real,
+                                              in_coords[i,j+6].real,
+                                              in_coords[i,j+5].real,
+                                              in_coords[i,j+4].real,
+                                              in_coords[i,j+3].real,
+                                              in_coords[i,j+2].real,
+                                              in_coords[i,j+1].real,
+                                              in_coords[i,j].real)
+                
+                # Store count of iterations for each coordinate
                 iterations = AVX.float_to_float8(0.0)
+
+                # Store magnitude of each coordinate
                 magnitude = AVX.float_to_float8(0.0)
+
+                # Record which coordinates have reached their magnitude
                 bitmask = AVX.float_to_float8(0.0)
+
+                # Record the z values, separated by real and imaginary forms
                 z_real = AVX.float_to_float8(0.0)
                 z_imag = AVX.float_to_float8(0.0)
 
                 for iter in range(max_iterations):
+                    # Add to iterations count if magnitude is less than 4
                     iterations = AVX.add(iterations, AVX.bitwise_and(bitmask, one_bits))
+
+                    # Calculate real and imaginary z values (z = z^2 + c)
                     z2_real = AVX.mul(z_real, z_real)
                     z2_imag = AVX.mul(z_imag, z_imag)
                     z_real_imag = AVX.add(AVX.mul(z_real, z_imag), AVX.mul(z_real, z_imag))
                     z_real = AVX.add(AVX.sub(z2_real, z2_imag), coords_real)
                     z_imag = AVX.add(z_real_imag, coords_imag)
 
+                    # Calculate magnitude
                     magnitude = AVX.add(z2_real, z2_imag)
+
+                    # Check if magnitudes are less than four
                     bitmask = AVX.less_than(magnitude, max_mag)
 
+                    # If all magnitudes are >= 4, break and continue to next 8 coordinates
                     if not AVX.signs(bitmask):
                         break
                 
+                # Output iterations
                 counts_to_output(iterations, out_counts, i, j)
 
 # An example using AVX instructions
