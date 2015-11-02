2259e5a7233d9416e39b2f8dd783f4f622891df4
Luong Hoang <lhoang@g.harvard.edu>
2015-11-01 23:42:05 -0500

    finished p2

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -79,12 +79,17 @@ cpdef move_data_fine_grained(np.int32_t[:] counts,
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+   for r in range(repeat):
+       for idx in prange(src.shape[0], nogil=True, num_threads=4):
+           if counts[src[idx]] > 0:
+
+               acquire(&locks[dest[idx]])
+               counts[dest[idx]] += 1
+               release(&locks[dest[idx]])
+
+               acquire(&locks[src[idx]])
+               counts[src[idx]] -= 1
+               release(&locks[src[idx]])
 
    free_N_locks(counts.shape[0], locks)
 
@@ -105,11 +110,16 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+   for r in range(repeat):
+       for idx in prange(src.shape[0], nogil=True, num_threads=4):
+           if counts[src[idx]] > 0:
+
+               acquire(&locks[dest[idx] / N])
+               counts[dest[idx]] += 1
+               release(&locks[dest[idx] / N])
+
+               acquire(&locks[src[idx] / N])
+               counts[src[idx]] -= 1
+               release(&locks[src[idx] / N])
 
    free_N_locks(num_locks, locks)
