77d2246ee1661a05b1037f1821ea89a63f6c670d
Hyunkwang Lee <hyunkwang@Hyunkwangs-MacBook-Air.local>
2015-11-02 01:00:12 -0500

    P2 finished

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -6,7 +6,7 @@
 ##################################################
 
 
-from cython.parallel import parallel, prange
+from cython.parallel import parallel, prange, threadid
 from openmp cimport omp_lock_t, \
     omp_init_lock, omp_destroy_lock, \
     omp_set_lock, omp_unset_lock, omp_get_thread_num
@@ -14,6 +14,7 @@ from libc.stdlib cimport malloc, free
 
 import numpy as np
 cimport numpy as np
+cimport cython
 
 
 # lock helper functions
@@ -49,7 +50,6 @@ cdef void free_N_locks(int N, omp_lock_t *locks) nogil:
 ##################################################
 # Your code below
 ##################################################
-
 cpdef move_data_serial(np.int32_t[:] counts,
                        np.int32_t[:] src,
                        np.int32_t[:] dest,
@@ -65,39 +65,65 @@ cpdef move_data_serial(np.int32_t[:] counts,
                    counts[dest[idx]] += 1
                    counts[src[idx]] -= 1
 
-
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
                              int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+    cdef:
+        int idx, jdx, kdx, r
+        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
    ##########
    # Your code here
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    with nogil:
+        for r in range(repeat):
+            for idx in prange(src.shape[0], num_threads=4):
+                jdx = src[idx]
+                kdx = dest[idx]
+                # always smaller index locked 
+                # before larger index locked
+                # to avoid Deadlock
+                if (jdx > kdx):
+                    acquire(&(locks[kdx]))
+                    acquire(&(locks[jdx]))
+                elif (jdx < kdx):
+                    acquire(&(locks[jdx]))
+                    acquire(&(locks[kdx]))
+                # when two indices are the same, 
+                # only a lock is acquired 
+                else:
+                    acquire(&(locks[jdx]))
+                    
+                if (counts[src[idx]] > 0):
+                    counts[dest[idx]] += 1
+                    counts[src[idx]] -= 1
 
-   free_N_locks(counts.shape[0], locks)
+                # when two indices are the same, 
+                # only a lock is released
+                if (jdx != kdx):
+                    release(&(locks[jdx]))
+                    release(&(locks[kdx]))
+                else:
+                    release(&(locks[jdx]))
+                    
+    free_N_locks(counts.shape[0], locks)
 
-
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] src,
                                np.int32_t[:] dest,
                                int repeat,
                                int N):
-   cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+    cdef:
+        int idx, jdx, kdx, r
+        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+        omp_lock_t *locks = get_N_locks(num_locks)
 
    ##########
    # Your code here
@@ -105,11 +131,33 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    with nogil:
+        for r in range(repeat):
+            for idx in prange(src.shape[0], num_threads=4):
+                jdx = src[idx]/N
+                kdx = dest[idx]/N
+                # always smaller index locked 
+                # before larger index locked
+                # to avoid Deadlock
+                if (jdx > kdx):
+                    acquire(&(locks[kdx]))
+                    acquire(&(locks[jdx]))
+                elif (jdx < kdx):
+                    acquire(&(locks[jdx]))
+                    acquire(&(locks[kdx]))
+                # when two indices are the same, 
+                # only a lock is acquired 
+                else:
+                    acquire(&(locks[jdx]))
+                    
+                if (counts[src[idx]] > 0):
+                    counts[dest[idx]] += 1
+                    counts[src[idx]] -= 1
 
-   free_N_locks(num_locks, locks)
+                # when two indices are the same, 
+                # only a lock is released
+                if (jdx != kdx):
+                    release(&(locks[jdx]))
+                    release(&(locks[kdx]))
+                else:
+                    release(&(locks[jdx]))
