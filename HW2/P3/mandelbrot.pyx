67516511cdd854c7b1b9fb76a16ed7d6a4635366
Patrick Kuiper <patrick.kendal.kuiper@gmail.com>
2015-11-02 12:40:11 -0500

    updated comments

--- HW2/P3/mandelbrot.pyx
+++ HW2/P3/mandelbrot.pyx
@@ -47,8 +47,6 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
     assert in_coords.shape[0] == out_counts.shape[0], "Input and output arrays must be the same size"
     assert in_coords.shape[1] == out_counts.shape[1],  "Input and output arrays must be the same size"
 
-    #with nogil:
-        #for i in range(in_coords.shape[0]):
     for i in prange(in_coords.shape[0], nogil=True, num_threads=8, schedule=static):
         for j in range(in_coords.shape[1]):
             c = in_coords[i, j]
@@ -67,8 +65,15 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
 # An example using AVX instructions
 cpdef mandelbrot_AVX(np.complex64_t [:,:] in_coords, np.uint32_t [:,:] out_counts):
 
+'''
+This is the mandelbrot calcualtion using AVX
+'''
+
     cdef:
-        AVX.float8 tmp_real, tmp_img, c, z, mask, xx, yy, real, img1, img, mag, new_ones, xx_mul, yy_mul, xy_sub, z_mag_real, z_mag_img, z_mag, z_real, z_img, xy_2_mul, two_xy_mul, xy_mul, counts
+        AVX.float8 tmp_real, tmp_img, c, z, mask, xx, yy, real, img1
+        AVX.float8 img, mag, new_ones, xx_mul, yy_mul, xy_sub, z_mag_real
+        AVX.float8 z_mag_img, z_mag, z_real, z_img, xy_2_mul, two_xy_mul, xy_mul 
+        AVX.float8 counts
         int i, j, it, k
         int max_iterations=511
         np.float32_t [:,:] coords_1 
@@ -88,49 +93,62 @@ cpdef mandelbrot_AVX(np.complex64_t [:,:] in_coords, np.uint32_t [:,:] out_count
     for j in prange(in_coords.shape[0], nogil=True, num_threads=8, schedule=static):
       for i in range(0, in_coords.shape[1], 8):
 
+        #pull out the real components
         tmp_real = AVX.make_float8(coords_1[j, i],coords_1[j, i+1],coords_1[j, i+2],coords_1[j, i+3],coords_1[j, i+4],coords_1[j, i+5],coords_1[j, i+6],coords_1[j, i+7])
 
-
+        #pull out the imaginary components
         tmp_img = AVX.make_float8(coords_2[j, i],coords_2[j, i+1],coords_2[j, i+2],coords_2[j, i+3],coords_2[j, i+4],coords_2[j, i+5],coords_2[j, i+6],coords_2[j, i+7])
 
+
+        #initialize all of the vectors
         z_real = zeros
         z_img = zeros
         z_mag_real = zeros
         z_mag_img = zeros
         z = zeros
         counts = zeros
-        # z_real, z_img, z_mag_real, z_mag_img, z, counts = zeros
-
         z_mag = AVX.add(z_mag_real, z_mag_img)
+
 
         for it in range(max_iterations):
 
+          #create the mask which acts as the if statement
           mask = AVX.less_than(z_mag, thresh) 
-          if not AVX.signs(mask):
-              break
-          new_ones = AVX.bitwise_and(ones, mask)#added not
-          ##### ADD break statement here
-          # AVX.to_mem(new_ones, &(out_vals[0]))
-          # if sum(out_view) == 0:
 
+          if not AVX.signs(mask): #this will ensure that the loop breaks if the conditions are no longer met
+              break
+
+          new_ones = AVX.bitwise_and(ones, mask)#added not
           counts = AVX.add(new_ones, counts)
 
-          #z_real = x^2 - y^2 + x
-          #z_img = 2xyi + y
+          ################overall computatations################
+          #    Must execute the computations separately, 
+          #    real and imaginary
+          #    z_real = x^2 - y^2 + x
+          #    z_img = 2xyi + y
+          ######################################################
+
+          #calculate the real poriton using AVX
           xx_mul = AVX.mul(z_real, z_real) # x^2
           yy_mul = AVX.mul(z_img, z_img) # y^2
           xy_sub = AVX.sub(xx_mul, yy_mul) # x^2 - y^2 
+
+          #calculate the imaginary poriton using AVX
           xy_mul = AVX.mul(z_real, z_img) # xy
           two_xy_mul = AVX.mul(two, xy_mul) # 2xy
 
+          #consolidate the the real and imaginary portions
           z_real = AVX.add(xy_sub, tmp_real) # x^2 - y^2 + x
           z_img = AVX.add(two_xy_mul, tmp_img) # xyi + y
 
+          #consolidate real and imaginary for the magnitude calculation
           z_mag_real = AVX.mul(z_real, z_real) 
           z_mag_img = AVX.mul(z_img, z_img)
           z_mag = AVX.add(z_mag_real, z_mag_img)
 
-        for k in range(8):
+
+
+        for k in range(8): #write the information out to memory 
           out_counts[j,i+k] = <np.uint32_t>((<np.float32_t*>&counts)[k])
 
 
