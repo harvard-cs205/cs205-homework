a75b7cc210e34fdaa0442a849198c8808619d5bb
George Lok <george.j.lok@gmail.com>
2015-11-02 16:46:05 -0500

    P4 answers completed.

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -17,18 +17,29 @@ import threading
 
 def median_3x3_worker(A, B, offset, step, iterations, sems) : 
     for i in range(iterations):
+        # Acquire appropriate n-1 and n+1 semaphores
+        # This will only be available if iteration i-1 is done for threads
+        # n-1 and n+1
+
+        # Acquiring semaphore in this way will allow us to begin an iteration of filtering
+        # asynchronously
+
+        # Note that this logic is robust enough for N=2 and N=1, though
+        # it will do some unrequired semaphore operations.
         lower_sem = sems[(offset - 1) % step][1]
         upper_sem = sems[(offset + 1) % step][0]
 
-        (self_sem_1, self_sem_2) = sems[offset]
         lower_sem.acquire()
         upper_sem.acquire()
        
+        # Perform filtering starting at line n and for every Nth line after
         filtering.median_3x3(A, B, offset, step)
 
         # swap direction of filtering
         A, B = B, A
 
+        # Indicate to threads n-1 and n+1 that iteration i is done.
+        (self_sem_1, self_sem_2) = sems[offset]
         self_sem_1.release()
         self_sem_2.release()
 
@@ -38,16 +49,23 @@ def py_median_3x3(image, iterations=10, num_threads=1):
     tmpA = image.copy()
     tmpB = np.empty_like(tmpA)
 
+    # Create the dual semaphores for each thread. 
     sems = []
     for i in range(num_threads) :
         sems.append((threading.Semaphore(1), threading.Semaphore(1)))
 
+    # Create threads
     threads = []
     for i in range(num_threads) :
+        # Thread i will start at line i and every Nth line after.
         t = threading.Thread(target=median_3x3_worker, name=str(i), args=(tmpA, tmpB, i, num_threads, iterations, sems))
-        t.start()
+        
+        # Make sure we keep track of the thread
         threads.append(t)
 
+        t.start()
+
+    # Wait for all the threads to finish
     for t in threads :
         t.join()
 
@@ -83,7 +101,7 @@ if __name__ == '__main__':
     assert np.all(from_cython == from_numpy)
 
     with Timer() as t:
-        new_image = py_median_3x3(input_image, 10, 4)
+        new_image = py_median_3x3(input_image, 10, 1)
 
     pylab.figure()
     pylab.imshow(new_image[1200:1800, 3000:3500])
