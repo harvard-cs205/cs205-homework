8624369326a2cb79e5d111668982c04c27a8a1db
Hyunkwang Lee <hyunkwang@Hyunkwangs-MacBook-Air.local>
2015-11-02 01:02:10 -0500

    P3 finished

--- HW2/P3/driver.py
+++ HW2/P3/driver.py
@@ -26,13 +26,49 @@ def make_coords(center=(-0.575 - 0.575j),
 
 
 if __name__ == '__main__':
+    # Multithreaded without instruction-level paralleism
+    # Number of Threads = 1
     in_coords, out_counts = make_coords()
-
     with Timer() as t:
-        mandelbrot.mandelbrot(in_coords, out_counts, 1024)
+        mandelbrot.mandelbrot(in_coords, out_counts, 1024, 1)
     seconds = t.interval
+    print("NUM of Threads [1] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
 
-    print("{} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
+    # Number of Threads = 2
+    in_coords, out_counts = make_coords()
+    with Timer() as t:
+        mandelbrot.mandelbrot(in_coords, out_counts, 1024, 2)
+    seconds = t.interval
+    print("NUM of Threads [2] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
+
+    # Number of Threads = 4
+    in_coords, out_counts = make_coords()
+    with Timer() as t:
+        mandelbrot.mandelbrot(in_coords, out_counts, 1024, 4)
+    seconds = t.interval
+    print("NUM of Threads [4] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
+
+    # Multithreaded with instruction-level paralleism
+    # Number of Threads = 1
+    in_coords, out_counts = make_coords()
+    with Timer() as t:
+        mandelbrot.mandelbrot_avx(in_coords, out_counts, 1024, 1)
+    seconds = t.interval
+    print("NUM of Threads [1] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
+
+    # Number of Threads = 2
+    in_coords, out_counts = make_coords()
+    with Timer() as t:
+        mandelbrot.mandelbrot_avx(in_coords, out_counts, 1024, 2)
+    seconds = t.interval
+    print("NUM of Threads [2] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
+
+    # Number of Threads = 4
+    in_coords, out_counts = make_coords()
+    with Timer() as t:
+        mandelbrot.mandelbrot_avx(in_coords, out_counts, 1024, 4)
+    seconds = t.interval
+    print("NUM of Threads [4] : {} Million Complex FMAs in {} seconds, {} million Complex FMAs / second".format(out_counts.sum() / 1e6, seconds, (out_counts.sum() / seconds) / 1e6))
 
     plt.imshow(np.log(out_counts))
     plt.show()
