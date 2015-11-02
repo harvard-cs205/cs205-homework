5c4a1a8a004a50827d0a6237e2067bce2429d1a0
George Lok <george.j.lok@gmail.com>
2015-11-02 04:31:51 -0500

    P5 in progress.

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -19,6 +19,8 @@ def randcolor():
 if __name__ == '__main__':
     num_balls = 10000
     radius = 0.002
+    # num_balls = 100
+    # radius = 0.01
     positions = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
 
@@ -64,7 +66,7 @@ if __name__ == '__main__':
     while True:
         with Timer() as t:
             update(positions, velocities, grid,
-                   radius, grid_size, locks_ptr,
+                   radius, grid_spacing, locks_ptr,
                    physics_step)
 
         # udpate our estimate of how fast the simulator runs
--- HW2/P5/physics.pyx
+++ HW2/P5/physics.pyx
@@ -6,6 +6,9 @@ from libc.stdint cimport uintptr_t
 cimport cython
 from omp_defs cimport omp_lock_t, get_N_locks, free_N_locks, acquire, release
 
+from cython.parallel import prange
+
+
 # Useful types
 ctypedef np.float32_t FLOAT
 ctypedef np.uint32_t UINT
@@ -58,7 +61,10 @@ cdef void sub_update(FLOAT[:, ::1] XY,
                      float grid_spacing) nogil:
     cdef:
         FLOAT *XY1, *XY2, *V1, *V2
-        int j, dim
+        int GXY[25] # maximum 13 balls to check
+        int ii, jj, j, dim, gx, gy
+        int gxy = 0
+        int grid_size = int((1.0 / grid_spacing) + 1)
         float eps = 1e-5
 
     # SUBPROBLEM 4: Add locking
@@ -69,9 +75,30 @@ cdef void sub_update(FLOAT[:, ::1] XY,
     ############################################################
     # SUBPROBLEM 2: use the grid values to reduce the number of other
     # objects to check for collisions.
-    for j in range(i + 1, count):
+    
+    # Use the Grid here to limit the balls we need to check.
+    gx = <int>(XY1[0] / grid_spacing)
+    gy = <int>(XY1[1] / grid_spacing)
+
+    # Search all possible locations for collision
+    # 2 < 2 * sqrt(2) < 3, so the other circle can be at max 3 squares away
+    # for the circles to be touching.
+    # To simplify code, we'll slightly expand the search area into a 7 x 7 grid.
+    for ii in range(-2 + gx, 3 + gx) :
+        if ii < 0 or ii >= grid_size :
+            continue
+        for jj in range(-2 + gy, 3 + gy) :
+            if jj < 0 or jj >= grid_size  :
+                continue
+            if Grid[ii,jj] <= i or Grid[ii,jj] > count :
+                continue
+            GXY[gxy] = Grid[ii,jj]
+            gxy += 1
+
+    for ii in range(gxy):
+        j = GXY[ii]
         XY2 = &(XY[j, 0])
-        V2 = &(V[j, 0])
+        V2 = &(V[j, 0])        
         if overlapping(XY1, XY2, R):
             # SUBPROBLEM 4: Add locking
             if not moving_apart(XY1, V1, XY2, V2):
@@ -90,7 +117,8 @@ cpdef update(FLOAT[:, ::1] XY,
              float t):
     cdef:
         int count = XY.shape[0]
-        int i, j, dim
+        int grid_size = int((1.0 / grid_spacing) + 1)
+        int i, j, dim, gx, gy
         FLOAT *XY1, *XY2, *V1, *V2
         # SUBPROBLEM 4: uncomment this code.
         # omp_lock_t *locks = <omp_lock_t *> <void *> locks_ptr
@@ -103,7 +131,8 @@ cpdef update(FLOAT[:, ::1] XY,
         #
         # SUBPROBLEM 1: parallelize this loop over 4 threads, with static
         # scheduling.
-        for i in range(count):
+        num_threads = 4
+        for i in prange(count, num_threads=num_threads, schedule='static') :
             for dim in range(2):
                 if (((XY[i, dim] < R) and (V[i, dim] < 0)) or
                     ((XY[i, dim] > 1.0 - R) and (V[i, dim] > 0))):
@@ -113,7 +142,7 @@ cpdef update(FLOAT[:, ::1] XY,
         #
         # SUBPROBLEM 1: parallelize this loop over 4 threads, with static
         # scheduling.
-        for i in range(count):
+        for i in prange(count, num_threads=num_threads, schedule='static') :
             sub_update(XY, V, R, i, count, Grid, grid_spacing)
 
         # update positions
@@ -121,9 +150,18 @@ cpdef update(FLOAT[:, ::1] XY,
         # SUBPROBLEM 1: parallelize this loop over 4 threads (with static
         #    scheduling).
         # SUBPROBLEM 2: update the grid values.
-        for i in range(count):
+
+        # Reset grid values
+        for i in prange(grid_size, num_threads=num_threads, schedule='static') :
+            for j in range(grid_size) :
+                Grid[i,j] = -1
+
+        for i in prange(count, num_threads=num_threads, schedule='static') :
             for dim in range(2):
                 XY[i, dim] += V[i, dim] * t
+            gx = <int> (XY[i, 0] / grid_spacing)
+            gy = <int> (XY[i, 1] / grid_spacing)
+            Grid[gx,gy] = i
 
 
 def preallocate_locks(num_locks):
