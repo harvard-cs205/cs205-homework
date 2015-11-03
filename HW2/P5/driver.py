0bb18c5f7d1cdb919a17626be3461a58df25909e
Elena-Zhao <zhaoqing513@126.com>
2015-11-03 02:04:20 -0500

    P5 finished

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -16,9 +16,26 @@ from physics import update, preallocate_locks
 def randcolor():
     return np.random.uniform(0.0, 0.89, (3,)) + 0.1
 
+# Source: http://www.thejach.com/view/2011/9/playing_with_morton_numbers
+def tomorton(x,y):
+    x = bin(x)[2:]
+    lx = len(x)
+    y = bin(y)[2:]
+    ly = len(y)
+    L = max(lx, ly)
+    m = 0
+    for j in xrange(1, L+1):
+        # note: ith bit of x requires x[lx - i] since our bin numbers are big endian
+        xi = int(x[lx-j]) if j-1 < lx else 0
+        yi = int(y[ly-j]) if j-1 < ly else 0
+        m += 2**(2*j)*xi + 2**(2*j+1)*yi
+    return m/4
+
 if __name__ == '__main__':
     num_balls = 10000
     radius = 0.002
+    # num_balls = 500
+    # radius = 0.01
     positions = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
 
@@ -65,7 +82,7 @@ if __name__ == '__main__':
         with Timer() as t:
             update(positions, velocities, grid,
                    radius, grid_size, locks_ptr,
-                   physics_step)
+                   physics_step, 4)
 
         # udpate our estimate of how fast the simulator runs
         physics_step = 0.9 * physics_step + 0.1 * t.interval
@@ -80,3 +97,24 @@ if __name__ == '__main__':
             # SUBPROBLEM 3: sort objects by location.  Be sure to update the
             # grid if objects' indices change!  Also be sure to sort the
             # velocities with their object positions!
+
+            # Get the order of locations based on Morton ordering
+            zorder = [tomorton(int(positions[i,0] * grid_size), int(positions[i,1] * grid_size)) for i in range(num_balls)]
+            sorted_positions = np.argsort(zorder)
+
+            # 1. update each ball's position according to its Morton order
+            positions = positions[sorted_positions]
+
+            # 2. update each ball's velocities according to its Morton order
+            velocities = velocities[sorted_positions]
+
+            # 3.update grid indices
+            for i in range(num_balls):
+                # Boundary check
+                if positions[i,0]>=0 and positions[i,1]>=0 and positions[i,0]<=1 and positions[i,1]<=1:
+                    positions_coords = (positions[i,] / grid_spacing).astype(np.int)
+                    grid[positions_coords[0], positions_coords[1]] = i
+
+
+
+
