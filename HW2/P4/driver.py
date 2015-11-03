db38db577f2f8e68124b472b55dbb95db02e20f4
ymiyamot <yohsuke.miyamoto@gmail.com>
2015-11-02 19:41:45 -0500

    Finished P4

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -20,13 +20,56 @@ def py_median_3x3(image, iterations=10, num_threads=1):
     tmpA = image.copy()
     tmpB = np.empty_like(tmpA)
 
-    for i in range(iterations):
-        filtering.median_3x3(tmpA, tmpB, 0, 1)
+    events = []
+    threads = []
+    for iter_i in range(iterations):
+        print(iter_i)
+
+        events.append([])
+        threads.append([])
+        for thread_i in range(num_threads):            
+            # Create events to synchronize threads
+            events[iter_i].append(threading.Event())           
+            
+            t = threading.Thread(target=threaded_work,
+                                 args=(tmpA, 
+                                       tmpB, 
+                                       thread_i, 
+                                       num_threads, 
+                                       iter_i,
+                                       events))
+            t.start()
+            threads[iter_i].append(t)
+            
         # swap direction of filtering
         tmpA, tmpB = tmpB, tmpA
 
+    # Make sure all the the threads are complete before exiting
+    for iter_i in range(iterations):
+        for thread_i in range(num_threads):
+            threads[iter_i][thread_i].join()
     return tmpA
+    
+def threaded_work(tmpA, tmpB, thread_i, num_threads, iter_i, events):
+    # If this is not the first iteration,
+    # identify which threads of the previous iteration 
+    # need to be done before moving on.
+    if iter_i > 0:
+        back_thread = np.mod(thread_i - 1, num_threads)
+        front_thread = np.mod(thread_i + 1, num_threads)
 
+        #print("Checking for iter{}, thread{}, checking t{} and t{}".format(iter_i, thread_i, back_thread, front_thread))
+        events[iter_i - 1][thread_i].wait()
+        events[iter_i - 1][back_thread].wait()
+        events[iter_i - 1][front_thread].wait()
+        #print("Completed check for iter{}, thread{}, checked t{} and t{}".format(iter_i, thread_i, back_thread, front_thread))        
+        
+    filtering.median_3x3(tmpA, tmpB, thread_i, num_threads)
+    
+    # Notify other threads of completion
+    events[iter_i][thread_i].set() 
+
+# Note that this function modifies the original image
 def numpy_median(image, iterations=10):
     ''' filter using numpy '''
     for i in range(iterations):
@@ -51,17 +94,54 @@ if __name__ == '__main__':
     pylab.imshow(input_image[1200:1800, 3000:3500])
     pylab.title('before - zoom')
 
-    # verify correctness
-    from_cython = py_median_3x3(input_image, 2, 5)
-    from_numpy = numpy_median(input_image, 2)
-    assert np.all(from_cython == from_numpy)
+    n_iterations = 10
+    num_threads_list = [1, 2, 4]
+    perf_times = []
+    new_images = []
 
+    for num_threads in num_threads_list:
+        with Timer() as t:
+            from_cython = py_median_3x3(input_image, n_iterations, num_threads)
+
+        perf_times.append(t.interval)
+        new_images.append(from_cython)
+        print("{} threads took {} sec".format(num_threads, t.interval))
+        
     with Timer() as t:
-        new_image = py_median_3x3(input_image, 10, 8)
+        from_numpy = numpy_median(input_image, n_iterations)  
+    original_time = t.interval
+    print("Original took {} sec".format(t.interval))
+
+    # verify correctness
+    for thread_i in range(len(num_threads_list)):
+        assert(np.all(from_numpy == new_images[thread_i]))
+
 
     pylab.figure()
-    pylab.imshow(new_image[1200:1800, 3000:3500])
-    pylab.title('after - zoom')
+    for thread_i in range(len(num_threads_list)):
+        pylab.subplot(1, 4, thread_i + 2)
+        pylab.imshow(new_images[thread_i][1200:1800, 3000:3500])
+        pylab.title("{} threads".format(num_threads_list[thread_i]))
+        pylab.xticks([])
+        pylab.yticks([])
+        
+    pylab.subplot(1, 4, 1)
+    pylab.imshow(new_images[thread_i][1200:1800, 3000:3500])
+    pylab.title('Serial version')
+    pylab.xticks([])
+    pylab.yticks([])
+    pylab.savefig('P4_image_correctness.png')
+#    pylab.show()
+    
+    pylab.figure()
+    pylab.bar(range(4), [original_time] + perf_times, color = ['k', 'b', 'b', 'b'])
+    pylab.xticks(0.5 + np.arange(4), ['Numpy', '1 thread', '2 threads', '4 threads'])
+    pylab.title('Run times for {} iterations'.format(n_iterations))
+    pylab.ylabel('Run time (sec)')
+    pylab.savefig('P4_perf_times.png')
+#    plt.show()
+    
+    
 
-    print("{} seconds for 10 filter passes.".format(t.interval))
-    pylab.show()
+#    print("{} seconds for 10 filter passes.".format(t.interval))
+#    pylab.show()
