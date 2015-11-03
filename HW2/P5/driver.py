e115c257e32708427de5a2e43468594d7ff90f04
yuhaozhu <yuhaozhu@g.harvard.edu>
2015-11-02 23:22:10 -0500

    P5 done!

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -16,21 +16,35 @@ from physics import update, preallocate_locks
 def randcolor():
     return np.random.uniform(0.0, 0.89, (3,)) + 0.1
 
+# from wiki
+def zorder(p, q):
+    j, k, x = 0, 0, 0
+    a = p[1]
+    b = q[1]
+    for k in range(2):
+        y = a[k] ^ b[k]
+        if (x < y and x < (x ^ y)):
+            j = k
+            x = y
+    return a[j] - b[j]
+
+
 if __name__ == '__main__':
     num_balls = 10000
     radius = 0.002
-    positions = np.random.uniform(0 + radius, 1 - radius,
+    pos = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
+
 
     # make a hole in the center
     while True:
-        distance_from_center = np.sqrt(((positions - 0.5) ** 2).sum(axis=1))
+        distance_from_center = np.sqrt(((pos - 0.5) ** 2).sum(axis=1))
         mask = (distance_from_center < 0.25)
         num_close_to_center = mask.sum()
         if num_close_to_center == 0:
             # everything is out of the center
             break
-        positions[mask, :] = np.random.uniform(0 + radius, 1 - radius,
+        pos[mask, :] = np.random.uniform(0 + radius, 1 - radius,
                                                (num_close_to_center, 2)).astype(np.float32)
 
     velocities = np.random.uniform(-0.25, 0.25,
@@ -44,11 +58,11 @@ if __name__ == '__main__':
     grid_spacing = radius / np.sqrt(2.0)
     grid_size = int((1.0 / grid_spacing) + 1)
     grid = - np.ones((grid_size, grid_size), dtype=np.uint32)
-    grid[(positions[:, 0] / grid_spacing).astype(int),
-         (positions[:, 1] / grid_spacing).astype(int)] = np.arange(num_balls)
+    grid[(pos[:, 0] / grid_spacing).astype(int),
+         (pos[:, 1] / grid_spacing).astype(int)] = np.arange(num_balls)
 
     # A matplotlib-based animator object
-    animator = Animator(positions, radius * 2)
+    animator = Animator(pos, radius * 2)
 
     # simulation/animation time variablees
     physics_step = 1.0 / 100  # estimate of real-time performance of simulation
@@ -60,12 +74,14 @@ if __name__ == '__main__':
     # SUBPROBLEM 4: uncomment the code below.
     # preallocate locks for objects
     locks_ptr = preallocate_locks(num_balls)
+    interval = []
 
     while True:
         with Timer() as t:
-            update(positions, velocities, grid,
-                   radius, grid_size, locks_ptr,
+            update(pos, velocities, grid,
+                   radius, grid_spacing, locks_ptr,
                    physics_step)
+
 
         # udpate our estimate of how fast the simulator runs
         physics_step = 0.9 * physics_step + 0.1 * t.interval
@@ -73,10 +89,25 @@ if __name__ == '__main__':
 
         frame_count += 1
         if total_time > anim_step:
-            animator.update(positions)
+            animator.update(pos)
             print("{} simulation frames per second".format(frame_count / total_time))
+            interval += [frame_count / total_time]
             frame_count = 0
             total_time = 0
             # SUBPROBLEM 3: sort objects by location.  Be sure to update the
             # grid if objects' indices change!  Also be sure to sort the
-            # velocities with their object positions!
+            # velocities with their object pos!
+            # Sort the grid using morton order
+            grid_pos = (pos / grid_spacing).astype(int)
+            i_pos = zip(range(len(grid_pos)), grid_pos).sort(zorder)
+            index = [x[0] for x in i_pos]
+            pos = pos[index]
+            velocities = velocities[index]
+            grid_bound = np.array(filter(lambda x: x[0] > 0 and x[1] < 1, pos))
+            grid = - np.ones((grid_size, grid_size), dtype = np.uint32)
+            grid[(grid_bound[:, 0] / grid_spacing).astype(int),
+                 (grid_bound[:, 1] / grid_spacing).astype(int)] = np.arange(len(grid_bound))
+
+
+
+
