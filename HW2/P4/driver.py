0c551df301bbd7cb6166bec441bc088d11325202
Vivek Jayaram <vivekjayaram@college.harvard.edu>
2015-11-03 00:02:02 -0500

    done with part 4

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -15,15 +15,47 @@ import filtering
 from timer import Timer
 import threading
 
+def target_function(tmpA, tmpB, iterations, events, num_threads, thread_no):
+    for i in range(iterations):
+        filtering.median_3x3(tmpA, tmpB, thread_no, num_threads)
+        # if our thread index is greater than 0, wait for thread no -1 iteration i-1
+        if thread_no > 0 and i > 0:
+            thread_min_one = (thread_no - 1) % num_threads
+            thread_plus_one = (thread_no + 1) % num_threads
+            events[i-1][thread_min_one].wait()
+            events[i-1][thread_plus_one].wait()
+
+        # set this event as finished
+        events[i][thread_no].set()
+        # Swap like it was originally
+        tmpA, tmpB = tmpB, tmpA
+
+
+
+
 def py_median_3x3(image, iterations=10, num_threads=1):
     ''' repeatedly filter with a 3x3 median '''
     tmpA = image.copy()
     tmpB = np.empty_like(tmpA)
-
+    threads = []
+    events = []
+    # Create a bunch of events
     for i in range(iterations):
-        filtering.median_3x3(tmpA, tmpB, 0, 1)
-        # swap direction of filtering
-        tmpA, tmpB = tmpB, tmpA
+        events.append([threading.Event() for j in range(num_threads)])
+
+    for i in range(num_threads):
+        new_thread = threading.Thread(target=target_function, args=(tmpA, tmpB, iterations, events, num_threads, i))
+        threads.append(new_thread)
+        new_thread.start()
+
+    # Make sure we wait for all the threads to finish
+    for thread in threads:
+        thread.join()
+
+    # for i in range(iterations):
+    #     filtering.median_3x3(tmpA, tmpB, 0, 1)
+    #     # swap direction of filtering
+    #     tmpA, tmpB = tmpB, tmpA
 
     return tmpA
 
@@ -56,12 +88,13 @@ if __name__ == '__main__':
     from_numpy = numpy_median(input_image, 2)
     assert np.all(from_cython == from_numpy)
 
-    with Timer() as t:
-        new_image = py_median_3x3(input_image, 10, 8)
+    for num_threads in [1,2,4]:
+        with Timer() as t:
+            new_image = py_median_3x3(input_image, 10, num_threads)
 
-    pylab.figure()
-    pylab.imshow(new_image[1200:1800, 3000:3500])
-    pylab.title('after - zoom')
+        pylab.figure()
+        pylab.imshow(new_image[1200:1800, 3000:3500])
+        pylab.title('after - zoom')
 
-    print("{} seconds for 10 filter passes.".format(t.interval))
-    pylab.show()
+        print("{} seconds for 10 filter passes. number of threads{}".format(t.interval, num_threads))
+        pylab.show()
