3075bddef724db1d3e969e9d4215ec751c17befc
Daniel <drajchwald@g.harvard.edu>
2015-11-04 14:03:36 -0500

    add par vec

--- HW2/P2/parallel_vector.pyx
+++ HW2/P2/parallel_vector.pyx
@@ -54,39 +54,55 @@ cpdef move_data_serial(np.int32_t[:] counts,
                        np.int32_t[:] src,
                        np.int32_t[:] dest,
                        int repeat):
-   cdef:
-       int idx, r
+    cdef:
+        int idx, r
 
-   assert src.size == dest.size, "Sizes of src and dest arrays must match"
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    assert src.size == dest.size, "Sizes of src and dest arrays must match"
+    with nogil:
+        for r in range(repeat):
+            for idx in range(src.shape[0]):
+                if counts[src[idx]] > 0:
+                    counts[dest[idx]] += 1
+                    counts[src[idx]] -= 1
 
 
 cpdef move_data_fine_grained(np.int32_t[:] counts,
                              np.int32_t[:] src,
                              np.int32_t[:] dest,
                              int repeat):
-   cdef:
-       int idx, r
-       omp_lock_t *locks = get_N_locks(counts.shape[0])
+    cdef:
+        int idx, r
+        omp_lock_t *locks = get_N_locks(counts.shape[0])
 
    ##########
    # Your code here
    # Use parallel.prange() and a lock for each element of counts to parallelize
    # data movement.  Be sure to avoid deadlock, and double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    for r in range(repeat):
+        for idx in prange(src.shape[0],nogil=True,num_threads = 4,schedule=dynamic):
+            if dest[idx] < src[idx]:
+                omp_set_lock(&(locks[dest[idx]]))
+                omp_set_lock(&(locks[src[idx]]))
+            elif dest[idx] > src[idx]:
+                omp_set_lock(&(locks[src[idx]]))
+                omp_set_lock(&(locks[dest[idx]]))
+            else:
+                omp_set_lock(&(locks[src[idx]]))
+                           
+            if counts[src[idx]] > 0:
+                counts[dest[idx]] += 1
+                counts[src[idx]] -= 1
 
-   free_N_locks(counts.shape[0], locks)
+            if dest[idx] < src[idx]:
+                omp_unset_lock(&(locks[dest[idx]]))
+                omp_unset_lock(&(locks[src[idx]]))
+            elif dest[idx] > src[idx]:
+                omp_unset_lock(&(locks[src[idx]]))
+                omp_unset_lock(&(locks[dest[idx]]))
+            else:
+                omp_unset_lock(&(locks[src[idx]]))
+    free_N_locks(counts.shape[0], locks)
 
 
 cpdef move_data_medium_grained(np.int32_t[:] counts,
@@ -94,10 +110,10 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
                                np.int32_t[:] dest,
                                int repeat,
                                int N):
-   cdef:
-       int idx, r
-       int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
-       omp_lock_t *locks = get_N_locks(num_locks)
+    cdef:
+        int idx, r
+        int num_locks = (counts.shape[0] + N - 1) / N  # ensure enough locks
+        omp_lock_t *locks = get_N_locks(num_locks)
 
    ##########
    # Your code here
@@ -105,11 +121,30 @@ cpdef move_data_medium_grained(np.int32_t[:] counts,
    # to parallelize data movement.  Be sure to avoid deadlock, as well as
    # double-locking.
    ##########
-   with nogil:
-       for r in range(repeat):
-           for idx in range(src.shape[0]):
-               if counts[src[idx]] > 0:
-                   counts[dest[idx]] += 1
-                   counts[src[idx]] -= 1
+    for r in range(repeat):
+        for idx in prange(src.shape[0],nogil=True,num_threads = 4,schedule=dynamic):
+            if dest[idx]/N < src[idx]/N:
+                omp_set_lock(&(locks[dest[idx]/N]))
+                omp_set_lock(&(locks[src[idx]/N]))
+            elif dest[idx]/N > src[idx]/N:
+                omp_set_lock(&(locks[src[idx]/N]))
+                omp_set_lock(&(locks[dest[idx]/N]))
+            else:
+                omp_set_lock(&(locks[src[idx]/N]))
 
-   free_N_locks(num_locks, locks)
+            if counts[src[idx]] > 0:
+                counts[dest[idx]] += 1
+                counts[src[idx]] -= 1
+
+            if dest[idx]/N < src[idx]/N:
+                omp_unset_lock(&(locks[dest[idx]/N]))
+                omp_unset_lock(&(locks[src[idx]/N]))
+            elif dest[idx]/N > src[idx]/N:
+                omp_unset_lock(&(locks[src[idx]/N]))
+                omp_unset_lock(&(locks[dest[idx]/N]))
+            else:
+                omp_unset_lock(&(locks[src[idx]/N]))
+
+    free_N_locks(num_locks, locks)
+
+
