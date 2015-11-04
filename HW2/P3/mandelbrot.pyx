41b64582cf3d9b0b24a1a995d60b4ee5208eab05
Victor Lei <vlei@g.harvard.edu>
2015-11-04 07:39:45 -0500

    Finish draft of Q4

--- HW2/P3/mandelbrot.pyx
+++ HW2/P3/mandelbrot.pyx
@@ -38,55 +38,57 @@ cpdef mandelbrot(np.complex64_t [:, :] in_coords,
     assert in_coords.shape[1] == out_counts.shape[1],  "Input and output arrays must be the same size"
 
     # No instruction-level parallelism
-    with nogil:
-        for i in prange(in_coords.shape[0], schedule='static', chunksize=1, num_threads=1):
-            for j in range(in_coords.shape[1]):
-                c = in_coords[i, j]
-                z = 0
-                for iter in range(max_iterations):
-                    if magnitude_squared(z) > 4:
-                        break
-                    z = z * z + c
-                out_counts[i, j] = iter
-
     # with nogil:
     #     for i in prange(in_coords.shape[0], schedule='static', chunksize=1, num_threads=1):
-    #         for j in range(0, in_coords.shape[1], 8):
-    #             avx_imag = AVX.make_float8(img[i, j+7], img[i, j+6], img[i,j+5], img[i,j+4], img[i,j+3], img[i,j+2], img[i,j+1],
-    #                                        img[i,j+0])
-    #             avx_real = AVX.make_float8(real[i, j+7], real[i, j+6], real[i,j+5], real[i,j+4], real[i,j+3], real[i,j+2], real[i,j+1],
-    #                                        real[i,j+0])
-    #
-    #             z_imag = AVX.float_to_float8(0.)
-    #             z_real = AVX.float_to_float8(0.)
-    #
-    #             res = AVX.float_to_float8(0.) # Stores the iteration counts
-    #
+    #         for j in range(in_coords.shape[1]):
+    #             c = in_coords[i, j]
+    #             z = 0
     #             for iter in range(max_iterations):
-    #                 z_sq = AVX.sub( AVX.mul(z_real, z_real), AVX.mul(z_imag, z_imag) )
-    #                 z_ri = AVX.mul(z_real, z_imag)
-    #                 cur_iter = iter
-    #
-    #                 z_real = AVX.add( z_sq, avx_real )
-    #                 z_imag = AVX.add( AVX.add(z_ri, z_ri),  avx_imag )
-    #
-    #                 # Check if we have results > 4
-    #                 mask_gt4 = AVX.less_than(AVX.float_to_float8(4.0), z_sq) # Mask for z_sq > 4.0
-    #                 mask_res0 = AVX.greater_than(res, AVX.float_to_float8(0.)) # Mask for res > 0
-    #                 res = AVX.add(res,
-    #                               AVX.bitwise_and(
-    #                                   AVX.bitwise_andnot(mask_res0, mask_gt4),
-    #                                   AVX.float_to_float8(cur_iter) ) )
-    #
-    #                 # Exit loop if all results are > 0
-    #                 if AVX.signs(mask_res0) == 255:
+    #                 if magnitude_squared(z) > 4:
     #                     break
-    #
-    #                 if iter == max_iterations - 1: # On the last iteration so set any unset results
-    #                     res = AVX.add(res, AVX.bitwise_andnot(mask_res0, AVX.float_to_float8(cur_iter)) )
-    #
-    #             # Update out_counts
-    #             AVX.to_mem(res, &(out_vals[0]))
-    #             for k in range(8):
-    #                 out_counts[i, j + k] = <int> out_vals[k]
+    #                 z = z * z + c
+    #             out_counts[i, j] = iter
+
+    # Reference: http://nullprogram.com/blog/2015/07/10/
+
+    with nogil:
+        for i in prange(in_coords.shape[0], schedule='static', chunksize=1, num_threads=1):
+            for j in range(0, in_coords.shape[1], 8):
+                avx_imag = AVX.make_float8(img[i, j+7], img[i, j+6], img[i,j+5], img[i,j+4], img[i,j+3], img[i,j+2], img[i,j+1],
+                                           img[i,j+0])
+                avx_real = AVX.make_float8(real[i, j+7], real[i, j+6], real[i,j+5], real[i,j+4], real[i,j+3], real[i,j+2], real[i,j+1],
+                                           real[i,j+0])
+
+                z_imag = AVX.float_to_float8(0.)
+                z_real = AVX.float_to_float8(0.)
+
+                res = AVX.float_to_float8(0.) # Stores the iteration counts
+
+                for iter in range(max_iterations):
+                    z_sq = AVX.sub( AVX.mul(z_real, z_real), AVX.mul(z_imag, z_imag) )
+                    z_ri = AVX.mul(z_real, z_imag)
+                    cur_iter = iter
+
+                    z_real = AVX.add( z_sq, avx_real )
+                    z_imag = AVX.add( AVX.add(z_ri, z_ri),  avx_imag )
+
+                    # Check if we have results > 4
+                    mask_gt4 = AVX.less_than(AVX.float_to_float8(4.0), z_sq) # Mask for z_sq > 4.0
+                    mask_res0 = AVX.greater_than(res, AVX.float_to_float8(0.)) # Mask for res > 0
+                    res = AVX.add(res,
+                                  AVX.bitwise_and(
+                                      AVX.bitwise_andnot(mask_res0, mask_gt4),
+                                      AVX.float_to_float8(cur_iter) ) )
+
+                    # Exit loop if all results are > 0
+                    if AVX.signs(mask_res0) == 255:
+                        break
+
+                    if iter == max_iterations - 1: # On the last iteration so set any unset results
+                        res = AVX.add(res, AVX.bitwise_andnot(mask_res0, AVX.float_to_float8(cur_iter)) )
+
+                # Update out_counts
+                AVX.to_mem(res, &(out_vals[0]))
+                for k in range(8):
+                    out_counts[i, j + k] = <int> out_vals[k]
 
--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -15,15 +15,60 @@ import filtering
 from timer import Timer
 import threading
 
+mtx_rem = threading.Lock()
+rem = 0
+fin = threading.Event()
+st = threading.Event()
+
+def runThread(tmpA, tmpB, ID, nThreads, iter):
+    global rem
+    for i in xrange(iter):
+        st.wait() # Wait for the signal
+        for r in xrange(ID, len(tmpA), nThreads):
+            uRow = max(0,r-1)
+            dRow = min(r+2, tmpA.shape[0])
+            offset = int(r > 0)
+            filtering.median_3x3(tmpA[uRow:dRow, :], tmpB[uRow:dRow, :], offset, 2)
+
+        mtx_rem.acquire()
+        rem -= 1
+        if rem == 0:
+            fin.set()
+            fin.clear()
+        mtx_rem.release()
+
+        tmpA, tmpB = tmpB, tmpA
+
 def py_median_3x3(image, iterations=10, num_threads=1):
     ''' repeatedly filter with a 3x3 median '''
+    global rem, event
     tmpA = image.copy()
     tmpB = np.empty_like(tmpA)
+    event = threading.Event()
+    threads = [threading.Thread(target=runThread, args=(tmpA, tmpB, i, num_threads, iterations)) for i in xrange(
+        num_threads)]
+
+    for t in threads:
+        t.start()
 
     for i in range(iterations):
-        filtering.median_3x3(tmpA, tmpB, 0, 1)
-        # swap direction of filtering
+        mtx_rem.acquire()
+        rem = num_threads
+        mtx_rem.release()
+
+        st.set()
+        st.clear()
+
+        fin.wait()
+
         tmpA, tmpB = tmpB, tmpA
+        # for r in xrange(image.shape[0]):
+        #     uRow = max(0,r-1)
+        #     dRow = min(r+2,image.shape[0])
+        #     offset = int(r > 0)
+        #     filtering.median_3x3(tmpA[uRow:dRow, :], tmpB[uRow:dRow, :], offset, 2)
+        # swap direction of filtering
+
 
     return tmpA
 
@@ -54,6 +99,15 @@ if __name__ == '__main__':
     # verify correctness
     from_cython = py_median_3x3(input_image, 2, 5)
     from_numpy = numpy_median(input_image, 2)
+
+    # res = (from_cython == from_numpy)
+    #
+    # for i in xrange( len(from_cython) ):
+    #     for j in xrange( len(from_cython) ):
+    #         if not res[i, j]:
+    #             print 'No match: at ', i, ' ' , j, ' with ', res[i,j]
+    #             print from_cython[i, j], ' vs ', from_numpy[i, j]
+
     assert np.all(from_cython == from_numpy)
 
     with Timer() as t:
