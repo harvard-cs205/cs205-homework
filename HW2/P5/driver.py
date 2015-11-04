caa04b2bf9c3c96fb66dc704851a9d6c054605b6
Patrick Kuiper <patrick.kendal.kuiper@gmail.com>
2015-11-03 23:31:36 -0500

    first commit of driver

--- HW2/P5/driver.py
+++ HW2/P5/driver.py
@@ -16,9 +16,38 @@ from physics import update, preallocate_locks
 def randcolor():
     return np.random.uniform(0.0, 0.89, (3,)) + 0.1
 
+#########################################
+# I employed Z - ordered code from 
+# https://en.wikipedia.org/wiki/Z-order_curve
+# employing the building quad tree algorithm
+##############################################
+
+
+def order(temp, update, D = 2):
+    a = 0
+    b = 0
+    c = 0
+    d = temp[1]
+    e = update[1]
+    for i in range(D):
+        bit = d[i] ^ e[i]
+        if compare(c, bit):
+            a = i
+            c = bit
+    return d[a] - e[a]
+
+def compare(left, right):
+
+    return left < right and left < (left ^ right)
+
+
+
+
 if __name__ == '__main__':
     num_balls = 10000
     radius = 0.002
+    # num_balls = 10
+    # radius = .05
     positions = np.random.uniform(0 + radius, 1 - radius,
                                   (num_balls, 2)).astype(np.float32)
 
@@ -59,13 +88,15 @@ if __name__ == '__main__':
 
     # SUBPROBLEM 4: uncomment the code below.
     # preallocate locks for objects
+    #assign locks pointer
+
     locks_ptr = preallocate_locks(num_balls)
 
     while True:
         with Timer() as t:
             update(positions, velocities, grid,
-                   radius, grid_size, locks_ptr,
-                   physics_step)
+                   radius, grid_spacing, locks_ptr,
+                   physics_step, grid_size)
 
         # udpate our estimate of how fast the simulator runs
         physics_step = 0.9 * physics_step + 0.1 * t.interval
@@ -77,6 +108,40 @@ if __name__ == '__main__':
             print("{} simulation frames per second".format(frame_count / total_time))
             frame_count = 0
             total_time = 0
+
             # SUBPROBLEM 3: sort objects by location.  Be sure to update the
             # grid if objects' indices change!  Also be sure to sort the
             # velocities with their object positions!
+
+
+            point = (positions / grid_spacing).astype(int)
+            array = zip(range(num_balls), point)
+            array.sort(order)
+            index = [i[0] for i in array]
+            positions = positions[index]
+            velocities = velocities[index]
+            test_position = np.array(filter(lambda x: x[0] > 0 and x[1] < 1, positions))
+            test_result = len(test_position)
+            update_positions = - np.ones((grid_size, grid_size), dtype = np.uint32)
+            update_positions[(test_position[:, 0] / grid_spacing).astype(int), (positions[:, 1] / grid_spacing).astype(int)] = np.array(range(test_result))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
