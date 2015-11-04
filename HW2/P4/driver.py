47c523f3963f23f627cc77e6b381b2d6aec96243
Patrick Kuiper <patrick.kendal.kuiper@gmail.com>
2015-11-04 12:31:21 -0500

    updated comments on the code

--- HW2/P4/driver.py
+++ HW2/P4/driver.py
@@ -47,9 +47,11 @@ def py_median_3x3_threading(image, iterations, num_threads):
         thread = threading.Thread(target=worker, args=(tmpA, tmpB, iterations, n, 
                                                        num_threads, event))
         
-        thread.start()
-    thread.join()
+        thread.start() #intiate threads
+    thread.join() #join threads
     
+
+    #make sure to pick out the appropriate thread for odd vs. even iteration
     
     if  iterations % 2 == 1:
         return tmpB
@@ -58,16 +60,28 @@ def py_median_3x3_threading(image, iterations, num_threads):
 
 
 def worker(in_coord, out_coord, iterations, threadid, thread_num, event_array):
+    
+'''
+This function test ensures thread synchronization first evaluates to ensure all adjacent threads
+have finished the previous iteration then executes the operation on the 
+image.
+'''
+
     for i in range(iterations):
 
-        if thread_num == 1:
+        #pass if there is only one thread, no logic neccessary for serial
+        if thread_num == 1: 
             pass
+        #pass if this is only the first iteration, no thread sync neccssary
         elif i == 0:
             pass
+        #if the thread is the first iteration only check the thread after, for previous iteration
         elif threadid == 0:
             event_array[threadid + 1,i - 1].wait()
+        #if thread is the last, only check thread afterward, for previous iteration
         elif threadid == thread_num - 1:
             event_array[threadid - 1, i - 1].wait()
+        #for middle threads check above and below threads, for previous iteration
         else:
             event_array[threadid - 1, i - 1].wait()#causes to current thread
             event_array[threadid + 1, i - 1].wait()# to wait on these threads 
