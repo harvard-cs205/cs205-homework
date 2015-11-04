26974fca421e1c2a259f71393ded0003594fe390
Virgile Audi <vaudi@g.harvard.edu>
2015-11-04 17:18:59 -0500

    Done

--- HW2/P3/example.py
+++ HW2/P3/example.py
@@ -10,5 +10,7 @@ pyximport.install()
 
 import numpy as np
 from mandelbrot import example_sqrt_8
+from mandelbrot import example_
 
-print example_sqrt_8(np.arange(8, dtype=np.float32))
+#print example_sqrt_8(np.arange(8, dtype=np.float32))
+print example_(np.array([5.,5.,5.,3.,6.,5.,5.,8.]).astype(np.float32))
