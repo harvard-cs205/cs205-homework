59745c3bff5d7c8c0e5bf4483dea80fd7b346dc2
Anna Whitney <awhitney@college.harvard.edu>
2015-11-13 14:01:50 -0500

    P3 working

--- HW3/P3/tune.py
+++ HW3/P3/tune.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     devices = [d for platform in platforms for d in platform.get_devices()]
     for i, d in enumerate(devices):
         print("#{0}: {1} on {2}".format(i, d.name, d.platform.name))
-    ctx = cl.Context(devices)
+    ctx = cl.Context(devices[2:])
 
     queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
 
@@ -21,11 +21,10 @@ if __name__ == "__main__":
     x = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=host_x)
 
     times = {}
-
     for num_workgroups in 2 ** np.arange(3, 10):
         partial_sums = cl.Buffer(ctx, cl.mem_flags.READ_WRITE, 4 * num_workgroups)
         host_partial = np.empty(num_workgroups).astype(np.float32)
-        for num_workers in 2 ** np.arange(2, 8):
+        for num_workers in 2 ** np.arange(2, 6):
             local = cl.LocalMemory(num_workers * 4)
             event = program.sum_coalesced(queue, (num_workgroups * num_workers,), (num_workers,),
                                           x, partial_sums, local, np.uint64(N))
@@ -34,7 +33,7 @@ if __name__ == "__main__":
             sum_gpu = sum(host_partial)
             sum_host = sum(host_x)
             seconds = (event.profile.end - event.profile.start) / 1e9
-            assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4
+            assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4,"GPU sum: %r, host sum: %r" % (sum_gpu, sum_host)
             times['coalesced', num_workgroups, num_workers] = seconds
             print("coalesced reads, workgroups: {}, num_workers: {}, {} seconds".
                   format(num_workgroups, num_workers, seconds))
@@ -42,7 +41,7 @@ if __name__ == "__main__":
     for num_workgroups in 2 ** np.arange(3, 10):
         partial_sums = cl.Buffer(ctx, cl.mem_flags.READ_WRITE, 4 * num_workgroups)
         host_partial = np.empty(num_workgroups).astype(np.float32)
-        for num_workers in 2 ** np.arange(2, 8):
+        for num_workers in 2 ** np.arange(2, 6):
             local = cl.LocalMemory(num_workers * 4)
             event = program.sum_blocked(queue, (num_workgroups * num_workers,), (num_workers,),
                                         x, partial_sums, local, np.uint64(N))
@@ -51,7 +50,7 @@ if __name__ == "__main__":
             sum_gpu = sum(host_partial)
             sum_host = sum(host_x)
             seconds = (event.profile.end - event.profile.start) / 1e9
-            assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4
+            assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4,"GPU sum: %r, host sum: %r" % (sum_gpu, sum_host)
             times['blocked', num_workgroups, num_workers] = seconds
             print("blocked reads, workgroups: {}, num_workers: {}, {} seconds".
                   format(num_workgroups, num_workers, seconds))
