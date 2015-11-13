a7eba5d5d82e4b27d8ab55d9bb283a7ff1710d8f
Yj Cheng <cheng.yungjen@gmail.com>
2015-11-12 23:53:16 -0500

    P5 Part 4

--- HW3/P5/label_regions.py
+++ HW3/P5/label_regions.py
@@ -42,7 +42,8 @@ if __name__ == '__main__':
 
     program = cl.Program(context, open('label_regions.cl').read()).build(options='')
 
-    host_image = np.load('maze1.npy')
+    #host_image = np.load('maze1.npy')
+    host_image = np.load('maze2.npy')
     host_labels = np.empty_like(host_image)
     host_done_flag = np.zeros(1).astype(np.int32)
 
