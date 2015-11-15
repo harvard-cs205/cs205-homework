2f32fe8868658c6af7be8d9e5791c5dab541ba4c
Christian Junge <christianjunge@g.harvard.edu>
2015-11-14 19:07:16 -0500

    Finished P3

--- HW3/P3/tune.py
+++ HW3/P3/tune.py
@@ -33,12 +33,10 @@ if __name__ == "__main__":
 
             sum_gpu = sum(host_partial)
             sum_host = sum(host_x)			
-            print(sum_gpu)
-            print(sum_host)
-			
+    		
             seconds = (event.profile.end - event.profile.start) / 1e9
 			
-            #assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4
+            assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4
             times['coalesced', num_workgroups, num_workers] = seconds
             print("coalesced reads, workgroups: {}, num_workers: {}, {} seconds".
                   format(num_workgroups, num_workers, seconds))
@@ -54,6 +52,7 @@ if __name__ == "__main__":
 
             sum_gpu = sum(host_partial)
             sum_host = sum(host_x)
+            
             seconds = (event.profile.end - event.profile.start) / 1e9
             assert abs((sum_gpu - sum_host) / max(sum_gpu, sum_host)) < 1e-4
             times['blocked', num_workgroups, num_workers] = seconds
